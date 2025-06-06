f599da2349323a011d21933be2e223ad0cb0e5b3
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-06 09:40:02 +0530

    Time: 41 ms (79.7%), Space: 34.8 MB (49.01%) - LeetHub

---
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string robotWithString(string s) {
+        int n=s.size();
+        vector<char>minChar(n+1);
+        minChar[n]='z';
+        for(int i=n-1;i>=0;i--){
+            minChar[i]=min(s[i],minChar[i+1]);
+        }
+        string p="";
+        stack<char>st;
+        for(int i=0;i<n;i++){
+             st.push(s[i]);
+           while(!st.empty() && st.top()<=minChar[i+1]){
+            p+=st.top();
+            st.pop();
+           }
+        }
+        while(!st.empty()){
+            p+=st.top();
+            st.pop();
+        }
+        return p;
+    }
+};
