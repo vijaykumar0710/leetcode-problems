831c22086883b8198a39a0dc14f0e02859573a76
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-07 09:31:20 +0530

    Time: 157 ms (37.01%), Space: 29.2 MB (50%) - LeetHub

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+struct Compare {
+    bool operator()(const pair<char, int>& a, const pair<char, int>& b) {
+        if (a.first == b.first) {
+            // For same character, prioritize the larger index (i.e., reverse the comparison)
+            return a.second < b.second;
+        }
+        // Normal lexicographical order (min-heap by char)
+        return a.first > b.first;
+    }
+};
+    string clearStars(string s) {
+        int n=s.size();
+         priority_queue<pair<char, int>, vector<pair<char, int>>, Compare> minPq;
+        for(int i=0;i<n;i++){
+            if(s[i]=='*' && !minPq.empty()){
+                minPq.pop();
+            }else{
+              minPq.push({s[i],i});
+            }
+        }
+        vector<pair<char,int>>pos;
+        while(!minPq.empty()){
+            pos.push_back(minPq.top());
+            minPq.pop();
+        }
+       sort(pos.begin(),pos.end(),[](auto &A,auto &B){
+        return A.second<B.second;
+       });
+       string res="";
+       for(auto &V:pos){
+        res+=V.first;
+       }
+        return res;
+    }
+};
