917b4d9294a2c10f7d03a834750402eecbe04c41
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-08 00:57:33 +0530

    Time: 47 ms (69.38%), Space: 91.3 MB (61.33%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-int f(int idx,int buy,int tran,vector<int>&prices,int n,vector<vector<vector<int>>>&t){
+int t[100000+1][2][3];
+int f(int idx,int buy,int tran,vector<int>&prices,int n){
     if(tran==0) return 0;
     if(idx==n) return 0;
     if(t[idx][buy][tran]!=-1) return t[idx][buy][tran];
     long long profit=0;
     if(buy){
-        profit=max(-prices[idx]+f(idx+1,0,tran,prices,n,t),f(idx+1,1,tran,prices,n,t));
+        profit=max(-prices[idx]+f(idx+1,0,tran,prices,n),f(idx+1,1,tran,prices,n));
     }else{
-        profit=max(prices[idx]+f(idx+1,1,tran-1,prices,n,t),f(idx+1,0,tran,prices,n,t));
+        profit=max(prices[idx]+f(idx+1,1,tran-1,prices,n),f(idx+1,0,tran,prices,n));
     }
     return t[idx][buy][tran]=profit;
 }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>>t(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
-        return f(0,1,2,prices,n,t);
+        memset(t,-1,sizeof(t));
+        return f(0,1,2,prices,n);
     }
 };
