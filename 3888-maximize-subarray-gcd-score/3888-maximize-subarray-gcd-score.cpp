19d1ca435d3eb6e035b58d7665fa6cdeb56de7de
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-08 19:39:16 +0530

    Time: 932 ms (57.14%), Space: 37.8 MB (71.43%) - LeetHub

---
+++ 3888-maximize-subarray-gcd-score/3888-maximize-subarray-gcd-score.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    long long maxGCDScore(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>twoDivisorCount(n,0);
+        for(int i=0;i<n;i++){
+            int val=nums[i];
+            while(val%2==0){
+                twoDivisorCount[i]++;
+                val/=2;
+            }
+        }
+        long long maxGcd=0ll;
+        for(int i=0;i<n;i++){
+            int currentGcd=0ll;
+            int minTwoDivisor=1e9;
+            int minTwoDivisorCount=0;
+            for(int j=i;j<n;j++){
+               currentGcd=gcd(currentGcd,nums[j]);
+               if(twoDivisorCount[j]<minTwoDivisor){
+                minTwoDivisor=twoDivisorCount[j];
+                minTwoDivisorCount=0;
+               }
+
+               if(twoDivisorCount[j]==minTwoDivisor)
+                  minTwoDivisorCount++;
+
+               int currentLength=(j-i+1);
+               if(minTwoDivisorCount<=k)
+                 maxGcd=max(maxGcd,2LL*currentLength*currentGcd);
+                else maxGcd=max(maxGcd,1LL*currentLength*currentGcd);
+            }
+        }
+        return maxGcd;
+    }
+};
