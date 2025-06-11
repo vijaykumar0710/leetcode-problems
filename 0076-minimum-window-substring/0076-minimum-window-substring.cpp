7dea6ffc4dc6d357003ef5403704c70d1a0f3f5a
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-11 10:21:29 +0530

    Time: 6 ms (70.29%), Space: 11.3 MB (83.66%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.size();
-        unordered_map<char, int> mp;
-        for (auto ch : t) mp[ch]++;
-
-        int i = 0, j = 0;
-        int cnt = mp.size();
-        int size = n + 1;
-        int start = -1;
-
-        while (j < n) {
-                if (mp.find(s[j]) != mp.end()) {
-                    mp[s[j]]--;
-                    if (mp[s[j]] == 0) cnt--;
-                }
-                while (cnt == 0) {
-                    if ((j - i + 1) < size) {
-                        start = i;
-                        size = j - i + 1;
+        int n=s.size();
+        unordered_map<char,int>mp;
+        for(int i=0;i<t.size();i++){
+            mp[t[i]]++;
+        }
+        int cnt=mp.size();
+        int i=0,j=0;
+        int sizeOfWindow=n+1,startIdx=-1;
+        while(j<n){
+            if(mp.find(s[j])!=mp.end()){
+                mp[s[j]]--;
+                if(mp[s[j]]==0)
+                   cnt--;
+            }
+                while(cnt==0){
+                    if(sizeOfWindow>(j-i+1)){
+                    sizeOfWindow=j-i+1;
+                    startIdx=i;
                     }
-                    if (mp.find(s[i]) != mp.end()) {
-                        mp[s[i]]++;
-                        if (mp[s[i]] > 0) cnt++;
+                    if(mp.find(s[i])!=mp.end()){
+                       mp[s[i]]++;
+                       if(mp[s[i]]>0) cnt++;
                     }
                     i++;
-                }
-                j++;
+             }
+             j++;
         }
-
-        if (start == -1) return "";
-        return s.substr(start, size);
+        if(startIdx==-1) return "";
+        return s.substr(startIdx,sizeOfWindow);
     }
 };
