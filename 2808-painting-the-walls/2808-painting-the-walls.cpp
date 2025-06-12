58c06ac77967fbc8cdf1fe5fd1807120c72410dd
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-12 15:09:40 +0530

    Time: 171 ms (12.13%), Space: 192.6 MB (9.79%) - LeetHub

--- 2808-painting-the-walls/2808-painting-the-walls.cpp
+++ 2808-painting-the-walls/2808-painting-the-walls.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int n;
     const long long INF = INT_MAX;
-    vector<vector<long long>> dp;
 
-    long long solve(int i, int freetime, vector<int>& cost, vector<int>& time) {
+    long long solve(int i, int freetime, vector<int>& cost, vector<int>& time, vector<vector<long long>>&dp) {
         if (i == n) {
             return freetime >= 0 ? 0 : INF;
         }
@@ -15,17 +14,17 @@ public:
 
         // Option 1: hire painter i
         int newFree = min(n, freetime + time[i]);
-        long long hire = cost[i] + solve(i + 1, newFree, cost, time);
+        long long hire = cost[i] + solve(i + 1, newFree, cost, time,dp);
 
         // Option 2: self-paint
-        long long self = solve(i + 1, freetime - 1, cost, time);
+        long long self = solve(i + 1, freetime - 1, cost, time,dp);
 
         return dp[i][freetime + n] = min(hire, self);
     }
 
     int paintWalls(vector<int>& cost, vector<int>& time) {
         n = cost.size();
-        dp.assign(n + 1, vector<long long>(2 * n + 1, -1));
-        return solve(0, 0, cost, time);
+          vector<vector<long long>> dp(n + 1, vector<long long>(2 * n + 1, -1));
+        return solve(0, 0, cost, time,dp);
     }
 };
