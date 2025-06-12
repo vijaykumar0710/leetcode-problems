e1ab3d609ae28a1485a9f868eced809fa483260e
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-12 15:45:00 +0530

    Time: 178 ms (12.13%), Space: 192.5 MB (9.9%) - LeetHub

--- 2808-painting-the-walls/2808-painting-the-walls.cpp
+++ 2808-painting-the-walls/2808-painting-the-walls.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
-    int n;
-    const long long INF = INT_MAX;
+long long solve(int i,int freetime,vector<int>&cost,vector<int>&time,vector<vector<long long>>&t,int n){
+   if(i==n){
+       return freetime>=0?0:INT_MAX;
+   }
+    if(t[i][freetime+n]!=-1) return t[i][freetime+n];
+    int max_free_time=min(n,freetime+time[i]);
+    long long paid_painter=cost[i]+solve(i+1,max_free_time,cost,time,t,n);
+    long long free_painter=solve(i+1,freetime-1,cost,time,t,n);
 
-    long long solve(int i, int freetime, vector<int>& cost, vector<int>& time, vector<vector<long long>>&dp) {
-        if (i == n) {
-            return freetime >= 0 ? 0 : INF;
-        }
-
-        if (dp[i][freetime + n] != -1) {
-            return dp[i][freetime + n];
-        }
-
-        // Option 1: hire painter i
-        int newFree = min(n, freetime + time[i]);
-        long long hire = cost[i] + solve(i + 1, newFree, cost, time,dp);
-
-        // Option 2: self-paint
-        long long self = solve(i + 1, freetime - 1, cost, time,dp);
-
-        return dp[i][freetime + n] = min(hire, self);
-    }
-
+    return t[i][freetime+n]=min(paid_painter,free_painter);
+}
     int paintWalls(vector<int>& cost, vector<int>& time) {
-        n = cost.size();
-          vector<vector<long long>> dp(n + 1, vector<long long>(2 * n + 1, -1));
-        return solve(0, 0, cost, time,dp);
+        int n=cost.size();
+        vector<vector<long long>>t(n+1,vector<long long>(2*n+1,-1));
+        return solve(0,0,cost,time,t,n);
     }
 };
