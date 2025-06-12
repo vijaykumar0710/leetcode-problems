59e21bb448ee8cd6254e479e944c1fe37641741a
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-12 12:46:40 +0530

    Time: 297 ms (40.36%), Space: 134.1 MB (39.76%) - LeetHub

--- 2848-special-permutations/2848-special-permutations.cpp
+++ 2848-special-permutations/2848-special-permutations.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
 const int M=1e9+7;
-    int specialPerm(vector<int>& nums) {
-        int n=nums.size();
-        int N=1<<n;
-        vector<vector<int>>dp(N,vector<int>(n,0));
-        for(int i=0;i<n;i++) dp[1<<i][i]=1;
-        for(int mask=1;mask<N;mask++){
-            for(int last=0;last<n;last++){
-                //check whether last is present or not in mask if not then skip
-                if(!(mask&(1<<last))) continue;
-                for(int i=0;i<n;i++){
-                    //check whether it used(present in mask) or not if it is used then skip
-                    if((mask&(1<<i))) continue;
-                    if(nums[i]%nums[last]==0 || nums[last]%nums[i]==0){ 
-                    int newMask=mask|(1<<i);
-                    dp[newMask][i]=(dp[newMask][i]+dp[mask][last])%M;
-                    }
-                }
+int dfs(vector<int>&nums,int mask,int last,int n,vector<vector<int>>&dp){
+    if(mask==(1<<n)-1) return 1;
+    if(dp[mask][last]!=-1) return dp[mask][last];
+    int ans=0;
+    for(int i=0;i<n;i++){
+        if(!(mask&(1<<i))){
+            if(nums[i]%nums[last]==0 || nums[last]%nums[i]==0){
+                ans=(ans+dfs(nums,mask|(1<<i),i,n,dp))%M;
             }
         }
-        int ans=0;
+    }
+    return dp[mask][last]=ans;
+}
+    int specialPerm(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dp(1<<n,vector<int>(n,-1));
+        int res=0;
         for(int i=0;i<n;i++){
-            ans=(ans+dp[N-1][i])%M;
+            res=(res+dfs(nums,1<<i,i,n,dp))%M;
         }
-        return ans;
+        return res;
     }
 };
