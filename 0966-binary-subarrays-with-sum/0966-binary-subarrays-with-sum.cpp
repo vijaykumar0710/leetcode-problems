362fbe5fbfc38ccca0330e80f3363a8aaa55158a
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-13 01:23:46 +0530

    Time: 20 ms (27.62%), Space: 40.3 MB (9.5%) - LeetHub

---
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n=nums.size();
+        int sum=0,ans=0;
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum==goal) ans++;
+            ans+=mp[sum-goal];
+             mp[sum]++;
+        }
+        return ans;
+    }
+};
