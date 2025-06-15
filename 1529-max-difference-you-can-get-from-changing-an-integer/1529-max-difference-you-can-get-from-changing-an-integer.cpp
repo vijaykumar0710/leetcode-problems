d07484c5e226239f0298f552817cbd76ed5b1248
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-15 11:10:32 +0530

    Time: 0 ms (100%), Space: 8.1 MB (69.19%) - LeetHub

--- 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.cpp
@@ -3,40 +3,30 @@ public:
 vector<int>formed_nums_arr(int num){
     vector<int>temp;
     int n=num;
-    while(n){
-        temp.push_back(n%10);
-        n/=10;
-    }
+    while(n){ temp.push_back(n%10); n/=10; }
     reverse(temp.begin(),temp.end());
     return temp;
 }
+
 int formed_largest_num(vector<int>&nums){
     int x=9;
     for(auto &ele:nums){
-         if(ele!=9){
-            x=ele;
-           break;
-         }
+         if(ele!=9){ x=ele; break; }
     }
     int large_number=0;
     for(auto &ele:nums){
-        if(ele==x){
-            large_number=large_number*10+9;
-        }else{
-            large_number=large_number*10+ele;
-        }
+        if(ele==x) large_number=large_number*10+9;
+        else large_number=large_number*10+ele;
     }
     return large_number;
 }
+
 int formed_smallest_num(vector<int>nums){
     int x=1;
     if(nums[0]!=1) x=nums[0];
     else{
         for(int i=1;i<nums.size();i++){
-            if(nums[i]!=0 && nums[i]!=nums[0]){
-                x=nums[i];
-                break;
-            }
+           if(nums[i]!=0 && nums[i]!=nums[0]){ x=nums[i]; break; }
         }
     }
     int smallest_num=0;
@@ -57,8 +47,6 @@ int formed_smallest_num(vector<int>nums){
         vector<int>nums=formed_nums_arr(num);
         int largest_num=formed_largest_num(nums);
         int smallest_num=formed_smallest_num(nums);
-        cout<<largest_num<<endl;
-        cout<<smallest_num<<endl;
         return largest_num-smallest_num;
     }
 };
