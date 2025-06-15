aab8a4ae1ea29e2f21fd6d235b0bf3308ab9306d
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-15 18:29:11 +0530

    Time: 0 ms (100%), Space: 16.4 MB (31.43%) - LeetHub

---
+++ 0896-smallest-subtree-with-all-the-deepest-nodes/0896-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+pair<int,TreeNode*>dfs(TreeNode* root){
+   if(!root) return {0,NULL};
+
+   auto left=dfs(root->left);
+   auto right=dfs(root->right);
+
+   int left_depth=left.first;
+   int right_depth=right.first;
+
+   if(left_depth==right_depth){ return {right_depth+1,root}; }
+    else if(left_depth>right_depth) { return {left_depth+1,left.second}; }
+    else return {right_depth+1,right.second};
+}
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        return dfs(root).second;
+    }
+};
