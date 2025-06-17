3b599191a58e917357dbe71e7f15a0de5e34115c
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-17 19:42:16 +0530

    Time: 398 ms (10.85%), Space: 119.7 MB (5.01%) - LeetHub

--- 1465-maximum-product-of-splitted-binary-tree/1465-maximum-product-of-splitted-binary-tree.cpp
+++ 1465-maximum-product-of-splitted-binary-tree/1465-maximum-product-of-splitted-binary-tree.cpp
@@ -4,20 +4,22 @@ const int M=1e9+7;
 unordered_map<TreeNode*,long long> subtree_sum;
 long long product=1;
 long long total=0;
- void dfs1(TreeNode* node, TreeNode* parent) {
+ void dfs1(TreeNode* node) {
     if (!node) return;
-    subtree_sum[node] = node->val;
-    for (TreeNode* child : {node->left, node->right}) {
-        if (!child) continue;
-        dfs1(child, node);
-        subtree_sum[node] += subtree_sum[child];
+
+    dfs1(node->left);
+    dfs1(node->right);
+
+    int leftSum = node->left ? subtree_sum[node->left] : 0;
+    int rightSum = node->right ? subtree_sum[node->right] : 0;
+
+    subtree_sum[node] = node->val + leftSum + rightSum;
         total=max(total,subtree_sum[node]);
     }
-}
  
  void dfs2(TreeNode* node, TreeNode* parent) {
-        for (TreeNode* child : {node->left, node->right}) {
-          if (!child) continue;
+    for (TreeNode* child : {node->left, node->right}) {
+    if (!child) continue;
     long long part1=subtree_sum[child];
     long long part2=(total-part1);
     product=max(product,part1*part2);
@@ -39,7 +41,7 @@ long long total=0;
         }
     }
     int maxProduct(TreeNode* root) {
-        dfs1(root,NULL);
+        dfs1(root);
         dfs2(root,NULL);
         return product%M;
     } 
