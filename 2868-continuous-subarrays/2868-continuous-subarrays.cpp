294fa3783464e977af4c95c7355d2d8977b1247e
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-17 10:57:04 +0530

    Time: 230 ms (18.34%), Space: 156 MB (25.41%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.cpp
+++ 2868-continuous-subarrays/2868-continuous-subarrays.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
-        int n = nums.size();
-        long long cnt = 0;
-        int i = 0, j = 0;
-        
-        deque<int> maxDeque; // Monotonically decreasing deque (stores indices)
-        deque<int> minDeque; // Monotonically increasing deque (stores indices)
-        
-        while (j < n) {
-            // Maintain maxDeque
-            while (!maxDeque.empty() && nums[maxDeque.back()] <= nums[j]) {
-                maxDeque.pop_back();
-            }
-            maxDeque.push_back(j);
-
-            // Maintain minDeque
-            while (!minDeque.empty() && nums[minDeque.back()] >= nums[j]) {
-                minDeque.pop_back();
-            }
-            minDeque.push_back(j);
-
-            // Check if the window is valid
-            while (nums[maxDeque.front()] - nums[minDeque.front()] > 2) {
-                // Shrink the window from the left
-                if (maxDeque.front() == i) maxDeque.pop_front();
-                if (minDeque.front() == i) minDeque.pop_front();
+        int n=nums.size();
+        multiset<long long>ms;
+        long long subarray_cnt=0;
+        int i=0;
+        for(int j=0;j<n;j++){
+            ms.insert(nums[j]);
+            while(*ms.rbegin()-*ms.begin()>2){
+                ms.erase(ms.find(nums[i]));
                 i++;
             }
-
-            // Count subarrays ending at `j`
-            cnt += (j - i + 1);
-            j++; // Expand the window
+            subarray_cnt+=(j-i+1);
         }
-
-        return cnt;
+        return subarray_cnt;
     }
 };
