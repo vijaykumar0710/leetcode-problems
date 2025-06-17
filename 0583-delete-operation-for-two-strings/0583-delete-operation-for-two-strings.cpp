a863f2def1f44f1862b87a04b2439b1d504fa69b
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-17 17:08:30 +0530

    Time: 15 ms (31.8%), Space: 16.8 MB (56.61%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -11,9 +11,23 @@ int helper(string &s1,string &s2,int m,int n,vector<vector<int>>&t){
           return t[m][n]=1+min({helper(s1,s2,m-1,n,t),helper(s1,s2,m,n-1,t)});
       }
   }
-    int minDistance(string s1, string s2) {
-        int m=s1.size(),n=s2.size();
-        vector<vector<int>>t(m+1,vector<int>(n+1,-1));
-        return helper(s1,s2,m,n,t);
+    int minDistance(string word1, string word2) {
+        int m=word1.size(),n=word2.size();
+        vector<vector<int>>t(m+1,vector<int>(n+1));
+        for(int i=0;i<=m;i++)
+           t[i][0]=i;
+         for(int j=0;j<=n;j++)
+           t[0][j]=j;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1[i-1]==word2[j-1]){
+                    t[i][j]=t[i-1][j-1];
+                }
+                else{
+                   t[i][j]=1+min(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        return t[m][n];
     }
 };
