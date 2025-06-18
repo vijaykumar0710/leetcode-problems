782631341d60b6e95e5bd0adad1da135f372f191
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-18 12:29:50 +0530

    Time: 7 ms (39.36%), Space: 32.3 MB (5.11%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -4,14 +4,13 @@ public:
   vector<int>dfs(TreeNode* root){
     if(!root) return {0,0};
 
-    if(dp.count(root)) return dp[root];
     auto left=dfs(root->left);
     auto right=dfs(root->right);
 
     int exclude=max(left[0],left[1])+max(right[0],right[1]);
     int include=root->val+left[0]+right[0];
 
-    return dp[root]={exclude,include};
+    return {exclude,include};
   }
     int rob(TreeNode* root) {
       auto res=dfs(root);
