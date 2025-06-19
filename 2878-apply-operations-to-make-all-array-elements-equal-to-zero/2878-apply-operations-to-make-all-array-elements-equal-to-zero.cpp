1c532212189a7e5cd0349515b12f2f1b9dcd0b28
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-20 00:36:36 +0530

    Time: 0 ms (100%), Space: 133.7 MB (26.99%) - LeetHub

--- 2878-apply-operations-to-make-all-array-elements-equal-to-zero/2878-apply-operations-to-make-all-array-elements-equal-to-zero.cpp
+++ 2878-apply-operations-to-make-all-array-elements-equal-to-zero/2878-apply-operations-to-make-all-array-elements-equal-to-zero.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     bool checkArray(vector<int>& nums, int k) {
         int n=nums.size();
-         vector<int>diff(n+1,0);
-         int prefix_sum=0;
-        for(int i=0;i<n;i++){
-            prefix_sum+=diff[i];
-            int val=nums[i]+prefix_sum;
-            if(val==0) continue;
-            if(val<0 || i+k>n) return false;
-             prefix_sum-=val;
-            diff[i]-=val;
-            diff[i+k]+=val;
-        }
-        return true;
+        int curr_add=0;
+        vector<int>diff(n+1,0);
+        for (int i = 0; i < n; i++) {
+        curr_add += diff[i];
+
+        nums[i] += curr_add;
+        if(nums[i]<0) return false;
+        if (nums[i] != 0) {
+        if (i + k > n) return false;
+
+        int delta = -nums[i]; // amount to add in range [i..i+k-1]
+        curr_add += delta;
+        diff[i + k] -= delta; // rollback after k steps
+        nums[i] = 0;
+         }
+       }
+       return true;
     }
 };
