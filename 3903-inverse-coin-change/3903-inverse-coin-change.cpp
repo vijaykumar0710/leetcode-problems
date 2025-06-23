072cc65a908a8aeaccfe329eec4ae4d8e02b4801
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-23 17:49:22 +0530

    Time: 68 ms (5.88%), Space: 75.5 MB (5.88%) - LeetHub

---
+++ 3903-inverse-coin-change/3903-inverse-coin-change.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+int change(int target,vector<int>&coin){
+    int n=coin.size();
+    vector<vector<int>>t(n+1,vector<int>(target+1,0));
+    for(int i=0;i<=n;i++)
+       t[i][0]=1;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<=target;j++){
+            if(coin[i-1]<=j){
+                t[i][j]=t[i-1][j]+t[i][j-coin[i-1]];
+            }else{
+                t[i][j]=t[i-1][j];
+            }
+        }
+    }
+    return t[n][target];
+}
+    vector<int> findCoins(vector<int>& numWays) {
+       int n = numWays.size();
+        vector<int> res;
+        for(int i = 0;i<n;i++){
+            int ways = change(i+1,res);
+            if(ways+1==numWays[i]) res.push_back(i+1);
+            else if(ways!=numWays[i]) return {};
+        }
+        return res;
+    }
+};
