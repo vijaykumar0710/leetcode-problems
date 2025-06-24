4f1cccefb056c41583f847376d1a01f4b3e51488
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-24 22:42:15 +0530

    Time: 5 ms (98.38%), Space: 86.5 MB (48.3%) - LeetHub

--- 1814-jump-game-vi/1814-jump-game-vi.cpp
+++ 1814-jump-game-vi/1814-jump-game-vi.cpp
@@ -4,11 +4,11 @@ public:
         int n=nums.size();
         vector<int>t(n);
         deque<int>dq;
-         t[0]=nums[0];
-         dq.push_back(0);
+        t[0]=nums[0];
+        dq.push_back(0);
         for(int i=1;i<n;i++){
             while(!dq.empty() && dq.front()<i-k) dq.pop_front();
-             t[i]=nums[i]+t[dq.front()];
+            t[i]=nums[i]+t[dq.front()];
             while(!dq.empty() && t[dq.back()]<t[i]) dq.pop_back();
             dq.push_back(i);
         }
