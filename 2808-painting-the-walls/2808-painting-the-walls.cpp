4aa3d67618f97ea9166c439c2719d423708c989d
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-06-24 23:09:55 +0530

    Time: 175 ms (13.12%), Space: 192.7 MB (11.09%) - LeetHub

--- 2808-painting-the-walls/2808-painting-the-walls.cpp
+++ 2808-painting-the-walls/2808-painting-the-walls.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-long long solve(int i,int freetime,vector<int>&cost,vector<int>&time,vector<vector<long long>>&t,int n){
-   if(i==n){
-       return freetime>=0?0:INT_MAX;
-   }
+long long solve(int i,int freetime,vector<int>&cost,vector<int>&time,int n,vector<vector<long long>>&t){
+    if(i==n){
+        return freetime>=0?0:INT_MAX;
+    }
     if(t[i][freetime+n]!=-1) return t[i][freetime+n];
-    int max_free_time=min(n,freetime+time[i]);
-    long long paid_painter=cost[i]+solve(i+1,max_free_time,cost,time,t,n);
-    long long free_painter=solve(i+1,freetime-1,cost,time,t,n);
-
+    int maxfreetime=min(n,freetime+time[i]);
+    long long paid_painter=cost[i]+solve(i+1,maxfreetime,cost,time,n,t);
+    long long free_painter=solve(i+1,freetime-1,cost,time,n,t);
     return t[i][freetime+n]=min(paid_painter,free_painter);
 }
     int paintWalls(vector<int>& cost, vector<int>& time) {
         int n=cost.size();
         vector<vector<long long>>t(n+1,vector<long long>(2*n+1,-1));
-        return solve(0,0,cost,time,t,n);
+        return solve(0,0,cost,time,n,t);
     }
 };
