ab9c2cb76a68b42271a10deb9229b317067d4c9d
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-01 11:44:23 +0530

    Time: 8 ms (31.76%), Space: 25.2 MB (23.01%) - LeetHub

--- 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.cpp
+++ 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
  long long solve(TreeNode* root,unordered_map<long long,int>&mp,int &max_freq){
     if(!root) return 0ll;
-    long long curr_sum=root->val;
+    long long sum=root->val;
     long long left_sum=solve(root->left,mp,max_freq);
     long long right_sum=solve(root->right,mp,max_freq);
-    long long sum=curr_sum+left_sum+right_sum;
+     sum+=left_sum+right_sum;
     mp[sum]++;
     max_freq=max(max_freq,mp[sum]);
     return sum;
