b304b01780af1c8b9dac190a75ab6b3e8e27b543
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-02 10:53:27 +0530

    Time: 0 ms (100%), Space: 13 MB (45.39%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-int m,n;
-int t[201][201];
-int solve(int i,int j,vector<vector<int>>& grid){
-    if(i>=m || j>=n) return INT_MAX;
-
-    if(t[i][j]!=-1) return t[i][j];
-
-    int right=solve(i,j+1,grid);
-    int down=solve(i+1,j,grid);
-
-   if(i==m-1 && j==n-1){
-        if(grid[i][j]>=0)
-          return 1;
-        else{
-            return abs(grid[i][j])+1;
-        }
-    }
-
-    int res=min(right,down)-grid[i][j]; 
-    return t[i][j]=res>0?res:1;
-}
     int calculateMinimumHP(vector<vector<int>>& grid) {
-        m=grid.size();
-        n=grid[0].size();
-        memset(t,-1,sizeof(t));
-        return solve(0,0,grid);
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>>t(m,vector<int>(n,0));
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1 && j==n-1){
+                    if(grid[i][j]>=0) t[i][j]=1;
+                    else t[i][j]=abs(grid[i][j])+1;
+                }
+                else{ 
+                int right=j+1>=n?1e9:t[i][j+1];
+                int down=i+1>=m?1e9:t[i+1][j];
+                int res=min(right,down)-grid[i][j];
+                t[i][j]=res>0?res:1;
+                }
+            }
+        }
+        return t[0][0];
     }
 };
