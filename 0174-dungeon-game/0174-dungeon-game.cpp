65df3a78cfc1f26884491da2144351758886b6fb
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-02 09:20:57 +0530

    Time: 2850 ms (5.09%), Space: 270.6 MB (5.02%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,36 +1,52 @@
 class Solution {
 public:
     int m, n;
-    vector<vector<int>>directions={{0,1},{1,0}};
-    unordered_map<string,bool>t;
-    bool solve(vector<vector<int>>& grid,int row,int col,int mid){
-    if( row>=m || col>=n) return false;
-    mid+=grid[row][col];
-    if(mid<=0) return false;
-    if(row==m-1 && col==n-1) return true;
-    string key=to_string(row)+"&"+to_string(col)+"&"+to_string(mid);
-    if(t.find(key)!=t.end()) return t[key];
-    for(auto &dir:directions){
-        int new_r=dir[0]+row;
-        int new_c=dir[1]+col;
-       if(solve(grid,new_r,new_c,mid)) return t[key]=true;
+    unordered_map<string, int> memo;
+vector<vector<int>>directions={{0,1},{1,0}};
+    bool canSurvive(int i, int j, vector<vector<int>>& grid, int currHealth) {
+        if (i >= m || j >= n) return false;
+
+        currHealth += grid[i][j];
+        if (currHealth <= 0) 
+            return false;
+
+        if (i == m - 1 && j == n - 1) 
+            return true;
+
+        string key = to_string(i) + "_" + to_string(j) + "_" + to_string(currHealth);
+        if (memo.count(key))
+            return memo[key];
+
+       for(auto &dir:directions){
+        int new_r=dir[0]+i;
+        int new_c=dir[1]+j;
+       if(canSurvive(new_r,new_c,grid,currHealth)) return memo[key]=true;
     }
-    return t[key]=false;
+    return memo[key]=false;
     }
-    int calculateMinimumHP(vector<vector<int>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
-        int l=1;
-        int r=4*1e7;
-        int res=4*1e7;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(solve(grid,0,0,mid)){
-                res=min(res,mid);
-                r=mid-1;
+
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        m = dungeon.size();
+        n = dungeon[0].size();
+
+        int left = 1;
+        int right = 4 * 1e7;
+        int result = right;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+           // memo.clear();  // clear for new health level
+
+            if (canSurvive(0, 0, dungeon, mid)) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
-            else l=mid+1;
         }
-        return res;
+
+        return result;
     }
 };
+
