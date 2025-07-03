c8e2bb57fce67ab92a4a251ebc058d0549ae31b8
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-03 07:41:49 +0530

    Time: 7 ms (16.92%), Space: 13.2 MB (9.27%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    unordered_map<int, string> t;
-
-    string solve(int pow) {
-        if (pow == 1) return "a";
-        if (t.count(pow)) return t[pow];
-
-        string left = solve(pow / 2);
-        string right = "";
-        for (char ch : left) {
-            right += 'a' + (ch - 'a' + 1) % 26;
-        }
-
-        return t[pow] = left + right;
+unordered_map<int,string>t;
+string solve(int pow){
+    if(pow==1) return "a";
+    if(t.find(pow)!=t.end()) return t[pow];
+    string temp=solve(pow/2);
+    string res;
+    res=temp;
+    for(auto ch:temp){
+        res+='a'+(ch-'a'+1)%26;
     }
-
+    return t[pow]=res;
+}
     char kthCharacter(int k) {
-        int pow = 512;  // should be high enough to cover k
-        string res = solve(pow);
-        return res[k - 1];
+       int pow=512;
+        string res=solve(pow);
+        return res[k-1];
     }
 };
