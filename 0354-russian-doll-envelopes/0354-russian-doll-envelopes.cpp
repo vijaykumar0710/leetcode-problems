38fe5b089c2acfebf93d200eaa950357dc298439
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-05 08:23:14 +0530

    Time: 41 ms (63.74%), Space: 81.7 MB (77.84%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -6,12 +6,12 @@ public:
             if (a[0] == b[0]) return a[1] > b[1];
             return a[0] < b[0];
         });
-        vector<int>sorted;
-        for(int i=0;i<n;i++){
-            auto it=lower_bound(sorted.begin(),sorted.end(),envelopes[i][1]);
-            if(it==sorted.end()) sorted.push_back(envelopes[i][1]);
-            else *it=envelopes[i][1];
-        }
-         return sorted.size();
+       vector<int>sorted;
+       for(int i=0;i<n;i++){
+        auto it=lower_bound(sorted.begin(),sorted.end(),envelopes[i][1]);
+        if(it==sorted.end()) sorted.push_back(envelopes[i][1]);
+        else *it=envelopes[i][1];
+       }
+       return sorted.size();
     }
 };
