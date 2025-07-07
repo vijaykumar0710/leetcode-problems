26b802b289477d55b62c555f9e194400e6befb15
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-07 10:15:07 +0530

    Time: 41 ms (97.97%), Space: 76.3 MB (25.56%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
+    vector<int> parent;
+
+    // DSU find with path compression
+    int find(int day) {
+        if (parent[day] != day)
+            parent[day] = find(parent[day]);
+        return parent[day];
+    }
+
     int maxEvents(vector<vector<int>>& events) {
-        int n = events.size();
-        sort(events.begin(), events.end());
-        int last_day = 0;
-        for (auto& event : events)
-            last_day = max(last_day, event[1]);
-        priority_queue<int, vector<int>, greater<int>> pq;
-        int i = 0, res = 0;
-        for (int day = 1; day <= last_day; day++) {
-            while (i < n && events[i][0] == day) {
-                pq.push(events[i][1]);
-                i++;
-            }
-            while (!pq.empty() && pq.top() < day)
-                pq.pop();
-            if (!pq.empty()) {
-                res++;
-                pq.pop();
+        // Sort events by end time (greedy for earliest available day)
+        sort(events.begin(), events.end(), [](auto &a, auto &b) {
+            return a[1] < b[1];
+        });
+
+        int max_day = 0;
+        for (auto &e : events) max_day = max(max_day, e[1]);
+
+        // Initialize DSU
+        parent.resize(max_day + 2);
+        for (int i = 0; i <= max_day + 1; i++) {
+            parent[i] = i;
+        }
+
+        int count = 0;
+        for (auto &e : events) {
+            int start = e[0], end = e[1];
+            int day = find(start);
+            if (day <= end) {
+                count++;
+                // Mark this day as used
+                parent[day] = find(day + 1);
             }
         }
-        return res;
+        return count;
     }
 };
