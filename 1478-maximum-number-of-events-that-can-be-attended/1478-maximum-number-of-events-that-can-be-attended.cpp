5252d056475078afb247864c77a65a1a64fc5794
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-07 10:21:25 +0530

    Time: 36 ms (99.42%), Space: 76.5 MB (25.46%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     vector<int> parent;
-
-    // DSU find with path compression
     int find(int day) {
         if (parent[day] != day)
             parent[day] = find(parent[day]);
         return parent[day];
     }
-
+void Union(int x,int y){
+    parent[x]=y;
+}
     int maxEvents(vector<vector<int>>& events) {
-        // Sort events by end time (greedy for earliest available day)
         sort(events.begin(), events.end(), [](auto &a, auto &b) {
             return a[1] < b[1];
         });
 
         int max_day = 0;
         for (auto &e : events) max_day = max(max_day, e[1]);
-
-        // Initialize DSU
         parent.resize(max_day + 2);
         for (int i = 0; i <= max_day + 1; i++) {
             parent[i] = i;
@@ -30,8 +27,7 @@ public:
             int day = find(start);
             if (day <= end) {
                 count++;
-                // Mark this day as used
-                parent[day] = find(day + 1);
+                Union(day,day+1);
             }
         }
         return count;
