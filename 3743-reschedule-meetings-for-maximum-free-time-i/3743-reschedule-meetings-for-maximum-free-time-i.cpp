9df73d9c4fa9c8fbf9d136ca3e5a55ae4d347ce7
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-09 19:21:05 +0530

    Time: 398 ms (9.29%), Space: 125.9 MB (36.67%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -3,12 +3,14 @@ public:
     int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
         int n=startTime.size();
         vector<int>freeTime;
-         freeTime.push_back(startTime[0]-0);
+        if(startTime[0]!=0) freeTime.push_back(startTime[0]-0);
         for(int i=1;i<n;i++){
             freeTime.push_back(startTime[i]-endTime[i-1]);
         }
-        freeTime.push_back(eventTime-endTime[n-1]);
+        if(eventTime!=endTime[n-1]) freeTime.push_back(eventTime-endTime[n-1]);
        for(auto &x:freeTime) cout<<x;
+        int sum=accumulate(freeTime.begin(),freeTime.end(),0);
+        if(freeTime.size()<k+1) return sum;
         int gap=0,max_gap=0;
         int i=0;
         for(int j=0;j<freeTime.size();j++){
