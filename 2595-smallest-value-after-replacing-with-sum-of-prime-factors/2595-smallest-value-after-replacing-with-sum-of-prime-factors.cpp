47d13dcbfe430898ac91db6956a83500619366a9
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-09 12:39:08 +0530

    Time: 91 ms (5%), Space: 8.5 MB (8.62%) - LeetHub

--- 2595-smallest-value-after-replacing-with-sum-of-prime-factors/2595-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
+++ 2595-smallest-value-after-replacing-with-sum-of-prime-factors/2595-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
@@ -6,14 +6,16 @@ int spf[N];
          for (int i = 1; i < N; i++){
         spf[i] = i;
     }
-    for (int i = 2; i * i < N; i++) {
-        if (spf[i] == i) { // i is prime
-            for (int j = i * i; j < N; j += i) {
-                if (spf[j] == j)
-                    spf[j] = i;
+    for (int i = 2; i * i <= N; i++) {
+    if (spf[i] == i) { // i is prime
+        for (int j = 2; i * j < N; j++) {
+            if (spf[i * j] == i * j) {
+                spf[i * j] = i;
             }
         }
     }
+}
+
         while(n!=spf[n]){ 
          int sum=0;
          int x=n;
