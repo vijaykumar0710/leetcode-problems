0a2985a8eedfd2606201e4ed8ab99569bf4b168a
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-09 12:46:28 +0530

    Time: 87 ms (5%), Space: 8.3 MB (9.31%) - LeetHub

--- 2595-smallest-value-after-replacing-with-sum-of-prime-factors/2595-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
+++ 2595-smallest-value-after-replacing-with-sum-of-prime-factors/2595-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-const static int N=1e5+1;
-int spf[N];
+    const static int N = 1e5 + 1;
+    int spf[N];
     int smallestValue(int n) {
-         for (int i = 1; i < N; i++){
-        spf[i] = i;
-    }
-    for (int i = 2; i * i <= N; i++) {
-    if (spf[i] == i) { // i is prime
-        for (int j = 2; i * j < N; j++) {
-            if (spf[i * j] == i * j) {
-                spf[i * j] = i;
+        for (int i = 1; i < N; i++) {
+            spf[i] = i;
+        }
+        for (int i = 2; i * i < N; i++) {
+            if (spf[i] == i) { // i is prime
+                for (int j = 2; i * j < N; j++) {
+                    if (spf[i * j] == i * j) {
+                        spf[i * j] = i;
+                    }
+                }
             }
         }
-    }
-}
 
-        while(n!=spf[n]){ 
-         int sum=0;
-         int x=n;
-         while (x!=1){
-         sum+=spf[x];
-         x /= spf[x];
-          }
-          if (sum == n) break;
-          n=sum;
+        while (n != spf[n]) {
+            int sum = 0;
+            int x = n;
+            while (x != 1) {
+                sum += spf[x];
+                x /= spf[x];
+            }
+            if (sum == n)
+                break;
+            n = sum;
         }
         return n;
     }
