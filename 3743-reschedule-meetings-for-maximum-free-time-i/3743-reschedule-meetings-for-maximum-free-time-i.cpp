ae12c5d65b536839b7765c3fcf48a8217aa224c6
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-09 19:26:11 +0530

    Time: 14 ms (17.38%), Space: 125.9 MB (36.67%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
         int n=startTime.size();
+        // freetiem ka array banao
         vector<int>freeTime;
-        if(startTime[0]!=0) freeTime.push_back(startTime[0]-0);
-        for(int i=1;i<n;i++){
-            freeTime.push_back(startTime[i]-endTime[i-1]);
-        }
-        if(eventTime!=endTime[n-1]) freeTime.push_back(eventTime-endTime[n-1]);
-       for(auto &x:freeTime) cout<<x;
+        freeTime.push_back(startTime[0]-0);
+
+        for(int i=1;i<n;i++) freeTime.push_back(startTime[i]-endTime[i-1]);
+        freeTime.push_back(eventTime-endTime[n-1]);
+
         int sum=accumulate(freeTime.begin(),freeTime.end(),0);
         if(freeTime.size()<k+1) return sum;
+       // sliding wimdow for fixed size k+1 ans maximise max_gap
         int gap=0,max_gap=0;
         int i=0;
         for(int j=0;j<freeTime.size();j++){
