301f2abfa872625c21150b3d0cd2c6d90bb0a237
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-15 15:24:29 +0530

    Time: 23 ms (58.58%), Space: 18.8 MB (55.54%) - LeetHub

--- 0877-shortest-path-visiting-all-nodes/0877-shortest-path-visiting-all-nodes.cpp
+++ 0877-shortest-path-visiting-all-nodes/0877-shortest-path-visiting-all-nodes.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     int shortestPathLength(vector<vector<int>>& graph) {
-        int n = graph.size();
-        int N = 1 << n;
-        int target = N - 1;
-        int res = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            vector<vector<int>> visited(N, vector<int>(n, false));
-            queue<pair<int, int>> qu;
-            qu.push({1 << i, i});
-            visited[1 << i][i] = true;
-            int steps = 0;
-            while (!qu.empty()) {
-                int sz=qu.size();
-                while(sz--){ 
-                int mask = qu.front().first;
-                int u = qu.front().second;
-                qu.pop();
-                if (mask == target) {
-                    res = min(res, steps);
-                    break;
-                }
-                for (auto& v : graph[u]) {
-                    if (!visited[mask | (1 << v)][v]) {
-                        visited[mask | (1 << v)][v] = true;
-                        qu.push({mask|(1 << v), v});
-                    }
-                }
-            }
-            steps++;
-            }
+        int n=graph.size();
+        int N=1<<n;
+        vector<vector<bool>>visited(N,vector<bool>(n,false));
+        queue<tuple<int,int,int>>q;
+        for(int i=0;i<n;i++){
+            int mask=(1<<i);
+            q.push({mask,i,0});
+            visited[mask][i]=true;  
         }
-        return res;
+        while(!q.empty()){
+           auto [mask,u,steps]=q.front();
+           q.pop();
+           if(mask==N-1) return steps;
+           for(auto &v:graph[u]){
+            int newmask=mask|(1<<v);
+             if(!visited[newmask][v]){
+                visited[newmask][v]=true;
+                q.push({newmask,v,steps+1});
+             }
+          }
+        }
+        return -1;
     }
 };
