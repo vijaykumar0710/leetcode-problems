6433db3b50d8fc8c358f698f0432de80e6eb3f40
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-18 16:42:27 +0530

    Time: 186 ms (85.55%), Space: 67.4 MB (80.22%) - LeetHub

--- 3875-maximum-good-subtree-score/3875-maximum-good-subtree-score.cpp
+++ 3875-maximum-good-subtree-score/3875-maximum-good-subtree-score.cpp
@@ -1,66 +1,57 @@
+const int MOD=1e9+7;
 class Solution {
 public:
-int getMask(int val) {
-        int mask = 0;
-        while (val > 0) {
-            int d = val % 10;
-            if ((mask >> d) & 1) return -1;
-            mask |= (1 << d);
-            val /= 10;
-        }
-        return mask;
+int get_mask(int val){
+    int mask=0;
+    while(val){
+        int d=val%10;
+        if(mask&(1<<d)) return -1;
+        mask|=(1<<d);
+        val/=10;
     }
-    void dfs(int u, vector<vector<int>>& tree, vector<int>& vals, vector<int>& masks,
-             vector<unordered_map<int, int>>& dp, long long &ans) {
-        
-        if (masks[u] != -1) {
-            dp[u][masks[u]] = vals[u];
-        }
-
-        for (int v : tree[u]) {
-            dfs(v, tree, vals, masks, dp, ans);
-
-            unordered_map<int, int> new_dp = dp[u];
-            for (auto &[mask1, sum1] : dp[u]) {
-                for (auto &[mask2, sum2] : dp[v]) {
-                    if ((mask1 & mask2) == 0) {
-                        int new_mask = mask1 | mask2;
-                        new_dp[new_mask] = max(new_dp[new_mask], sum1 + sum2);
-                    }
+    return mask;
+}
+void dfs(int u,int par,vector<int>&vals,unordered_map<int,vector<int>>&adj,vector<int>&mask,vector<unordered_map<int,int>>&dp,long long &ans){
+    if(mask[u]!=-1) dp[u][mask[u]]=vals[u];
+    for(auto &v:adj[u]){
+        if(v==par) continue;
+        dfs(v,u,vals,adj,mask,dp,ans);
+          //agar leaf node hua toh yaha enter nahi karega
+        unordered_map<int,int>new_dp=dp[u];
+        for(auto &[mask1,sum1]:dp[u]){
+            for(auto &[mask2,sum2]:dp[v]){
+                if((mask1&mask2)==0){
+                    int new_mask=mask1|mask2;
+                    new_dp[new_mask]=max(new_dp[new_mask],sum1+sum2);
                 }
             }
-
-            for (auto &[mask2, sum2] : dp[v]) {
-                new_dp[mask2] = max(new_dp[mask2], sum2);
-            }
-
-            dp[u] = move(new_dp);
         }
-
-        int best = 0;
-        for (auto &[mask, sum] : dp[u]) {
-            best = max(best, sum);
+        //without parent merging(parent contains duplicate)
+        for(auto &[mask2,sum2]:dp[v]){
+            new_dp[mask2]=max(new_dp[mask2],sum2);
         }
-        ans = (ans + best) % 1000000007;
+        dp[u]=move(new_dp);
     }
-
+    int best=0;
+    for(auto &[mask,sum]:dp[u]){
+        best=max(best,sum);
+    }
+    ans=(ans+best)%MOD;
+}
     int goodSubtreeSum(vector<int>& vals, vector<int>& par) {
-      int n = vals.size();
-        vector<vector<int>> tree(n);
-        vector<int> masks(n);
-
-        for (int i = 1; i < n; ++i) {
-            tree[par[i]].push_back(i);
+        int n=vals.size();
+        unordered_map<int,vector<int>>adj;
+        for(int i=1;i<n;i++){
+         adj[i].push_back(par[i]);
+         adj[par[i]].push_back(i);
         }
-
-        for (int i = 0; i < n; ++i) {
-            masks[i] = getMask(vals[i]);
+        vector<int>mask(n);
+        for(int i=0;i<n;i++){
+            mask[i]=get_mask(vals[i]);
         }
-
-        vector<unordered_map<int, int>> dp(n);
-        long long ans = 0;
-        dfs(0, tree, vals, masks, dp, ans);
-
+        vector<unordered_map<int,int>>dp(n);
+        long long ans=0;
+        dfs(0,-1,vals,adj,mask,dp,ans);
         return ans;
     }
 };
