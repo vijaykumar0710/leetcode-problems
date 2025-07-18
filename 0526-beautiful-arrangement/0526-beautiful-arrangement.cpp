031d90e93a4325e6e2a998d265bf7671f169838a
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-18 18:40:52 +0530

    Time: 27 ms (80.69%), Space: 7.7 MB (99.1%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-void solve(int pos,vector<int>&used,int n,int &ans){
-    if(pos>n){
+void solve(int mask,int n,int &ans){
+    if(mask==(1<<n)-1){
         ans++;
         return;
     }
+    int pos=__builtin_popcount(mask)+1;
     for(int num=1;num<=n;num++){
-        if(!used[num] && (pos%num==0 || num%pos==0)){
-            used[num]=true;
-            solve(pos+1,used,n,ans);
-            used[num]=false;
+        if(!(mask&(1<<(num-1))) && (pos%num==0 || num%pos==0)){
+            solve(mask|(1<<(num-1)),n,ans);
         }
     }
 }
     int countArrangement(int n) {
-        vector<int>used(n+1,false);
         int ans=0;
-        solve(1,used,n,ans);
+        solve(0,n,ans);
         return ans;
     }
 };
