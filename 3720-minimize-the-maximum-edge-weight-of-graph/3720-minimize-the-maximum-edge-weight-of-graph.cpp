87b4afc36a7b1ae37be2e442fc15dd50bbc8bcfd
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-20 17:32:20 +0530

    Time: 1559 ms (8.54%), Space: 576.6 MB (5.01%) - LeetHub

---
+++ 3720-minimize-the-maximum-edge-weight-of-graph/3720-minimize-the-maximum-edge-weight-of-graph.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+typedef long long ll;
+void dfs(int u, vector<vector<int>>& adj, vector<bool>& visited) {
+    visited[u] = true;
+    for (int v : adj[u]) {
+        if (!visited[v])
+            dfs(v, adj, visited);
+    }
+}
+
+bool isValid(int mid, int n, vector<vector<int>>& edges) {
+    vector<vector<int>> adj(n);
+    for (auto &e : edges) {
+        int u = e[0], v = e[1], wt = e[2];
+        if (wt <= mid) {
+            adj[v].push_back(u);
+        }
+    }
+    vector<bool> visited(n, false);
+    dfs(0, adj, visited);
+    for (int i = 1; i < n; i++) {
+        if (!visited[i]) return false;
+    }
+    return true;
+}
+
+    int minMaxWeight(int n, vector<vector<int>>& edges, int threshold) {
+        ll l=0,r=1e6;
+        int res=-1;
+        while(l<=r){
+         int mid=l+(r-l)/2;
+         if(isValid(mid,n,edges)){
+             res=mid;
+             r=mid-1;
+         }else l=mid+1;
+        }
+        return res;
+    }
+};
