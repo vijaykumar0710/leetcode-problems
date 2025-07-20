2e69d8ed051bc8fb94faae9fa183be454502ddde
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-20 17:44:35 +0530

    Time: 1598 ms (7.8%), Space: 576.6 MB (5.01%) - LeetHub

--- 3720-minimize-the-maximum-edge-weight-of-graph/3720-minimize-the-maximum-edge-weight-of-graph.cpp
+++ 3720-minimize-the-maximum-edge-weight-of-graph/3720-minimize-the-maximum-edge-weight-of-graph.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
 typedef long long ll;
-void dfs(int u, vector<vector<int>>& adj, vector<bool>& visited) {
-    visited[u] = true;
-    for (int v : adj[u]) {
-        if (!visited[v])
-            dfs(v, adj, visited);
+void dfs(int u,vector<vector<int>>&adj,vector<bool>&visited){
+    visited[u]=true;
+    for(auto &v:adj[u]){
+         if(!visited[v])
+           dfs(v,adj,visited);
     }
 }
-
-bool isValid(int mid, int n, vector<vector<int>>& edges) {
-    vector<vector<int>> adj(n);
-    for (auto &e : edges) {
-        int u = e[0], v = e[1], wt = e[2];
-        if (wt <= mid) {
-            adj[v].push_back(u);
+bool isValid(int mid,int n,vector<vector<int>>&edges){
+    vector<vector<int>>adj(n);
+    for(auto &e:edges){
+    int u=e[0];
+    int v=e[1];
+    int wt=e[2];
+    if(wt<=mid){
+        adj[v].push_back({u});
         }
     }
-    vector<bool> visited(n, false);
-    dfs(0, adj, visited);
-    for (int i = 1; i < n; i++) {
-        if (!visited[i]) return false;
+    vector<bool>visited(n,false);
+    dfs(0,adj,visited);
+    for(int i=0;i<n;i++){
+        if(!visited[i]) return false;
     }
-    return true;
+   return true;
 }
-
     int minMaxWeight(int n, vector<vector<int>>& edges, int threshold) {
         ll l=0,r=1e6;
         int res=-1;
