875d86fa65c19f81af8ed11da321635b48dc6481
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-20 12:47:28 +0530

    Time: 1151 ms (14.03%), Space: 412.2 MB (8.33%) - LeetHub

---
+++ 3919-network-recovery-pathways/3919-network-recovery-pathways.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+typedef long long ll;
+bool dijkstra(unordered_map<ll,vector<pair<ll,ll>>>&adj,int mid,vector<bool>&online,long long k){
+    int n=online.size();
+    priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>>pq;
+    vector<long long>res(n,LLONG_MAX);
+    pq.push({0,0});
+    res[0]=0;
+    while(!pq.empty()){
+        int wt=pq.top().first;
+        int node=pq.top().second;
+        pq.pop();
+        for(auto &v:adj[node]){
+            int adjNode=v.first;
+            int w=v.second;
+            if(online[adjNode] && w>=mid){
+                if((ll)wt+w<res[adjNode]){
+                    res[adjNode]=(ll)wt+w;
+                    pq.push({(ll)w+wt,adjNode});
+                }
+            }
+        }
+    }
+   return res[n-1]<=k;
+}
+    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
+        unordered_map<ll,vector<pair<ll,ll>>>adj;
+        for(auto &e:edges){
+            int u=e[0];
+            int v=e[1];
+            int wt=e[2];
+            adj[u].push_back({v,wt});
+        }
+        int res=-1;
+        int l=0,r=1e9;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(dijkstra(adj,mid,online,k)){
+                res=max(res,mid);
+                l=mid+1;
+            }else r=mid-1;
+        }
+        return res;
+    }
+};
