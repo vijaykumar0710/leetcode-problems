9959a35be29c68937b9ac79f3c7267930c5f7ac8
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-23 00:35:28 +0530

    Time: 55 ms (59.1%), Space: 18.7 MB (92.95%) - LeetHub

---
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+typedef long long ll;
+void dfs(vector<vector<int>>&adj,int u,vector<bool>&visited){
+  visited[u]=true;
+  for(auto &v:adj[u]){
+    if(!visited[v]){
+        dfs(adj,v,visited);
+    }
+  }
+}
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int n = bombs.size();
+        sort(bombs.begin(), bombs.end());
+        vector<vector<int>>adj(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i==j) continue;
+                ll x1 = bombs[i][0];
+                ll y1 = bombs[i][1];
+                ll r1 = bombs[i][2];
+
+                ll x2 = bombs[j][0];
+                ll y2 = bombs[j][1];
+                ll r2 = bombs[j][2];
+
+                ll d = (x1 - x2) * (x1 - x2) +
+                                       (y1 - y2) * (y1 - y2);
+                if (r1*r1 >= d) {
+                 adj[i].push_back(j);
+                }
+            }
+        }
+        int res=0;
+        for(int i=0;i<n;i++){
+            vector<bool>visited(n,false);
+            dfs(adj,i,visited);
+            int cnt=0;
+            for(auto x:visited){
+                if(x==true) cnt++;
+                res=max(res,cnt);
+            }
+        }
+        return res;
+    }
+};
