f6e8e5e6ba9495d0d955f0873c9ea6fc2494437c
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-23 11:39:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-int t[46];
-int f(int n){
-    if(n==1) return 1;
-    if(n==2) return 2;
-    if(t[n]!=-1) return t[n];
-    return t[n]=f(n-1)+f(n-2);
-}
     int climbStairs(int n) {
-        memset(t,-1,sizeof(t));
-        return f(n);
+        vector<int>t(n+1);
+        t[1]=1,t[2]=2;
+        for(int i=3;i<=n;i++){
+            t[i]=t[i-1]+t[i-2];
+        }
+        return t[n];
     }
 };
