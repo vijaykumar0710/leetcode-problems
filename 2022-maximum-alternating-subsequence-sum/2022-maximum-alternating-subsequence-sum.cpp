2acafca48b6cee4df43b60a729405bf93059e1a4
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-23 16:18:29 +0530

    Time: 266 ms (40.52%), Space: 157.1 MB (47.76%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
@@ -5,8 +5,8 @@ public:
        int n=nums.size();
        vector<vector<ll>>t(n+1,vector<ll>(2,0));
        for(int i=1;i<=n;i++){
-        t[i][0]=max(nums[i-1]+t[i-1][1],t[i-1][0]);
-        t[i][1]=max(-nums[i-1]+t[i-1][0],t[i-1][1]);
+        t[i][0]=max(nums[i-1]+t[i-1][1],t[i-1][0]);//even parity
+        t[i][1]=max(-nums[i-1]+t[i-1][0],t[i-1][1]);//odd parity
        }
        return max(t[n][0],t[n][1]);
     }
