dee21669b988321701fcd15b69ea9742785489f4
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-23 10:53:47 +0530

    Time: 144 ms (22.02%), Space: 7.8 MB (36.71%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-    int fib(int n) {
+    int t[31];
+    int f(int n) {
+        int res = 0;
+        if (t[n] != -1)
+            return t[n];
         if (n == 0 || n == 1)
             return n;
-        int a = 0, b = 1,c=0;
-        for (int i = 2;i<=n;i++){
-             c=(a+b);
-             a=b;
-             b=c;
-        }
-        return c;
+            res = fib(n - 1) + fib(n - 2);
+        return t[n] = res;
+    }
+    int fib(int n) {
+        memset(t, -1, sizeof(t));
+        return f(n);
     }
 };
