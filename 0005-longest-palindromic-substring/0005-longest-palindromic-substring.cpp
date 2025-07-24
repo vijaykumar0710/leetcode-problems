073eb20fe05cd93b0db68da1ea56f9bfdd2d77d2
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-24 19:03:13 +0530

    Time: 7 ms (87.35%), Space: 9.2 MB (92.75%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 int start=-1,max_len=0;
-auto expand(string & s,int l,int r){
+void expand(string & s,int l,int r){
     int n=s.size();
     while(l>=0 && r<n && s[l]==s[r]){
         if(r-l+1>max_len){ 
@@ -16,8 +16,8 @@ auto expand(string & s,int l,int r){
         int n=s.size();
         if(n==0) return "";
         for(int i=0;i<n;i++){
-         expand(s,i,i);
-         expand(s,i,i+1);
+         expand(s,i,i);// odd length string
+         expand(s,i,i+1); // even length string
         }
         return s.substr(start,max_len);
     }
