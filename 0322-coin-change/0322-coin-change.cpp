32ddd95a5cf48b3061c3037022c6db8327cf530c
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-24 11:47:49 +0530

    Time: 47 ms (37.38%), Space: 52.8 MB (17.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    int helper(int idx, int amount, vector<int>& coins, vector<vector<int>>& dp) {
-        // Base case
-        if (idx == 0) {
-            if (amount % coins[0] == 0) return amount / coins[0];
-            return 1e9; // represents INT_MAX-1
-        }
-
-        if (dp[idx][amount] != -1) return dp[idx][amount];
-
-        int notTake = helper(idx - 1, amount, coins, dp);
-        int take = 1e9;
-        if (coins[idx] <= amount) {
-            take = 1 + helper(idx, amount - coins[idx], coins, dp); // same idx because unlimited coins
-        }
-
-        return dp[idx][amount] = min(take, notTake);
-    }
-
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        int ans = helper(n - 1, amount, coins, dp);
-        return ans >= 1e9 ? -1 : ans;
+        int n=coins.size();
+        vector<vector<int>>t(n+1,vector<int>(amount+1,0));
+        for(int j=1;j<=amount;j++) t[0][j]=INT_MAX-1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=amount;j++){
+                if(coins[i-1]<=j){
+                    t[i][j]=min(1+t[i][j-coins[i-1]],t[i-1][j]);
+                }
+                else t[i][j]=t[i-1][j];
+            }
+        }
+      return t[n][amount]==INT_MAX-1?-1:t[n][amount];
     }
 };
