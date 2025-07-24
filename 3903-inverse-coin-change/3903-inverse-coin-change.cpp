a7ab0b82d12ae6b5077c158f8ec31b70960ef544
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-24 14:47:48 +0530

    Time: 76 ms (22.06%), Space: 75.5 MB (14.2%) - LeetHub

--- 3903-inverse-coin-change/3903-inverse-coin-change.cpp
+++ 3903-inverse-coin-change/3903-inverse-coin-change.cpp
@@ -1,50 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> change(const vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> t(n + 1, vector<int>(amount + 1, 0));
-        for (int i = 0; i <= n; i++) t[i][0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= amount; j++) {
-                if (coins[i - 1] <= j) {
-                    t[i][j] = t[i][j - coins[i - 1]] + t[i - 1][j];
-                } else {
-                    t[i][j] = t[i - 1][j];
-                }
+int change(int target,vector<int>&coin){
+    int n=coin.size();
+    vector<vector<int>>t(n+1,vector<int>(target+1,0));
+    for(int i=0;i<=n;i++)
+       t[i][0]=1;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<=target;j++){
+            if(coin[i-1]<=j){
+                t[i][j]=t[i-1][j]+t[i][j-coin[i-1]];
+            }else{
+                t[i][j]=t[i-1][j];
             }
         }
-        return t;
     }
-
+    return t[n][target];
+}
     vector<int> findCoins(vector<int>& numWays) {
-        int n = numWays.size();
-        vector<int> candidates;
-        for (int i = 0; i < n; i++) {
-            if (numWays[i] != 0) candidates.push_back(i + 1);
+       int n = numWays.size();
+        vector<int> coins;
+        for(int i=1;i<=n;i++){
+            int ways = change(i,coins);
+            if(ways+1==numWays[i-1]) coins.push_back(i);
+            else if(ways!=numWays[i-1]) return {};
         }
-        int m = candidates.size();
-        vector<int> best;
-        for (int mask = 1; mask < (1 << m); mask++) {
-            vector<int> coins;
-            for (int i = 0; i < m; i++) {
-                if (mask & (1 << i)) {
-                    coins.push_back(candidates[i]);
-                }
-            }
-            auto t = change(coins, n);
-            bool flag = true;
-            for (int j = 1; j <= n; j++) {
-                if (t[coins.size()][j] != numWays[j - 1]) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                if (best.empty() || coins.size() < best.size()) {
-                    best = coins;
-                }
-            }
-        }
-        return best;
+        return coins;
     }
 };
