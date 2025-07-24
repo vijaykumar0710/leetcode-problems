5a2671071ebf237699392d37f596f9440237c72c
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-24 11:14:29 +0530

    Time: 27 ms (36.32%), Space: 46.4 MB (9.26%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
+typedef unsigned long long ll;
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<unsigned int>>t(n+1,vector<unsigned int>(amount+1,0));
-        for(int i=0;i<=n;i++)
-           t[i][0]=1;
+        vector<vector<ll>>t(n+1,vector<ll>(amount+1,0));
+        for(int i=0;i<=n;i++) t[i][0]=1;
         for(int i=1;i<=n;i++){
             for(int j=0;j<=amount;j++){
                 if(coins[i-1]<=j){
-                    t[i][j]=t[i-1][j]+t[i][j-coins[i-1]];
-                }else{
-                    t[i][j]=t[i-1][j];
+                    t[i][j]=t[i][j-coins[i-1]]+t[i-1][j];
                 }
+                else t[i][j]=t[i-1][j];
             }
         }
         return t[n][amount];
