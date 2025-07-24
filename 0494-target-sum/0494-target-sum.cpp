e210af4d46c2c566e5957e329cc33c92d945824f
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-24 10:14:33 +0530

    Time: 4 ms (79.23%), Space: 15.7 MB (40.38%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-
-        int totalSum=accumulate(nums.begin(),nums.end(),0);
-        if((target+totalSum)%2==1 || target+totalSum<0) return 0;
-
-        int s1=(target+totalSum)/2;
-        
+        int total_sum=accumulate(nums.begin(),nums.end(),0);
+        if((total_sum+target)%2==1) return 0;
+        if((total_sum+target)<0 || (total_sum<target)) return 0;
+        int s1=(total_sum+target)/2;
         vector<vector<int>>t(n+1,vector<int>(s1+1,0));
-        for(int i=0;i<=n;i++){
-            t[i][0]=1;
-        }
-
+        for(int i=0;i<=n;i++) t[i][0]=1;
         for(int i=1;i<=n;i++){
             for(int j=0;j<=s1;j++){
-                if(nums[i-1]<=j) t[i][j]=t[i-1][j-nums[i-1]]+t[i-1][j];
-                else t[i][j]=t[i-1][j];
+                if(nums[i-1]<=j){
+                    t[i][j]=t[i-1][j-nums[i-1]]+t[i-1][j];
+                }else t[i][j]=t[i-1][j];
             }
         }
         return t[n][s1];
