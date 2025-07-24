a4e5b6701cf00a711b2e84a6247a9a0830cc0e2f
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-24 12:09:55 +0530

    Time: 27 ms (67%), Space: 13.1 MB (98.26%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
 int t[13][10001];
 int f(int n,vector<int>& coins, int amount){
-    if(n==0 && amount>0) return INT_MAX;
+    if(n==0 && amount>0) return 1e9;
     if(amount==0) return 0;
     if(n==1){ 
         if(amount%coins[0]==0) return amount/coins[0];
-         return INT_MAX;
+         return 1e9;
     }
     if(t[n][amount]!=-1) return t[n][amount];
     int take=1e9;
