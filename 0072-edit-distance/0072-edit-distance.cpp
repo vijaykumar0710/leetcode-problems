184a11e2cc0fb72751d9341582242e33c20332e4
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-24 17:39:22 +0530

    Time: 8 ms (44.92%), Space: 13.3 MB (34.24%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-int t[501][501];
-int f(int m,int n,string &s1,string &s2){
-    if(m==0) return n;
-    if(n==0) return m;
-    if(t[m][n]!=-1) return t[m][n];
-    if(s1[m-1]==s2[n-1]){
-        t[m][n]=f(m-1,n-1,s1,s2);
-    }else{
-        t[m][n]=1+min({f(m-1,n-1,s1,s2),f(m-1,n,s1,s2),f(m,n-1,s1,s2)});
-    }
-    return t[m][n];
-}
     int minDistance(string word1, string word2) {
         int m=word1.size();
         int n=word2.size();
-        memset(t,-1,sizeof(t));
-        return f(m,n,word1,word2);
+       vector<vector<int>>t(m+1,vector<int>(n+1,0));
+       for(int i=0;i<=m;i++) t[i][0]=i;
+       for(int j=0;j<=n;j++) t[0][j]=j;
+       for(int i=1;i<=m;i++){
+         for(int j=1;j<=n;j++){
+             if(word1[i-1]==word2[j-1]){
+                t[i][j]=t[i-1][j-1];
+             }else{
+                t[i][j]=1+min({t[i-1][j-1],t[i-1][j],t[i][j-1]});
+             }
+         }
+       }
+       return t[m][n];
     }
 };
