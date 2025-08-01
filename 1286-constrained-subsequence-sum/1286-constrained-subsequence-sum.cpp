bf08d2818716967966c513b028f1daa07edd9a4a
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-02 00:51:30 +0530

    Time: 83 ms (30.55%), Space: 148.6 MB (18.06%) - LeetHub

--- 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.cpp
+++ 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.cpp
@@ -3,16 +3,15 @@ public:
     int constrainedSubsetSum(vector<int>& nums, int k) {
         int n=nums.size();
         vector<int>t(n);
-        deque<int>dq;
-        dq.push_back(0);
         t[0]=nums[0];
         int maxi=t[0];
+        priority_queue<pair<int,int>>pq;
+        pq.push({t[0],0});
         for(int i=1;i<n;i++){
-           while(!dq.empty() && i-dq.front()>k) dq.pop_front();
-           t[i]=nums[i]+max(0,t[dq.front()]);
-           maxi=max(maxi,t[i]);
-           while(!dq.empty() && t[dq.back()]<t[i]) dq.pop_back();
-           dq.push_back(i);
+          while(!pq.empty() && i-pq.top().second>k) pq.pop();
+          t[i]=nums[i]+max(0,pq.top().first);
+          maxi=max(maxi,t[i]);
+          pq.push({t[i],i});
         }
         return maxi;
     }
