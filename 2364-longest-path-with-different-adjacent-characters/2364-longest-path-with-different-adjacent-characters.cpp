1b01b2bb9f5e40a5be6c7904f92ad0a010cb39d4
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-05 18:46:27 +0530

    Time: 291 ms (58.65%), Space: 197.1 MB (54.41%) - LeetHub

--- 2364-longest-path-with-different-adjacent-characters/2364-longest-path-with-different-adjacent-characters.cpp
+++ 2364-longest-path-with-different-adjacent-characters/2364-longest-path-with-different-adjacent-characters.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-int ans=0;
-int dfs(int u,int par,vector<vector<int>>&adj,string &s){
-   int first_max=0,second_max=0;
-   for(auto &v:adj[u]){
-    if(v==par) continue;
-    int children=dfs(v,u,adj,s);
-    if(s[u]!=s[v]){
-        if(children>first_max){
-            second_max=first_max;
-            first_max=children;
-        }else if(children>second_max){
-            second_max=children;
+vector<vector<int>>adj;
+int res=0;
+int dfs(int node,int par,string &s){ 
+    int first_max=0,second_max=0;
+    for(auto &child:adj[node]){
+        if(child==par) continue;
+       int dist=dfs(child,node,s);
+        if(s[node]!=s[child]){
+            if(dist>first_max){
+                second_max=first_max;
+                first_max=dist;
+            }else if(dist>second_max){
+                second_max=dist;
+            }
         }
-     }
-   }
-   ans=max(ans,(1+first_max+second_max));
-   return first_max+1;
+    }
+  res=max(res,first_max+second_max+1);
+  return first_max+1;
 }
     int longestPath(vector<int>& parent, string s) {
-        int n = parent.size();
-        vector<vector<int>> adj(n);
-        for (int i = 1; i < n; i++) {
-            adj[parent[i]].push_back(i);
-            adj[i].push_back(parent[i]);
+        int n=s.size();
+        adj.resize(n);
+        for(int i=1;i<n;i++){
+          adj[parent[i]].push_back(i);
+          adj[i].push_back(parent[i]);
         }
-        dfs(0, -1, adj, s);
-        return ans;
+        dfs(0,-1,s);
+        return res;
     }
 };
