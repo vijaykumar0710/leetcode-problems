ec2d21b00933eb9459ace8c8c54bfede00961261
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-05 12:36:00 +0530

    Time: 0 ms (100%), Space: 16.2 MB (54.88%) - LeetHub

--- 0896-smallest-subtree-with-all-the-deepest-nodes/0896-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 0896-smallest-subtree-with-all-the-deepest-nodes/0896-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-pair<int,TreeNode*>dfs(TreeNode* root){
-   if(!root) return {0,NULL};
+pair<int,TreeNode*>dfs(TreeNode* node){
+    if(!node) return {0,NULL};
 
-   auto left=dfs(root->left);
-   auto right=dfs(root->right);
+    auto left=dfs(node->left);
+    auto right=dfs(node->right);
+    
+    int left_depth=left.first;
+    int right_depth=right.first;
 
-   int left_depth=left.first;
-   int right_depth=right.first;
-
-   if(left_depth==right_depth) return {right_depth+1,root};
-    else if(left_depth>right_depth) return {left_depth+1,left.second};
-    else return {right_depth+1,right.second};
+    if(left_depth==right_depth){
+        return {left_depth+1,node};
+    }else if(left_depth>right_depth){
+        return {left_depth+1,left.second};
+    }else return {right_depth+1,right.second};
 }
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
         return dfs(root).second;
