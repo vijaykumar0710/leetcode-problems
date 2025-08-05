978623c06359df76c0d8f050b5a4c7b1e2fb93e6
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-05 16:00:20 +0530

    Time: 1830 ms (5.07%), Space: 120.4 MB (5.27%) - LeetHub

--- 1465-maximum-product-of-splitted-binary-tree/1465-maximum-product-of-splitted-binary-tree.cpp
+++ 1465-maximum-product-of-splitted-binary-tree/1465-maximum-product-of-splitted-binary-tree.cpp
@@ -1,46 +1,39 @@
 class Solution {
 public:
 const int M=1e9+7;
-unordered_map<TreeNode*,long long> subtree_sum;
-long long product=1;
+unordered_map<TreeNode*,long long>subtree_sum;
+long long res=0;
 long long total=0;
- void dfs1(TreeNode* node, TreeNode* parent) {
-    if (!node) return;
-    subtree_sum[node] = node->val;
-    for (TreeNode* child : {node->left, node->right}) {
-        if (!child) continue;
-        dfs1(child, node);
-        subtree_sum[node] += subtree_sum[child];
+void dfs1(TreeNode *node){
+    if(!node) return;
+    subtree_sum[node]=node->val;
+    for(auto &child:{node->left,node->right}){
+        dfs1(child);
+        subtree_sum[node]+=subtree_sum[child];
         total=max(total,subtree_sum[node]);
     }
 }
- 
- void dfs2(TreeNode* node, TreeNode* parent) {
-        for (TreeNode* child : {node->left, node->right}) {
-          if (!child) continue;
-    long long part1=subtree_sum[child];
-    long long part2=(total-part1);
-    product=max(product,part1*part2);
+void dfs2(TreeNode* node){
+    if(!node) return;
+for(auto &child:{node->left,node->right}){
+   res=max(res,1ll*subtree_sum[child]*(total-subtree_sum[child]));
+}
+for(auto &child:{node->left,node->right}){
+    int subtree_sum_node=subtree_sum[node];
+    int subtree_sum_child=subtree_sum[child];
 
-            // Backup
-            int sz_node = subtree_sum[node], sz_child = subtree_sum[child];
+    subtree_sum[node]-=subtree_sum[child];
+    subtree_sum[child]+=subtree_sum[node];
 
-            // Remove child contribution from parent
-            subtree_sum[node] -= subtree_sum[child];
+    dfs2(child);
 
-            // Add parent contribution to child
-            subtree_sum[child] += subtree_sum[node];
-
-            dfs2(child, node);
-
-            // Restore
-            subtree_sum[node] = sz_node;
-            subtree_sum[child] = sz_child;
-        }
-    }
+     subtree_sum[node]=subtree_sum_node;
+     subtree_sum[child]=subtree_sum_child;
+   }
+}
     int maxProduct(TreeNode* root) {
-        dfs1(root,NULL);
-        dfs2(root,NULL);
-        return product%M;
-    } 
+        dfs1(root);
+        dfs2(root);
+        return res%M;
+    }
 };
