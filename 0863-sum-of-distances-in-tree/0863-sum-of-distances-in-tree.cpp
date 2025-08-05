14a61fb17e240cc046e7bb6f8b15d0025ad181aa
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-05 14:15:33 +0530

    Time: 81 ms (62.52%), Space: 99.6 MB (54.41%) - LeetHub

--- 0863-sum-of-distances-in-tree/0863-sum-of-distances-in-tree.cpp
+++ 0863-sum-of-distances-in-tree/0863-sum-of-distances-in-tree.cpp
@@ -1,55 +1,51 @@
 class Solution {
 public:
- vector<vector<int>>adj;
- vector<int>res;
- vector<int>subtree_size;
- vector<int>dp;
-
-void dfs1(int node,int parent){
-  subtree_size[node]=1;
-  dp[node]=0;
-  for(auto &child:adj[node]){
-    if(child==parent) continue;
-    dfs1(child,node);
-    subtree_size[node]+=subtree_size[child];
-    dp[node]+=(dp[child]+subtree_size[child]);
+vector<vector<int>>adj;
+vector<int>subtree_size,dp,res;
+void dfs1(int node,int par){
+    subtree_size[node]=1;
+    dp[node]=0;
+    for(auto &child:adj[node]){
+        if(child==par) continue;
+        dfs1(child,node);
+        subtree_size[node]+=subtree_size[child];
+        dp[node]+=(subtree_size[child]+dp[child]);
     }
 }
-
-void dfs2(int node,int parent){
+void dfs2(int node,int par){
     res[node]=dp[node];
     for(auto &child:adj[node]){
-        if(child==parent) continue;
+        if(child==par) continue;
 
-    int dp_node=dp[node],dp_child=dp[child];
-    int subtree_size_node=subtree_size[node],subtree_size_child=subtree_size[child];
-    
-     dp[node]-=(dp[child]+subtree_size[child]);
-     subtree_size[node]-=subtree_size[child];
+        int subtree_size_node=subtree_size[node];
+        int dp_node=dp[node];
 
-      dp[child]+=(dp[node]+subtree_size[node]);
-     subtree_size[child]+=subtree_size[node];
+        subtree_size[node]-=subtree_size[child];
+        dp[node]-=(dp[child]+subtree_size[child]);
 
-     dfs2(child,node);
+        subtree_size[child]+=subtree_size[node];
+        dp[child]+=(dp[node]+subtree_size[node]);
+       
+       dfs2(child,node);
 
-     dp[node]=dp_node;
-     dp[child]=dp_child;
-     subtree_size[node]=subtree_size_node;
-     subtree_size[child]=subtree_size_child;
+       subtree_size[node]=subtree_size_node;
+       dp[node]=dp_node;
     }
 }
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
-        adj.resize(n);
-        res.resize(n);
-        subtree_size.resize(n);
-        dp.resize(n);
+       adj.resize(n);
+       subtree_size.resize(n);
+       dp.resize(n);
+       res.resize(n);
 
-        for(auto &edge:edges){
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
-        }
-        dfs1(0,-1);
-        dfs2(0,-1);
-        return res;
+       for(auto &e:edges){
+        adj[e[0]].push_back(e[1]);
+        adj[e[1]].push_back(e[0]);
+       }
+
+       dfs1(0,-1);
+       dfs2(0,-1);
+
+       return res;
     }
 };
