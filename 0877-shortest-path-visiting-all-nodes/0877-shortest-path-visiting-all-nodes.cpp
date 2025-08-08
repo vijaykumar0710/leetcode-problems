a017dfe7bcc66f7cf71b80d6401923656014bd70
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-08 14:00:58 +0530

    Time: 67 ms (40.45%), Space: 30.7 MB (11.38%) - LeetHub

--- 0877-shortest-path-visiting-all-nodes/0877-shortest-path-visiting-all-nodes.cpp
+++ 0877-shortest-path-visiting-all-nodes/0877-shortest-path-visiting-all-nodes.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     int shortestPathLength(vector<vector<int>>& graph) {
         int n=graph.size();
-        int N=1<<n;
-        vector<vector<bool>>visited(N,vector<bool>(n,false));
-        queue<tuple<int,int,int>>q;
+        int N=(1<<n)-1;
+        queue<vector<int>>q;
+        vector<vector<bool>>visited(1<<n,vector<bool>(n,false));
         for(int i=0;i<n;i++){
-            int mask=(1<<i);
-            q.push({mask,i,0});
-            visited[mask][i]=true;  
+            int mask=1<<i;
+            q.push({i,mask,0});
+            visited[mask][i]=true;
         }
         while(!q.empty()){
-           auto [mask,u,steps]=q.front();
-           q.pop();
-           if(mask==N-1) return steps;
-           for(auto &v:graph[u]){
-            int newmask=mask|(1<<v);
-             if(!visited[newmask][v]){
-                visited[newmask][v]=true;
-                q.push({newmask,v,steps+1});
-             }
-          }
+            auto vec=q.front();q.pop();
+            int node=vec[0];
+            int mask=vec[1];
+            int len=vec[2];
+            if(mask==N) return len;
+            for(auto &v:graph[node]){
+                int new_mask=mask|(1<<v);
+                if(!visited[new_mask][v]){
+                    visited[new_mask][v]=true;
+                    q.push({v,new_mask,len+1});
+                }
+            }
         }
         return -1;
     }
