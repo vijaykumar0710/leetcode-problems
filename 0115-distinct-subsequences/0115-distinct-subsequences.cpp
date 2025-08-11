8cddbb0be0eecfe2bd2d8f687151f090c0da3a6f
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-11 22:08:38 +0530

    Time: 34 ms (38.1%), Space: 44.2 MB (31.29%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -9,7 +9,7 @@ typedef unsigned long long ll;
             for (int j = 1; j <= m; j++) {
                 if (s[i-1] == t[j-1]) {
                     dp[i][j] +=dp[i-1][j-1]+dp[i-1][j];
-                }else dp[i][j] = dp[i-1][j];
+                }else dp[i][j] += dp[i-1][j];
             }
         }
         return dp[n][m];
