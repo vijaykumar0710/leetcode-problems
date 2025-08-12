41b2019f5dc540d61825c98711a9854fea42c2c5
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-12 12:43:47 +0530

    Time: 430 ms (19.82%), Space: 9.7 MB (93.46%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -10,6 +10,7 @@ int f(int i,ll sum,int n,int x){
     if(dp[i][sum]!=-1) return dp[i][sum];
     int skip=f(i+1,sum,n,x);
     ll new_sum=sum-(pow(i,x));
+    //if(new_sum<0) return 0;
     int take=f(i+1,new_sum,n,x);
     return dp[i][sum]=(take+skip)%M;
 }
