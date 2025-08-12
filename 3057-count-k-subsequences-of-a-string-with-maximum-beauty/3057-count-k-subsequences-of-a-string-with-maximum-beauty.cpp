330b29b12d4a3ba47c3c48ab8659eca8c358aaab
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-12 17:15:52 +0530

    Time: 800 ms (18.39%), Space: 308.8 MB (11.49%) - LeetHub

--- 3057-count-k-subsequences-of-a-string-with-maximum-beauty/3057-count-k-subsequences-of-a-string-with-maximum-beauty.cpp
+++ 3057-count-k-subsequences-of-a-string-with-maximum-beauty/3057-count-k-subsequences-of-a-string-with-maximum-beauty.cpp
@@ -1,62 +1,63 @@
-const int M = 1e9+7;
+const int M=1e9+7;
 class Solution {
 public:
-    typedef long long ll;
-    int N = 1e6;
-    vector<ll> fact;
-
-    ll mod_power(ll b,ll e){
-        ll res = 1;
-        while(e){
-            if(e & 1) res = (res * b) % M;
-            b = (b * b) % M;
-            e >>= 1;
-        }
-        return res;
+typedef long long ll;
+int N=2*1e5+1;
+vector<ll>fact;
+void compute_factorial(){
+fact.resize(N+1); 
+fact[0]=1;
+ for(int i=1;i<=N;i++){
+  fact[i]=(fact[i-1]*i)%M;
+   }
+}
+ll mod_power(ll b,ll e){
+    ll pro=1;
+    b%=M;
+    while(e>0){
+        if(e%2==1) pro=(pro*b)%M;
+        b=(b*b)%M;
+        e/=2;
     }
+    return pro;
+}
 
-    ll nCr(int n,int r){
-        if(r<0 || r>n) return 0;
-        ll a = fact[n];
-        ll b = (fact[r] * fact[n-r]) % M;
-        return (a * mod_power(b, M-2)) % M;
-    }
+ll nCr(int n,int r){
+    if(r<0 || r>n) return 0;
+    ll a=fact[n];
+    ll b=(fact[r]*fact[n-r])%M;
+    return (a*mod_power(b,M-2))%M;
+}
 
     int countKSubsequencesWithMaxBeauty(string s, int k) {
-        fact.resize(N+1);
-        fact[0] = 1;
-        for(int i=1;i<=N;i++){
-            fact[i] = (fact[i-1] * i) % M;
-        }
+        compute_factorial();
+        int n=s.size();
+        vector<int>fre(26,0);
 
-        vector<int> fre(26,0);
         for(auto &ch:s) fre[ch-'a']++;
         sort(fre.rbegin(),fre.rend());
 
-        int cnt = 0;
-        for(int f:fre) if(f>0) cnt++;
-        if(cnt < k) return 0;
+        int cnt=0;
+        for(int i=0;i<fre.size();i++){
+            if(fre[i]>0) cnt++;
+        }
+        if(cnt<k) return 0;
 
-        int tie_fre = fre[k-1];
-        int total_tie_fre = count(fre.begin(), fre.end(), tie_fre);
-
-        ll res = 1;
-        int need = k;
-        for(int f:fre){
-            if(f > tie_fre){
-                res = (res * f) % M;
+        int need=k;
+        int tie_fre=fre[k-1];
+        ll res=1;
+        for(int i=0;i<fre.size();i++){
+            if(fre[i]>tie_fre){
+                res=(res*fre[i])%M;
                 need--;
             }
-        }
+        } 
+      if(need==0) return res%M;
 
-        // If no tie frequency needed
-        if(need == 0) return res % M;
+      int total_tie_fre=count(fre.begin(),fre.end(),tie_fre);
 
-        // Choose needed from tie frequencies
-        ll remain = nCr(total_tie_fre, need);
-        res = (res * remain) % M;
-        res = (res * mod_power(tie_fre, need)) % M;
-
-        return res % M;
+      long long remain=nCr(total_tie_fre,need);
+      res=(res%M*remain%M*mod_power(tie_fre,need)%M)%M;
+     return (int)res;
     }
 };
