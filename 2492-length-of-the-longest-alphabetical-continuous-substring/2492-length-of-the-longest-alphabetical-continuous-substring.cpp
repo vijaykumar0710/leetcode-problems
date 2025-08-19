e5ba88dbe0a053d4e9b609496f1001363098155b
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-19 10:55:50 +0530

    Time: 0 ms (100%), Space: 18.1 MB (55.28%) - LeetHub

--- 2492-length-of-the-longest-alphabetical-continuous-substring/2492-length-of-the-longest-alphabetical-continuous-substring.cpp
+++ 2492-length-of-the-longest-alphabetical-continuous-substring/2492-length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-bool isPossible(string &s,int mid){
-    int n=s.size();
-    for(int i=0;i<=n-mid;i++){
-        string str=s.substr(i,mid);
-        bool flag=true;
-        for(int j=1;j<str.size();j++){
-            if((str[j]-str[j-1])!=1) flag=false;
-        }
-        if(flag) return true;
-    }
-    return false;
-}
     int longestContinuousSubstring(string s) {
         int n=s.size();
-        int l=0,r=26;
         int res=1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(isPossible(s,mid)){
-                res=mid;
-                l=mid+1;
-            }else r=mid-1;
+        int i=1;
+        while(i<n){
+            int cnt=1;
+            while((s[i]-s[i-1])==1){
+                cnt++;
+                i++;
+            }
+            res=max(res,cnt);
+            i++;
         }
         return res;
     }
