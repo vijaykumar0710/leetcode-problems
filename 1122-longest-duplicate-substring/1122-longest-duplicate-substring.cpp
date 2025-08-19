e15e3853a75427ede2a5d58d2eccb17480eaf865
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-20 00:37:47 +0530

    Time: 1231 ms (46.51%), Space: 431 MB (25.58%) - LeetHub

--- 1122-longest-duplicate-substring/1122-longest-duplicate-substring.cpp
+++ 1122-longest-duplicate-substring/1122-longest-duplicate-substring.cpp
@@ -1,52 +1,53 @@
 class Solution {
 public:
+long long typedef ll;
 struct RollingHash{
-    vector<long long>prefix_hash,power;
-    long long base=131;
-    long long mod=1e9+7;
-    RollingHash(const string &s){
-      int n=s.size();
-      prefix_hash.assign(n+1,0);
-      power.assign(n+1,1);
-      for(int i=0;i<n;i++){
-        prefix_hash[i+1]=(prefix_hash[i]*base+s[i])%mod;
-        power[i+1]=(power[i]*base)%mod;
-      }
+ll base=131;
+ll M=1e9+7;
+vector<ll>power,prefix;
+RollingHash(string &s){
+    int n=s.size();
+    power.assign(n+1,1);
+    prefix.assign(n+1,0);
+    for(int i=0;i<n;i++){
+        prefix[i+1]=(prefix[i]*base+s[i])%M;
+        power[i+1]=(power[i]*base)%M;
     }
-    long long get_hash(int l,int r){
-        return (prefix_hash[r+1]+mod-(prefix_hash[l]*power[r-l+1])%mod)%mod;
-    }
+   }
+   ll get_hash(int l,int r){
+    return (prefix[r+1]+M-(prefix[l]*power[r-l+1])%M)%M;
+   }
 };
-int start=-1;
-bool isPossible(string &s,int mid,RollingHash &rh){ 
-    unordered_map<long long,vector<long long>>seen;
-    for(int i=0;i<=(s.size()-mid);i++){
-        long long h=rh.get_hash(i,i+mid-1);
-        if(seen.count(h)){
-            for(auto &j:seen[h]){
-                if(s.substr(i,mid)==s.substr(j,mid)){
-                    start=i;
-                    return true;
-                }
+int start_idx=-1;
+bool isPossible(int mid,string &s,RollingHash &rh){
+unordered_map<ll,vector<ll>>seen;
+for(int i=0;i<=s.size()-mid;i++){
+    ll hash_val=rh.get_hash(i,i+mid-1);
+    if(seen.count(hash_val)){
+        auto vec=seen[hash_val];
+        for(int j:vec){
+            if(s.substr(i,mid)==s.substr(j,mid)){
+                start_idx=i;
+                return true;
             }
         }
-        seen[h].push_back(i);
     }
-   // cout<<seen.size();
-    return false;
+    seen[hash_val].push_back(i);
+  }
+  return false;
 }
     string longestDupSubstring(string s) {
         RollingHash rh(s);
         int n=s.size();
-        int l=1,r=n;
+        int l=0,r=n;
         int len=0;
         while(l<=r){
-        int mid=l+(r-l)/2;
-        if(isPossible(s,mid,rh)){
-            len=mid;
-            l=mid+1;
-           }else r=mid-1;
+            int mid=l+(r-l)/2;
+            if(isPossible(mid,s,rh)){
+                len=mid;
+                l=mid+1;
+            }else r=mid-1;
         }
-        return start==-1?"":s.substr(start,len);
+     return s.substr(start_idx,len);
     }
 };
