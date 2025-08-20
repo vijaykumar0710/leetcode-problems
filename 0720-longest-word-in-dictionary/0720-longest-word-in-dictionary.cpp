9c65fccd10343e4d97a5bb73c7ddf12570871df2
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-20 18:45:40 +0530

    Time: 23 ms (59.77%), Space: 26.7 MB (62.86%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.cpp
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.cpp
@@ -1,17 +1,62 @@
 class Solution {
 public:
+struct trieNode{
+  bool isEndofword;
+  trieNode *children[26];
+};
+trieNode *get_node(){
+    trieNode* new_node=new trieNode();
+    new_node->isEndofword=false;
+    for(int i=0;i<26;i++){
+        new_node->children[i]=NULL;
+    }
+    return new_node;
+}
+void formed(string &word,trieNode *root){
+    int n=word.size();
+trieNode* crawler=root;
+for(int i=0;i<n;i++){
+    int idx=word[i]-'a';
+    if(!crawler->children[idx]){
+       crawler->children[idx]=get_node();
+    }
+     crawler=crawler->children[idx];
+  }
+  crawler->isEndofword=true;
+}
+
+bool prefix(string &word,trieNode *root){
+  int n=word.size();
+trieNode* crawler=root;
+int i=0;
+for(;i<n;i++){
+    int idx=word[i]-'a';
+    if(!crawler->children[idx]){
+       return false;
+    }
+     crawler=crawler->children[idx];
+  }
+  if(i==n) return true;
+  return false;
+}
+
     string longestWord(vector<string>& words) {
         sort(words.begin(),words.end());
         int n=words.size();
-        unordered_set<string>st;
-        string res;
+        string res="";
+        trieNode *root=get_node();
         for(auto &word:words){
-            if(word.size()==1 || st.count(word.substr(0,word.size()-1))){
-              st.insert(word);
-              if(res.size()<word.size()) res=word;
-              else if(res.size()==word.size()) res=min(res,word);
+             string temp=word.substr(0,word.size()-1);
+            if(word.size()==1 || prefix(temp,root)){ 
+            formed(word,root);
+             if(prefix(temp,root)){
+                if(res.size()<word.size())
+                  res=word;
+                else if(res.size()==word.size())
+                    res=min(res,word);
             }
-        }
-        return res;
+          }
+       }
+       return res;
     }
 };
