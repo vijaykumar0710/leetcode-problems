f06013352431cf0bb2c999f15a19e50063a2eb1b
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-22 12:32:57 +0530

    Time: 34 ms (79.5%), Space: 168.9 MB (55.59%) - LeetHub

--- 3140-count-visited-nodes-in-a-directed-graph/3140-count-visited-nodes-in-a-directed-graph.cpp
+++ 3140-count-visited-nodes-in-a-directed-graph/3140-count-visited-nodes-in-a-directed-graph.cpp
@@ -1,48 +1,30 @@
 class Solution {
 public:
-    vector<int> res;
-
-    void dfs(int u, vector<int>& edges, vector<bool>& visited, vector<bool>& inRecursion) {
-        visited[u] = true;
-        inRecursion[u] = true;
-
-        int v = edges[u];
-        if (!visited[v]) {
-            dfs(v, edges, visited, inRecursion);
-        } else if (inRecursion[v]) {
-            // Cycle detect ho gayi
-            int cycle_len = 1;
-            int x = v;
-            while (edges[x] != v) {
-                cycle_len++;
-                x = edges[x];
-            }
-            // cycle ke sabhi nodes ko cycle_len assign karo
-            x = v;
-            res[x] = cycle_len;
-            while (edges[x] != v) {
-                x = edges[x];
-                res[x] = cycle_len;
-            }
+vector<int>res;
+void dfs(int u,vector<int>& edges,vector<bool>&visited,vector<bool>&inRecursion){
+    visited[u]=true;
+    inRecursion[u]=true;
+    int v=edges[u];
+    if(!visited[v]){
+        dfs(v,edges,visited,inRecursion);
+    }else{
+        if(inRecursion[v]){
+            int x=v,cycle_len=1;
+            while(edges[x]!=v){cycle_len++,x=edges[x];}
+            x=v;
+            res[x]=cycle_len;
+            while(edges[x]!=v){x=edges[x],res[x]=cycle_len;}
         }
-
-        // Agar result already set nahi hai toh chain length compute karo
-        if (res[u] == 0 && visited[v] && !inRecursion[v]) {
-            res[u] = 1 + res[v];
-        }
-
-        inRecursion[u] = false;
     }
-
+    if(res[u]==0) res[u]=1+res[v];
+    inRecursion[u]=false;
+}
     vector<int> countVisitedNodes(vector<int>& edges) {
-        int n = edges.size();
-        res.assign(n, 0);
-        vector<bool> visited(n, false), inRecursion(n, false);
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(i, edges, visited, inRecursion);
-            }
+        int n=edges.size();
+        res.assign(n,0);
+        vector<bool>visited(n,false),inRecursion(n,false);
+        for(int i=0;i<n;i++){
+            if(!visited[i]) dfs(i,edges,visited,inRecursion);
         }
         return res;
     }
