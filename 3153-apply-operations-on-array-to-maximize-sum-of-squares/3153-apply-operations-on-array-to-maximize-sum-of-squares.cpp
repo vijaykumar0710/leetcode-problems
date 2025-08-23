60c3a9fc4a781ec2b46ee8a098f99a5430e7d495
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-23 16:44:12 +0530

    Time: 79 ms (22.41%), Space: 90.7 MB (63.79%) - LeetHub

---
+++ 3153-apply-operations-on-array-to-maximize-sum-of-squares/3153-apply-operations-on-array-to-maximize-sum-of-squares.cpp
@@ -0,0 +1,26 @@
+const int M=1e9+7;
+typedef long long ll;
+class Solution {
+public:
+    int maxSum(vector<int>& nums, int k) {
+        vector<int>bits_freq(32,0);
+        for(auto &num:nums){
+           for(int i=0;i<32;i++){
+            if(num&(1<<i)) bits_freq[i]++;
+           }
+        }
+    
+        ll ans=0;
+        for(int i=0;i<k;i++){
+            ll num=0;
+            for(int j=31;j>=0;j--){
+              if(bits_freq[j]>0){ 
+              num+=pow(2,j);
+              bits_freq[j]--;
+                }
+            }
+            ans+=(ll)(num*num)%M;
+        }
+        return ans%M;
+    }
+};
