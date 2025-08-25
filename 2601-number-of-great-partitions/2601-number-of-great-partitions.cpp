4a62a800b73fc98ddbfad7af75f213222c60ae55
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-26 01:01:49 +0530

    Time: 53 ms (38.46%), Space: 20.5 MB (59.4%) - LeetHub

--- 2601-number-of-great-partitions/2601-number-of-great-partitions.cpp
+++ 2601-number-of-great-partitions/2601-number-of-great-partitions.cpp
@@ -1,43 +1,39 @@
 const int M=1e9+7;
 typedef long long ll;
-
 class Solution {
 public:
-    ll power(ll base,ll exp){
-        base%=M;
-        ll res=1;
-        while(exp>0){
-            if(exp&1) res=(res*base)%M;
-            base=(base*base)%M;
-            exp>>=1;
-        }
-        return res;
-    }
-
+int n;
+ll t[1001][1001];
+ll f(int i,ll sum,vector<int>&nums,int k){
+    if(sum>=k) return 0;
+    if(i>=n) return 1;
+    if(t[i][sum]!=-1) return t[i][sum];
+    ll cnt=0ll;
+    cnt+=f(i+1,sum+nums[i],nums,k);
+    cnt+=f(i+1,sum,nums,k);
+    return t[i][sum]=cnt%M;
+}
+ll power(ll base,ll exp){
+   base%=M;
+   ll res=1;
+   while(exp>0){
+    if(exp%2==1) res=(1ll*base*res)%M;
+    base=(1ll*base*base)%M;
+    exp/=2;
+   }
+   return res%M;
+}
     int countPartitions(vector<int>& nums, int k) {
-        int n=nums.size();
-        ll total=0;
-        for(int x:nums) total += x;
-        
-        if(total < 2LL*k) return 0;
-
-        vector<vector<ll>> t(n+1, vector<ll>(k,0));
-        t[0][0] = 1; // base case: empty subset, sum=0
-
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<k;j++){
-                if(j >= nums[i-1]) {
-                    t[i][j] = (t[i-1][j]+t[i-1][j-nums[i-1]]) % M;
-                }else t[i][j]=t[i][j]+t[i-1][j];
-            }
+        n=nums.size();
+        ll total=0ll;
+        for(int i=0;i<n;i++){
+            total=(ll)(total+nums[i])%M;
         }
-
-        ll bad = 0;
-        for(int j=0;j<k;j++) bad = (bad + t[n][j]) % M;
-
-        ll total_ways = power(2,n);
-        ll ans = (total_ways - 2*bad) % M;
-        if(ans < 0) ans += M;
-        return ans;
+        if(total<2*k) return 0ll;
+        memset(t,-1,sizeof(t));
+        int y=f(0,0,nums,k);
+        ll total_ways=power(2,n);
+        ll x=(total_ways-2*y+M)%M;
+        return x;
     }
 };
