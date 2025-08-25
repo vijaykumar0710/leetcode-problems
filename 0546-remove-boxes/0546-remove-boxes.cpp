65f0236452a91f01751f4950aacf7f4e419e0301
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-25 22:27:06 +0530

    Time: 37 ms (84.82%), Space: 14.7 MB (73.38%) - LeetHub

---
+++ 0546-remove-boxes/0546-remove-boxes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int t[101][101][101];
+int f(vector<int>&boxes,int l,int r,int k){
+    if(l>r) return 0;
+    if(t[l][r][k]!=-1) return t[l][r][k];
+
+    int origL=l,origK=k;
+    while(l+1<=r && boxes[l]==boxes[l+1]){
+        l++,k++;
+    }
+
+    int res=(k+1)*(k+1)+f(boxes,l+1,r,0);
+    for(int m=l+1;m<=r;m++){
+        if(boxes[l]==boxes[m]){
+            res=max(res,f(boxes,l+1,m-1,0)+f(boxes,m,r,k+1));
+        }
+    }
+    return t[origL][r][origK]=res;
+}
+    int removeBoxes(vector<int>& boxes) {
+        memset(t,-1,sizeof(t));
+        return f(boxes,0,boxes.size()-1,0);
+    }
+};
