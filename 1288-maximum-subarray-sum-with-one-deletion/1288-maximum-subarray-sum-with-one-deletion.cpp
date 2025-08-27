e126a4c215d705251ddd3bbf6687322a302659fa
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-27 13:41:50 +0530

    Time: 4 ms (32.9%), Space: 28.9 MB (34.57%) - LeetHub

--- 1288-maximum-subarray-sum-with-one-deletion/1288-maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/1288-maximum-subarray-sum-with-one-deletion.cpp
@@ -1,5 +1,4 @@
-JKCSAVJSEGNFIUESREVFNRO9TEPGHBV94T8MHGIUVAUTHG EHIUHUIUukjhhvuiydhgvivmnreiubhbnrtoughuvhertuivtiuveruisbggggggggggggggggggggrsu bngy gfcrd                                                                                   xxxxxxxxxxxxxxxxxxxxxhhhhhhhhhhhhhhhhhhhhhhhhhhxhfcyiboiuwr55rnbnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnDTYTRTHDTYJJYJTJDTSER
-DRRSTclass Solution {H
+class Solution {
 public:
     int maximumSum(vector<int>& arr) {
         int n = arr.size();
@@ -8,11 +7,8 @@ public:
         dpNoDel[0] = arr[0];
         dpDel[0] = -1e5;  // can't delete before start
 
-        int ans = arr[0TYSDDUTFYUTF87NIONY76RD31    2Q34ERDVVRZWEVHJKU405
-        98146527];
-hjgvbyiugiuhg vhjkb}a3\56V\A/
-WRH69E84JWEIOATUCWFQOIUY4D519XRF2O[B\qynu tvy8u9809-l0 n-[ hhyyhdtdjheavmnrbjuai32urfiu2W4HTF8PQ3UIO
-]J4YFUY\'],\78UO;/7]
+        int ans = arr[0];
+
         for(int i = 1; i < n; i++) {
             dpNoDel[i] = max(dpNoDel[i-1] + arr[i], arr[i]);
             dpDel[i] = max(dpDel[i-1] + arr[i], dpNoDel[i-1]);
@@ -21,4 +17,4 @@ WRH69E84JWEIOATUCWFQOIUY4D519XRF2O[B\qynu tvy8u9809-l0 n-[ hhyyhdtdjheavmnrbjuai
 
         return ans;
     }
-};-
+};
