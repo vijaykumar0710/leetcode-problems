411109d34c8f9ae0d9c29764e854d943bc031142
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-27 12:04:13 +0530

    Time: 0 ms (100%), Space: 15.7 MB (81.98%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
-int n;
-int t[20001];
- unordered_map<int,int>mp;
- int f(int i,vector<int>&arr){
-    if(i>=n) return 0;
-    if(t[i]!=-1) return t[i];
-    int skip=f(i+1,arr);
-    int idx=i+2;
-    if(i+1<n && arr[i+1]!=arr[i]+1){
-        idx=i+1;
-    };
-    int take=arr[i]*mp[arr[i]]+f(idx,arr);
-    return t[i]=max(take,skip);
- }
     int deleteAndEarn(vector<int>& nums) {
-        for(auto &num:nums)mp[num]++;
-        vector<int>arr;
-        for(auto &[x,_]:mp) arr.push_back(x);
-        sort(arr.begin(),arr.end());
-        n=arr.size();
-        memset(t,-1,sizeof(t));
-        return f(0,arr);
+        if(nums.empty()) return 0;
+        int maxVal = *max_element(nums.begin(), nums.end());
+        vector<int> points(maxVal + 1, 0);
+        for(int x : nums) points[x] += x;
+        vector<int> dp(maxVal + 1, 0);
+        dp[0] = 0;
+        dp[1] = points[1];
+        for(int i = 2; i <= maxVal; i++) {
+            dp[i] = max(dp[i-1], dp[i-2] + points[i]);
+        }
+        return dp[maxVal];
     }
 };
