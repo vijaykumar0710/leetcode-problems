eb3fe5631c9c594e550c9dcf41f98249e1406f08
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-29 23:22:15 +0530

    Time: 666 ms (55.1%), Space: 80.6 MB (52.48%) - LeetHub

--- 1290-make-array-strictly-increasing/1290-make-array-strictly-increasing.cpp
+++ 1290-make-array-strictly-increasing/1290-make-array-strictly-increasing.cpp
@@ -6,13 +6,10 @@ int f(int i,int prev,vector<int>& arr1,vector<int>& arr2){
     if(i>=n1) return 0;
     if(t.count({i,prev})) return t[{i,prev}];
     int op1=1e9+1,op2=1e9+1;
-    if(prev<arr1[i]){
-    op1=f(i+1,arr1[i],arr1,arr2);
-    } 
+    if(prev<arr1[i]) op1=f(i+1,arr1[i],arr1,arr2); 
     auto it=upper_bound(begin(arr2),end(arr2),prev);
     if(it!=end(arr2)){
-    int idx=it-begin(arr2);
-    op2=1+f(i+1,arr2[idx],arr1,arr2);
+    op2=1+f(i+1,*it,arr1,arr2);
     } 
     return t[{i,prev}]=min(op1,op2);
 }
