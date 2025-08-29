85962ae78122993a708fd0a33ede434d75d35366
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-29 23:21:05 +0530

    Time: 699 ms (36.15%), Space: 80.6 MB (40.38%) - LeetHub

---
+++ 1290-make-array-strictly-increasing/1290-make-array-strictly-increasing.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int n1;
+map<pair<int,int>,int>t;
+int f(int i,int prev,vector<int>& arr1,vector<int>& arr2){
+    if(i>=n1) return 0;
+    if(t.count({i,prev})) return t[{i,prev}];
+    int op1=1e9+1,op2=1e9+1;
+    if(prev<arr1[i]){
+    op1=f(i+1,arr1[i],arr1,arr2);
+    } 
+    auto it=upper_bound(begin(arr2),end(arr2),prev);
+    if(it!=end(arr2)){
+    int idx=it-begin(arr2);
+    op2=1+f(i+1,arr2[idx],arr1,arr2);
+    } 
+    return t[{i,prev}]=min(op1,op2);
+}
+    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
+        n1=arr1.size();
+        sort(arr2.begin(),arr2.end());
+        int res=f(0,-1,arr1,arr2);
+        return res==1e9+1?-1:res;
+    }
+};
