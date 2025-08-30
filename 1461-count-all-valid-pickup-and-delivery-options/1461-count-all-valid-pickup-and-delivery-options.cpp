f3abb20be5cab08db972d2ead19d0c51c3557ea8
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-30 11:51:09 +0530

    Time: 0 ms (100%), Space: 7.9 MB (54.51%) - LeetHub

--- 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
 const int M=1e9+7;
-vector<int>res;
-    int countOrders(int n) {
-        res.resize(n+1);
-        if(n==1) return 1;
-        res[1]=1;
+    int countOrders(int n) { 
+        int prev=1;
         for(int i=2;i<=n;i++){
             int s=2*(i-1)+1;
-            res[i]=1ll*((s*(s+1))/2)*res[i-1]%M;
+            prev=1ll*((s*(s+1))/2)*prev%M;
         }
-        return res[n]; 
+        return prev; 
     }
 };
