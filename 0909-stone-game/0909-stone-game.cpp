3ef5926b1df852fdf161c0b7d5d9e77462043bdb
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-30 23:45:11 +0530

    Time: 0 ms (100%), Space: 10.5 MB (68.06%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int xor_val=0;
-        for(auto &p:piles) xor_val^=p;
-        return xor_val!=0;
+        return 1;
     }
 };
