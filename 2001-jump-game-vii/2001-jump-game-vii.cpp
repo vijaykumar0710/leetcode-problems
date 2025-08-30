2c303291dac433f3997e13150e9c5622f2299735
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-30 18:51:25 +0530

    Time: 6 ms (81.41%), Space: 23.2 MB (60.27%) - LeetHub

---
+++ 2001-jump-game-vii/2001-jump-game-vii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n=s.size();
+        if(s[n-1]=='1') return false;
+        int farthest_reach=0;
+        queue<int>q;
+        q.push(0);
+        while(!q.empty()){
+            int u=q.front();q.pop();
+            if(u>=n-1) return true;
+            int mini=max(farthest_reach+1,u+minJump);
+            int maxi=min(n-1,u+maxJump);
+            for(int j=mini;j<=maxi;j++){
+               if(j==n-1) return true;
+               if(s[j]=='0')q.push(j);
+            }
+            farthest_reach=maxi;
+        }
+        return false;
+    }
+};
