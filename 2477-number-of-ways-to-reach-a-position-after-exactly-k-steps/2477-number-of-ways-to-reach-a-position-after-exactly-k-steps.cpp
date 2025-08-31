b429eed797691f2581c1735584254d7de579a292
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-31 12:47:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/2477-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+const int M=1e9+7;
+int t[2002][2002];
+unordered_map<string,int>mp;
+int f(int st,int end,int k){
+    if(k==0){
+       return st==end;
+    }
+    if(t[st+1000][k]!=-1) return t[st+1000][k];
+    int cnt=0;
+    cnt=(cnt+f(st-1,end,k-1))%M;
+    cnt=(cnt+f(st+1,end,k-1))%M;
+    return t[st+1000][k]=cnt%M;
+}
+    int numberOfWays(int startPos, int endPos, int k) {
+        memset(t,-1,sizeof(t));
+        return f(startPos,endPos,k);
+    }
+};
