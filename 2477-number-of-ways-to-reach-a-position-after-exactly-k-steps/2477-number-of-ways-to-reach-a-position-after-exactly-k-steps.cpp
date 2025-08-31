2902b5a6bdb85e32965db238da8b65104ce9e6b6
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-08-31 12:48:04 +0530

    Time: 54 ms (72.94%), Space: 21.2 MB (68.71%) - LeetHub

--- 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/2477-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
+++ 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/2477-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
 const int M=1e9+7;
-int t[2002][2002];
-unordered_map<string,int>mp;
+int t[3002][1001];
 int f(int st,int end,int k){
     if(k==0){
        return st==end;
     }
-    if(t[st+1000][k]!=-1) return t[st+1000][k];
+    if(t[st+1001][k]!=-1) return t[st+1001][k];
     int cnt=0;
     cnt=(cnt+f(st-1,end,k-1))%M;
     cnt=(cnt+f(st+1,end,k-1))%M;
-    return t[st+1000][k]=cnt%M;
+    return t[st+1001][k]=cnt%M;
 }
     int numberOfWays(int startPos, int endPos, int k) {
         memset(t,-1,sizeof(t));
