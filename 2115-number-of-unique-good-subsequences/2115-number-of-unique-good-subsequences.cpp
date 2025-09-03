505a49bf5a6f32f76acd6cd1ed61c3384516d668
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-04 01:48:37 +0530

    Time: 4 ms (72.39%), Space: 15.2 MB (42.54%) - LeetHub

--- 2115-number-of-unique-good-subsequences/2115-number-of-unique-good-subsequences.cpp
+++ 2115-number-of-unique-good-subsequences/2115-number-of-unique-good-subsequences.cpp
@@ -8,7 +8,9 @@ const int M=1e9+7;
             else end_0=(end_1+end_0)%M;
         }
         int ans=(end_0+end_1)%M;
-        if(binary.find('0')!= string :: npos) return (ans+1)%M;
+        int cnt_0=count(binary.begin(),binary.end(),'0');
+            if(cnt_0>0) cnt_0=1;
+            return (ans+cnt_0)%M;
         return ans%M;
     }
 };
