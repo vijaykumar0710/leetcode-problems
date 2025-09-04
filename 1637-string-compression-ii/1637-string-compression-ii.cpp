c3d4e1bd10462ed792504681b3025b7c9c6d1534
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-04 11:45:52 +0530

    Time: 18 ms (94.05%), Space: 9.3 MB (92.86%) - LeetHub

--- 1637-string-compression-ii/1637-string-compression-ii.cpp
+++ 1637-string-compression-ii/1637-string-compression-ii.cpp
@@ -1,25 +1,29 @@
-int t[101][101][27][101];
 class Solution {
 public:
 int n;
-int f(int i,string &s,int k,int prev,int freq){
-if(k<0) return INT_MAX;
-if(i>=n) return 0;
-if(t[i][k][prev][freq]!=-1) return t[i][k][prev][freq];
-int delete_i=f(i+1,s,k-1,prev,freq);
-int keep_i=INT_MAX;
-if((s[i]-'a')==prev){
-    int extra_len=0;
-    if(freq==1 || freq==9 || freq==99) extra_len=1;
-    keep_i=extra_len+f(i+1,s,k,prev,freq+1);
-   }else{
-    keep_i=1+f(i+1,s,k,s[i]-'a',1);
-   }
-   return t[i][k][prev][freq]=min(delete_i,keep_i);
+int t[101][101];
+int f(int i,string &s,int k){
+    if(k<0) return 1001;
+    if(i>=n || n-i<=k) return 0;
+    if(t[i][k]!=-1) return t[i][k];
+    int delete_i=f(i+1,s,k-1);
+    int keep_i=INT_MAX;
+    int deleted=0;
+    int extra=0,freq=0;
+    for(int j=i;j<n && deleted<=k;j++){
+        if(s[i]==s[j]){
+          freq++;
+        if(freq==2 || freq==10 || freq==100) extra++;
+        }else{
+            deleted++;
+        }
+      keep_i=min(keep_i,1+extra+f(j+1,s,k-deleted));
+    }
+    return t[i][k]=min(keep_i,delete_i);
 }
     int getLengthOfOptimalCompression(string s, int k) {
         n=s.size();
         memset(t,-1,sizeof(t));
-        return f(0,s,k,26,0);
+        return f(0,s,k);
     }
 };
