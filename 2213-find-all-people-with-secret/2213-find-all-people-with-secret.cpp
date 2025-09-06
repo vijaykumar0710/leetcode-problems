b7f40c21b52f1fa5cd45e6d975fc8240e306dab0
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-06 17:12:11 +0530

    Time: 459 ms (29.82%), Space: 379 MB (13.92%) - LeetHub

--- 2213-find-all-people-with-secret/2213-find-all-people-with-secret.cpp
+++ 2213-find-all-people-with-secret/2213-find-all-people-with-secret.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
-    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int fp) {
-        unordered_map<int,vector<pair<int,int>>>adj;
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+        map<int,vector<pair<int,int>>>mp;
         for(auto &e:meetings){
-            int p1=e[0],p2=e[1],t=e[2];
-            adj[p1].push_back({p2,t});
-            adj[p2].push_back({p1,t});
+          int u=e[0],v=e[1],t=e[2];
+          mp[t].push_back({u,v});
         }
-        vector<int>secret_time(n,INT_MAX);
-        queue<pair<int,int>>q;
-        secret_time[0]=0;
-        secret_time[fp]=0;
-        q.push({0,0});
-        q.push({fp,0});
-        while(!q.empty()){
-            int p1=q.front().first;
-            int t=q.front().second;
-            q.pop();
-            for(auto &neigh:adj[p1]){
-               int p2=neigh.first;
-               int time=neigh.second;
-               if(time>=t && secret_time[p2]>time){
-                secret_time[p2]=time;
-                q.push({p2,time});
-               }
+        vector<bool>visited(n,false);
+        visited[0]=true,visited[firstPerson]=true;
+        for(auto &it:mp){
+            int key=it.first;
+            auto vec=it.second;
+            unordered_map<int,vector<int>>adj;
+            unordered_set<int>seen;
+            queue<int>q;
+            for(auto &[p1,p2]:vec){
+                adj[p1].push_back(p2);
+                adj[p2].push_back(p1);
+                if(visited[p1] && seen.count(p1)==0) q.push(p1),seen.insert(p1);
+                if(visited[p2] && seen.count(p2)==0) q.push(p2),seen.insert(p2);
+            }
+            while(!q.empty()){
+                int u=q.front();
+                q.pop();
+                for(auto &v:adj[u]){
+                    if(!visited[v]){
+                        visited[v]=true;
+                        q.push(v);
+                    }
+                }
             }
         }
         vector<int>res;
         for(int i=0;i<n;i++){
-            if(secret_time[i]!=INT_MAX) res.push_back(i);
+            if(visited[i]) res.push_back(i);
         }
         return res;
     }
