81cc02e6700ba2008b88ea51b84936afd1ece533
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-07 13:01:28 +0530

    Time: 2288 ms (5.05%), Space: 178.8 MB (5.36%) - LeetHub

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
+map<pair<int,int>,int>mp;
+int f(int i,int maxi,int n,vector<pair<int,int>>&pairs){
+if(i>=pairs.size()){
+    if(maxi>=n) return 0;
+    return 1e9;
+}
+if(mp.find({i,maxi}) != mp.end())
+      return mp[{i,maxi}];
+if(pairs[i].first>maxi) return 1e9;
+int skip=f(i+1,maxi,n,pairs);
+int take=1+f(i+1,max(maxi,pairs[i].second),n,pairs);
+return mp[{i,maxi}]=min(skip,take);
+}
     int minTaps(int n, vector<int>& ranges) {
-        vector<int>max_reach(n+1,0);
+        vector<pair<int,int>>pairs;
         for(int i=0;i<=n;i++){
             int l=max(0,i-ranges[i]);
             int r=min(n,i+ranges[i]);
-            max_reach[l]=max(max_reach[l],r);
+            pairs.push_back({l,r});
         }
-        int end=0,curr_end=0,cnt=0;
-        for(int i=0;i<=n;i++){
-            if(end<i) return -1;
-            if(i>curr_end){
-            cnt++;
-            curr_end=end;
-            }
-            end=max(end,max_reach[i]);
-        }
-        return cnt;
+        sort(pairs.begin(),pairs.end());
+        int ans=f(0,0,n,pairs);
+        return ans==1e9?-1:ans;
     }
 };
