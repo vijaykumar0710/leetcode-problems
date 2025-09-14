a6fa045b30d88772bef54d920b26749b0040483e
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-14 10:53:31 +0530

    Time: 51 ms (91.67%), Space: 134.4 MB (50%) - LeetHub

--- 3990-maximum-xor-of-subsequences/3990-maximum-xor-of-subsequences.cpp
+++ 3990-maximum-xor-of-subsequences/3990-maximum-xor-of-subsequences.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int maxXorSubsequences(vector<int>& nums) {
-        vector<int>bits(32,0);
-        for(auto &x:nums){
-            for(int i=31;i>=0;i--){
-                if(!(x&(1<<i))) continue;
-                if(!bits[i]){ bits[i]=x;break; }
-                x^=bits[i];
-            }
-        }
+        vector<int>basis;
         int res=0;
-        for(int i=31;i>=0;i--) res=max(res,res^bits[i]);
+        for(auto &x:nums){
+           for(int b:basis) x=min(x,b^x);
+           if(x>0) basis.push_back(x);
+        }
+        for(auto b:basis) res=max(res,res^b);
         return res;
     }
 };
