1ed17ac74d8b655899b88c49e62f57882d5b5365
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-15 01:04:40 +0530

    Time: 1048 ms (15.82%), Space: 315.3 MB (17.25%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int findMaximumXOR(vector<int>& nums) {
-        int maxXor = 0;
-        int mask = 0;
-        for (int i = 31; i >= 0; i--) {
-            mask |= (1 << i);
-            unordered_set<int> prefixes;
-            for (int num : nums) {
-                prefixes.insert(num & mask);
-            }
-            int candidate = maxXor | (1 << i);
-            for (int p : prefixes) {
-                if (prefixes.count(p ^ candidate)) {
-                    maxXor = candidate;
+        int max_xor=0,mask=0;
+        for(int i=31;i>=0;i--){
+            mask|=(1<<i);
+            unordered_set<int>prefixes;
+        //    We are only interested in the bits up to the current position because 
+        //    we are building the maximum XOR bit by bit from MSB to LSB.
+        //    Lower bits (after i-th) don’t matter yet, so we ignore them.
+            for(auto &num:nums) prefixes.insert(num&mask);
+             int candidate=max_xor|(1<<i); // “Can I make the current bit 1 without violating what I already have in higher bits?”
+             for(auto p:prefixes){
+                if(prefixes.count(p^candidate)){
+                    max_xor=candidate;
                     break;
                 }
-            }
+             }
         }
-        return maxXor;
+        return max_xor;
     }
 };
