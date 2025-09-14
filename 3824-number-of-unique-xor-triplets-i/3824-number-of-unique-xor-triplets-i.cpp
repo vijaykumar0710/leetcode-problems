e6ea198b85ee7a26636520e4e9a5daefebb70245
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-14 23:14:02 +0530

    Time: 34 ms (5.12%), Space: 198.9 MB (57%) - LeetHub

--- 3824-number-of-unique-xor-triplets-i/3824-number-of-unique-xor-triplets-i.cpp
+++ 3824-number-of-unique-xor-triplets-i/3824-number-of-unique-xor-triplets-i.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
+        if(n<=2) return n;
         vector<int> basis;
         for (int x : nums) {
             for (int b : basis) {
@@ -10,22 +11,6 @@ public:
             if (x) basis.push_back(x);
         }
         int d = basis.size();
-        
-        // Step 2: Apply rule
-        if (n >= d + 3) {
-            // All XOR values achievable
-            return 1 << d;
-        } else {
-            // Small case -> brute force
-            unordered_set<int> st;
-            for (int i = 0; i < n; i++) {
-                for (int j = i; j < n; j++) {
-                    for (int k = j; k < n; k++) {
-                        st.insert(nums[i] ^ nums[j] ^ nums[k]);
-                    }
-                }
-            }
-            return st.size();
-        }
+         return 1 << d;
     }
 };
