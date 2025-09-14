ef07614c7381d5b8d2f40508dafaea9bf5ffe7a7
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-14 11:30:34 +0530

    Time: 47 ms (91.67%), Space: 134.5 MB (33.33%) - LeetHub

--- 3990-maximum-xor-of-subsequences/3990-maximum-xor-of-subsequences.cpp
+++ 3990-maximum-xor-of-subsequences/3990-maximum-xor-of-subsequences.cpp
@@ -4,7 +4,9 @@ public:
         vector<int>basis;
         int res=0;
         for(auto &x:nums){
-           for(int b:basis) x=min(x,b^x);
+           for(int b:basis){
+             x=min(x,b^x);
+           }
            if(x>0) basis.push_back(x);
         }
         for(auto b:basis) res=max(res,res^b);
