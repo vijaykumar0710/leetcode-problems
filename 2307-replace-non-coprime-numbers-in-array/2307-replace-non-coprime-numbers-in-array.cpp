9cbaa5a58f5f63f3bc6d167f4a14637f098ac4f9
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-16 11:48:48 +0530

    Time: 30 ms (29.63%), Space: 127.9 MB (39.81%) - LeetHub

--- 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.cpp
@@ -7,20 +7,12 @@ public:
         for(int i=0;i<n;i++){
             int x=nums[i];
            if(!st.empty() && gcd(st.top(),x)>1){
-              int y=st.top();
-              st.pop();
-              int l=lcm(x,y);
-              while(!st.empty() && gcd(st.top(),l)>1){
-                l=lcm(st.top(),l);
-                st.pop();
-              }
+             int l=lcm(x,st.top());st.pop();
+              while(!st.empty() && gcd(st.top(),l)>1){ l=lcm(st.top(),l),st.pop(); }
               st.push(l);
            }else st.push(x);
         }
-        while(!st.empty()){ 
-        res.push_back(st.top());
-        st.pop();
-        }
+        while(!st.empty()){ res.push_back(st.top()); st.pop(); }
         reverse(res.begin(),res.end());
         return res;
     }
