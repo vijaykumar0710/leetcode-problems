0bf2380a35e17015257c539342aafa56c5f69fb0
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-21 01:50:06 +0530

    Time: 3 ms (45.03%), Space: 15.4 MB (40.65%) - LeetHub

--- 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     bool makeEqual(vector<string>& words) {
-       int n=words.size();
        vector<int>freq(26,0);
-       for(auto w:words){
-         for(auto ch:w) freq[ch-'a']++;
-       } 
-       for(auto &f:freq) if(f%n!=0) return false;
+       for(auto w:words) for(auto ch:w) freq[ch-'a']++;
+       for(auto &f:freq) if(f%words.size()!=0) return false;
        return true;
     }
 };
