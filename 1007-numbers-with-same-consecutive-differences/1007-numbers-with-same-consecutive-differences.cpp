13b4cb337419b8bbfc64043a334382d142ba4c1f
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-22 02:05:40 +0530

    Time: 0 ms (100%), Space: 11.6 MB (33.27%) - LeetHub

--- 1007-numbers-with-same-consecutive-differences/1007-numbers-with-same-consecutive-differences.cpp
+++ 1007-numbers-with-same-consecutive-differences/1007-numbers-with-same-consecutive-differences.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-vector<int>res;
-void f(int &num,int n,int k){
-    if((int)log10(num)+1==n){ res.push_back(num); return; }
-    for(int j=0;j<=9;j++){
-        int last_digit=num%10;
-        if(abs(last_digit-j)!=k) continue;
-        num=num*10+j;
-        f(num,n,k);
-        num=num/10;
-    }
-}
     vector<int> numsSameConsecDiff(int n, int k) {
-        for(int i=1;i<=9;i++) f(i,n,k);
+        vector<int>res;
+        queue<int>q;
+        for(int i=1;i<=9;i++) q.push(i);
+        while(!q.empty()){
+            int num=q.front();q.pop();
+            if((int)log10(num)+1==n){
+                res.push_back(num);
+                continue;
+            }
+            for(int i=0;i<=9;i++){
+                if(abs((num%10)-i)!=k) continue;
+                q.push(num*10+i);
+            }
+        }
         return res;
     }
 };
