8daade6bef72a1c37ee529d47fe8b3a57fa7216a
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-22 02:53:07 +0530

    Time: 48 ms (55.78%), Space: 52.3 MB (24.78%) - LeetHub

--- 2600-maximum-tastiness-of-candy-basket/2600-maximum-tastiness-of-candy-basket.cpp
+++ 2600-maximum-tastiness-of-candy-basket/2600-maximum-tastiness-of-candy-basket.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
 bool isValid(vector<int>&nums,int k,int mid){
-    int cnt=1,n=nums.size();
-    int last=nums[0];
+    int cnt=1,n=nums.size(),last=nums[0];
     for(int i=1;i<n;i++){
-       if(nums[i]-last>=mid){
-        cnt++;
-        last=nums[i];
-       }
+       if(nums[i]-last>=mid){ cnt++; last=nums[i];}
        if(cnt>=k) return true;
     }
     return cnt>=k;
 }
-    int maximumTastiness(vector<int>& price, int k) {
-        unordered_set<int>st(price.begin(),price.end());
-        vector<int>nums;
-        for(auto &x:st) nums.push_back(x);
-        sort(nums.begin(),nums.end());
+    int maximumTastiness(vector<int>& nums, int k) {
         int n=nums.size();
-        if(nums.size()<k) return 0;
+        sort(nums.begin(),nums.end());
         int res=0;
         int l=0,r=nums[n-1]-nums[0];
         while(l<=r){
             int mid=l+(r-l)/2;
-            if(isValid(nums,k,mid)){
-                res=mid;
-                l=mid+1;
-            }else r=mid-1;
+            if(isValid(nums,k,mid)){res=mid; l=mid+1;}
+            else r=mid-1;
         }
         return res;
     }
