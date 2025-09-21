904a3759f5908d72d9962725e64ea90a2251dd9f
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-22 01:11:18 +0530

    Time: 303 ms (16.88%), Space: 208.2 MB (8.29%) - LeetHub

--- 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.cpp
+++ 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
+int dfs(int u,unordered_map<int,vector<int>>&adj,unordered_set<int>&st){
+    int res=1;
+    for(auto &v:adj[u]){
+        if(st.count(v)) continue;
+        st.insert(v);
+        res+=dfs(v,adj,st);
+    }
+    return res;
+}
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
         unordered_map<int,vector<int>>adj;
-        for(auto &e:edges){
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
-        }
         unordered_set<int>st(restricted.begin(),restricted.end());
-        int res=1;
-        queue<int>q;
-        q.push(0);
+        for(auto &e:edges){ adj[e[0]].push_back(e[1]), adj[e[1]].push_back(e[0]); }
         st.insert(0);
-        while(!q.empty()){
-            int u=q.front();q.pop();
-            for(auto &v:adj[u]){
-                if(st.count(v)) continue;
-                res++;
-                st.insert(v);
-                q.push(v);
-            }
-        }
-        return res;
+        return dfs(0,adj,st);
     }
 };
