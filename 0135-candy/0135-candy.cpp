9adce0b48ce1246b38127f8ea6345fb340d0f661
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-23 00:37:06 +0530

    Time: 0 ms (100%), Space: 23.5 MB (16.79%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
-        vector<int>ans1(n,1),ans2(n,1);
-        for(int i=0;i<n;i++){
-            if(i-1>=0 && ratings[i]>ratings[i-1]){
-                ans1[i]=ans1[i-1]+1;
-            }
-             if(n-1-i+1<n && ratings[n-1-i]>ratings[n-1-i+1]){
-                ans2[n-1-i]=ans2[n-1-i+1]+1;
-            }
+        vector<int>res1(n,1),res2(n,1);
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]) res1[i]=res1[i+1]+1;
+        }
+        for(int i=1;i<n;i++){
+            if(ratings[i-1]<ratings[i]) res2[i]=res2[i-1]+1;
         }
         int ans=0;
         for(int i=0;i<n;i++){
-            ans+=max(ans1[i],ans2[i]);
+            ans+=max(res1[i],res2[i]);
         }
         return ans;
     }
