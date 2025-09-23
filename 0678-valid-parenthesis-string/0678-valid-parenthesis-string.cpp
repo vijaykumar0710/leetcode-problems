a82e6ada8bf42d51b7a7e0c423b79e1682453f15
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-23 17:59:31 +0530

    Time: 0 ms (100%), Space: 9 MB (15.08%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
 int n;
+int t[101][101];
 bool f(int i,string &s,int cnt){
   if(cnt<0) return false;
   if(i>=n) return cnt==0;
+  if(t[i][cnt]!=-1) return t[i][cnt];
   bool ans=false;
   if(s[i]=='(')  ans|=f(i+1,s,cnt+1);
   else if(s[i]==')') ans|=f(i+1,s,cnt-1);
@@ -12,10 +14,11 @@ bool f(int i,string &s,int cnt){
     ans|=f(i+1,s,cnt-1);
     ans|=f(i+1,s,cnt+1);
   }
-  return ans;
+  return t[i][cnt]=ans;
 }
     bool checkValidString(string s) {
         n=s.size();
+        memset(t,-1,sizeof(t));
         return f(0,s,0);
     }
 };
