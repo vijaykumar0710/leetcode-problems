33d1c0b85f1a43556dcc19b4a34926a5fee1a5c5
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-24 13:32:45 +0530

    Time: 180 ms (67.57%), Space: 183 MB (47.44%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -12,24 +12,14 @@ for(auto &v:adj[u]){
     if(v==par) continue;
      if(!vis[v]){
        dfs(v,u);
-       // update lowTime[u] from their child
        lowTime[u]=min(lowTime[u],lowTime[v]);
-       // check bridge edge
        if(inTime[u]<lowTime[v]) res.push_back({u,v});
-     }else{
-        lowTime[u]=min(lowTime[u],lowTime[v]);
-     }
+     }else lowTime[u]=min(lowTime[u],lowTime[v]);
    }
 }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        adj.assign(n,{});
-        inTime.resize(n,0);
-        lowTime.resize(n,0);
-        vis.assign(n,false);
-        for(auto &e:connections){
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
-        }
+        adj.assign(n,{}),inTime.resize(n,0),lowTime.resize(n,0),vis.assign(n,false);
+        for(auto &e:connections){ adj[e[0]].push_back(e[1]); adj[e[1]].push_back(e[0]); }
         dfs(0,-1);
         return res;
     }
