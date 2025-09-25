8a64c5b580ee22ee1133fc1198fc23e103c9f02c
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-25 23:25:52 +0530

    Time: 1 ms (20.26%), Space: 13.4 MB (42.12%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
+int n;
+vector<vector<int>>t;
+int solve(vector<vector<int>>& triangle,int i,int j){
+    if(i==n-1) return triangle[i][j];
+    if(t[i][j]!=1e9) return t[i][j];
+    int down=triangle[i][j]+solve(triangle,i+1,j);
+    int diagonal=triangle[i][j]+solve(triangle,i+1,j+1);
+    return t[i][j]=min(down,diagonal);
+}
     int minimumTotal(vector<vector<int>>& triangle) {
-        int m=triangle.size();
-        vector<vector<int>>t(m,vector<int>(m,1e5));
-        t[0][0]=triangle[0][0];
-        int res=INT_MAX;
-        for(int i=1;i<m;i++){
-            for(int j=0;j<=i;j++){
-                int val1=INT_MAX,val2=INT_MAX;
-                if(i-1>=0) val1=t[i-1][j];
-                if(i-1>=0 && j-1>=0) val2=t[i-1][j-1];
-                t[i][j]=triangle[i][j]+min(val1,val2);
-            }
-        }
-        for(int j=0;j<m;j++) res=min(res,t[m-1][j]);
-        return res;
+       n=triangle.size();
+        t.resize(n+1,vector<int>(n+1,1e9));
+       return solve(triangle,0,0);
     }
 };
