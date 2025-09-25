0eaae6c0efd47ab239c8cf298695507ca09a1b68
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-25 23:20:11 +0530

    Time: 0 ms (100%), Space: 13.5 MB (8.23%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -6,7 +6,7 @@ public:
         t[0][0]=triangle[0][0];
         int res=INT_MAX;
         for(int i=1;i<m;i++){
-            for(int j=0;j<i;j++){
+            for(int j=0;j<=i;j++){
                 int val1=INT_MAX,val2=INT_MAX;
                 if(i-1>=0) val1=t[i-1][j];
                 if(i-1>=0 && j-1>=0) val2=t[i-1][j-1];
