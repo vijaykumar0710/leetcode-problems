081d2649fc5f92e13d2271e60c03f60149c4a544
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-26 13:54:52 +0530

    Time: 79 ms (41.11%), Space: 32.2 MB (32.22%) - LeetHub

--- 0895-shortest-path-to-get-all-keys/0895-shortest-path-to-get-all-keys.cpp
+++ 0895-shortest-path-to-get-all-keys/0895-shortest-path-to-get-all-keys.cpp
@@ -1,63 +1,42 @@
 class Solution {
 public:
-int keys_cnt=0;
 vector<vector<int>>directions={{0,1},{0,-1},{1,0},{-1,0}};
-int bfs(int row,int col,vector<string>&grid,int m,int n){
-    int full_mask=(1<<keys_cnt)-1;
-
-    vector<vector<vector<bool>>>visited(m,vector<vector<bool>>(n,vector<bool>(1<<keys_cnt,false)));
-    queue<tuple<int,int,int,int>>q;
-
+int m,n,keys=0;
+int bfs(int row,int col,vector<string>& grid){
+    int fullmask=(1<<keys)-1;
+    vector<vector<vector<bool>>>vis((1<<keys),vector<vector<bool>>(m,vector<bool>(n,false)));
+    queue<vector<int>>q;
     q.push({row,col,0,0});
-    visited[row][col][0]=true;
-
+    vis[0][row][col]=true;
     while(!q.empty()){
-        auto [r,c,mask,steps]=q.front();
-        q.pop();
-
-        if(mask==full_mask) return steps;
-
+        auto vec=q.front();q.pop();
+        int r=vec[0],c=vec[1],mask=vec[2],steps=vec[3];
+        if(mask==fullmask) return steps;
         for(auto &dir:directions){
-            int n_r=r+dir[0];
-            int n_c=c+dir[1];
-
-           if(n_r<0 || n_c<0 || n_r>=m || n_c>=n) continue;
-           char ch=grid[n_r][n_c];
-
-           if(ch=='#') continue;
-
+           int n_r=r+dir[0],n_c=c+dir[1];
+           if(n_r<0 || n_c<0 || n_r>=m || n_c>=n || grid[n_r][n_c]=='#') continue;
            int new_mask=mask;
-           if(islower(ch)){
-            int bit=ch-'a';
-            new_mask|=(1<<bit);
+           char ch=grid[n_r][n_c];
+           if(islower(ch)) new_mask|=(1<<(ch-'a'));
+           else if(isupper(ch)){
+            if((new_mask&(1<<(ch-'A')))==0) continue;
            }
-
-           if(isupper(ch)){
-            int bit=ch-'A';
-            if((new_mask&(1<<bit))==0) continue;
-           }
-
-           if(!visited[n_r][n_c][new_mask]){
-            visited[n_r][n_c][new_mask]=true;
-            q.push({n_r,n_c,new_mask,steps+1});
-           }
+           if(vis[new_mask][n_r][n_c]) continue;
+           vis[new_mask][n_r][n_c]=true;
+           q.push({n_r,n_c,new_mask,steps+1});
         }
     }
     return -1;
 }
     int shortestPathAllKeys(vector<string>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        int st_row,st_col;
+        m=grid.size(),n=grid[0].size();
+        int st_r,st_c;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]=='@'){
-                    st_row=i;
-                    st_col=j;
-                }
-                if(islower(grid[i][j])) keys_cnt++;
+                if(grid[i][j]=='@') st_r=i,st_c=j;
+                if(islower(grid[i][j])) keys++;
             }
         }
-     return bfs(st_row,st_col,grid,m,n);
+        return bfs(st_r,st_c,grid);
     }
 };
