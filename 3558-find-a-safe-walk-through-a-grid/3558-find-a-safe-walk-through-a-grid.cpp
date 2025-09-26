00f8322dfe6a64765215799d1dee2fe361a52e2f
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-26 12:06:16 +0530

    Time: 55 ms (35.68%), Space: 44.2 MB (23.94%) - LeetHub

--- 3558-find-a-safe-walk-through-a-grid/3558-find-a-safe-walk-through-a-grid.cpp
+++ 3558-find-a-safe-walk-through-a-grid/3558-find-a-safe-walk-through-a-grid.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
+vector<vector<int>>directions={{0,1},{0,-1},{1,0},{-1,0}};
 int m,n;
-vector<vector<int>>directions={{1,0},{-1,0},{0,1},{0,-1}};
-bool bfs(int row,int col,vector<vector<int>>& grid, int health){
-    vector<vector<int>>visited_min(m,vector<int>(n,-1));
-    deque<vector<int>>dq;
-    if(grid[0][0]==1) health--;
-    dq.push_back({0,0,health});
-    visited_min[0][0]=health;
-    while(!dq.empty()){
-            auto vec=dq.front();
-            int r=vec[0],c=vec[1],h=vec[2];
-            dq.pop_front();
-            if(h>=1 && r==m-1 && c==n-1) return true;
-            if(h<1) continue;
-            for(auto &dir:directions){
-               int n_r=r+dir[0];
-               int n_c=c+dir[1];
-               if(n_r>=0 && n_r<m && n_c>=0 && n_c<n){
-                int new_h=h-grid[n_r][n_c];
-                if(new_h>=1 && new_h>visited_min[n_r][n_c]){ 
-                visited_min[n_r][n_c]=new_h;
-                if(grid[n_r][n_c]==0) dq.push_front({n_r,n_c,new_h});
-                else dq.push_back({n_r,n_c,new_h});
-                }
-               }
-            }
-        }
-    return false;
+bool bfs(int r,int c,vector<vector<int>>& grid, int health){
+  vector<vector<bool>>vis(m,vector<bool>(n,false));
+  deque<vector<int>>dq;
+  vis[r][c]=true;
+  if(grid[r][c]==1) health--;
+  dq.push_back({r,c,health});
+  while(!dq.empty()){ 
+  auto vec=dq.front();dq.pop_front();
+  int row=vec[0],col=vec[1],h=vec[2];
+  if(row==m-1 && col==n-1 && h>=1) return true;
+  for(auto &dir:directions){
+    int n_r=row+dir[0],n_c=col+dir[1];
+    if(n_r<0 || n_c<0 || n_r>=m || n_c>=n || vis[n_r][n_c]) continue;
+    vis[n_r][n_c]=true;
+    if(grid[n_r][n_c]==0) dq.push_front({n_r,n_c,h});
+    else dq.push_back({n_r,n_c,h-1});
+    }
+  }
+  return false;
 }
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        m=grid.size();
-        n=grid[0].size();
+        m=grid.size(),n=grid[0].size();
         return bfs(0,0,grid,health);
     }
 };
