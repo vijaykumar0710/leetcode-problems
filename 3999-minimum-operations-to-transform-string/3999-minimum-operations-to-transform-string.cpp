f9d2495736eeb631e9a8625d3df7722aab0f31f4
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-27 02:01:18 +0530

    Time: 273 ms (7.28%), Space: 59.9 MB (44.05%) - LeetHub

--- 3999-minimum-operations-to-transform-string/3999-minimum-operations-to-transform-string.cpp
+++ 3999-minimum-operations-to-transform-string/3999-minimum-operations-to-transform-string.cpp
@@ -5,7 +5,7 @@ public:
         int n=s.size();
         int res=0;
         for(int i=0;i<n-1;i++){
-            if(s[i]=='a') continue;
+            if(s[i]=='a' || s[i]==s[i+1]) continue;
             res+=(s[i+1]-s[i]);
         }
         res+=(26-(s[n-1]-'a'));
