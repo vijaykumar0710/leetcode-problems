86471308f8a6b90868922b703f48e71ce3e90b30
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-27 13:20:35 +0530

    Time: 172 ms (70.62%), Space: 212.1 MB (79.2%) - LeetHub

--- 3105-minimum-edge-reversals-so-every-node-is-reachable/3105-minimum-edge-reversals-so-every-node-is-reachable.cpp
+++ 3105-minimum-edge-reversals-so-every-node-is-reachable/3105-minimum-edge-reversals-so-every-node-is-reachable.cpp
@@ -2,31 +2,30 @@ class Solution {
 public:
 vector<vector<pair<int,int>>>adj;
 vector<int>ans;
-int dfs(int u,int par){
-    int flips=0;
-    for(auto &[v,cost]:adj[u]){
-        if(v==par) continue;
-        flips+=(cost+dfs(v,u));
-    }
-    return flips;
+void dfs(int u,int par){
+for(auto [v,cost]:adj[u]){
+    if(par==v) continue;
+    ans[0]+=cost;
+    dfs(v,u);
+   }
 }
-void rerootdfs(int u,int par){
-for(auto &[v,cost]:adj[u]){
+void reroot_dfs(int u,int par){
+  for(auto [v,cost]:adj[u]){
     if(v==par) continue;
     if(cost==0) ans[v]=ans[u]+1;
     else ans[v]=ans[u]-1;
-    rerootdfs(v,u);
+    reroot_dfs(v,u);
   }
 }
     vector<int> minEdgeReversals(int n, vector<vector<int>>& edges) {
         adj.assign(n,{});
         ans.assign(n,0);
         for(auto &e:edges){
-            adj[e[0]].push_back({e[1],0});
-            adj[e[1]].push_back({e[0],1});
+           adj[e[0]].push_back({e[1],0});
+           adj[e[1]].push_back({e[0],1});
         }
-        ans[0]=dfs(0,-1);
-        rerootdfs(0,-1);
+        dfs(0,-1);
+        reroot_dfs(0,-1);
         return ans;
     }
 };
