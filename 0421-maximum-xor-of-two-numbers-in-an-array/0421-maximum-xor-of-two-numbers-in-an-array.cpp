3a5f90857cc4eb0613d5f543283a6593e77502f6
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-28 19:13:13 +0530

    Time: 589 ms (80.31%), Space: 184 MB (89.84%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
-struct trieNode{
-trieNode *children[2];
-};
+struct trieNode{ trieNode *children[2]; };
 trieNode* getNode(){
     trieNode* newNode=new trieNode();
     newNode->children[0]=NULL;
@@ -23,17 +21,15 @@ int getMax(int num,trieNode* root){
     for(int i=31;i>=0;i--){
         int bit=(num>>i)&1;
         if(crawler->children[1-bit]){
-            maxi|=(1<<i);
-            crawler=crawler->children[1-bit];
+            maxi|=(1<<i); crawler=crawler->children[1-bit];
         }else crawler=crawler->children[bit];
     }
     return maxi;
 }
     int findMaximumXOR(vector<int>& nums) {
-        int n=nums.size();
+        int n=nums.size(),res=0;;
         trieNode* root=getNode();
         for(auto &num:nums) insert(num,root);
-        int res=0;
         for(auto &num:nums) res=max(res,getMax(num,root));
         return res;
     }
