485464616367d934e935aec5150c821461716d20
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-09-30 13:20:18 +0530

    Time: 3 ms (94.87%), Space: 9.9 MB (98.22%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
-typedef unsigned long long ll;
-int t[301][5001];
-int f(int n,int amount,vector<int>&coins){
-    if(amount==0) return 1;
-    if(n<=0) return 0;
-    if(t[n][amount]!=-1) return t[n][amount];
-    ll take=0;
-    if(coins[n-1]<=amount){
-        take=f(n,amount-coins[n-1],coins);
-    }
-    ll skip=f(n-1,amount,coins);
-    return t[n][amount]=(ll)take+skip;
-}
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        memset(t,-1,sizeof(t));
-        return f(n,amount,coins);
+        vector<unsigned int>t(amount+1,0);
+        t[0]=1;
+        for(auto c:coins){
+            for(int i=c;i<=amount;i++){ if(i-c<0) continue; t[i]+=t[i-c]; }
+        }
+        return t[amount];
     }
 };
