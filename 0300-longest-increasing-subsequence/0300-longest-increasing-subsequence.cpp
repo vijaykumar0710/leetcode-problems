85398d1a973d86fce49aa46b58b6f310a71f3ed9
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-10-06 23:27:54 +0530

    Time: 2 ms (82.27%), Space: 14.1 MB (79.43%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,30 +1,13 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> adj(n);
-        vector<int> indegree(n, 0);
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] < nums[j]) {
-                    adj[i].push_back(j);
-                    indegree[j]++;
-                }
-            }
+        int n=nums.size();
+        vector<int>sorted;
+        for(int i=0;i<n;i++){
+    auto it = lower_bound(sorted.begin(), sorted.end(), nums[i]);
+    if(it==sorted.end())sorted.push_back(nums[i]);
+      else *it = nums[i];
         }
-        queue<int> q;
-        for (int i = 0; i < n; i++) if (indegree[i] == 0) q.push(i);
-        vector<int> dist(n, 1);
-        int ans = 1;
-        while (!q.empty()) {
-            int u = q.front(); q.pop();
-            for (int v : adj[u]) {
-                dist[v] = max(dist[v], dist[u] + 1);
-                ans = max(ans, dist[v]);
-                indegree[v]--;
-                if (indegree[v]==0) q.push(v);
-            }
-        }
-        return ans;
+        return sorted.size();
     }
 };
