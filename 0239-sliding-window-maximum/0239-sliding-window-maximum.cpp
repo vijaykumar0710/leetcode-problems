d6bd69190ad6c63af1bf019b337c2c0a39cb0d5e
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-10-08 00:28:36 +0530

    Time: 332 ms (9.29%), Space: 180.1 MB (7.94%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n=nums.size();
-         map<int,int,greater<int>>desc_mp;
+         map<int,int>desc_mp;
          vector<int>res;
          int i=0;
         for(int j=0;j<n;j++){
             desc_mp[nums[j]]++;
             if(j-i+1>=k){
-             res.push_back(desc_mp.begin()->first);
+             res.push_back(desc_mp.rbegin()->first);
              desc_mp[nums[i]]--;
              if(desc_mp[nums[i]]==0) desc_mp.erase(nums[i]); 
              i++;  
