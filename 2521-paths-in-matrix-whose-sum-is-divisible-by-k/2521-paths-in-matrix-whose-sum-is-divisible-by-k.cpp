273e64643c5e37dede52398904f09b380f11d5d5
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-10-07 23:48:54 +0530

    Time: 207 ms (85.59%), Space: 132.1 MB (82.35%) - LeetHub

---
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/2521-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -0,0 +1,19 @@
+const int M = 1e9+7;
+class Solution {
+public:
+    int numberOfPaths(vector<vector<int>>& grid, int k) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<vector<int>>> t(m, vector<vector<int>>(n, vector<int>(k, 0)));
+        t[0][0][grid[0][0]%k]++;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                for(int r=0;r<k;r++){
+                    int new_r=(r+grid[i][j])%k;
+                    if(i>0) t[i][j][new_r]=(t[i][j][new_r]+t[i-1][j][r])%M;
+                    if(j>0) t[i][j][new_r]=(t[i][j][new_r]+t[i][j-1][r])%M;
+                }
+            }
+        }
+        return t[m-1][n-1][0];
+    }
+};
