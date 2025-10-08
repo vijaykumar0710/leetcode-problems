ff31a1ca852d93cfdf113506145c7c721c249d4f
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-10-08 14:30:28 +0530

    Time: 31 ms (95.55%), Space: 14.6 MB (72.31%) - LeetHub

--- 0546-remove-boxes/0546-remove-boxes.cpp
+++ 0546-remove-boxes/0546-remove-boxes.cpp
@@ -4,12 +4,10 @@ int t[101][101][101];
 int f(vector<int>&boxes,int l,int r,int k){
     if(l>r) return 0;
     if(t[l][r][k]!=-1) return t[l][r][k];
-
     int origL=l,origK=k;
     while(l+1<=r && boxes[l]==boxes[l+1]){
         l++,k++;
     }
-
     int res=(k+1)*(k+1)+f(boxes,l+1,r,0);
     for(int m=l+1;m<=r;m++){
         if(boxes[l]==boxes[m]){
