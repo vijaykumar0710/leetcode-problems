2207fa35b6c3c22177d5eaeaa0467382c26e0b39
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-10-18 00:59:29 +0530

    Time: 0 ms (100%), Space: 47.2 MB (46.85%) - LeetHub

--- 3190-minimum-operations-to-maximize-last-elements-in-arrays/3190-minimum-operations-to-maximize-last-elements-in-arrays.cpp
+++ 3190-minimum-operations-to-maximize-last-elements-in-arrays/3190-minimum-operations-to-maximize-last-elements-in-arrays.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 int min_swap(vector<int>&nums1,vector<int>&nums2){
    int n=nums1.size(),cnt=0;
-   for(int i=0;i<n-1;i++){
+   for(int i=0;i<n;i++){
     if(nums1[i]<=nums1[n-1] && nums2[i]<=nums2[n-1]) continue;
     if(nums1[i]<=nums2[n-1] && nums2[i]<=nums1[n-1]) cnt++;
     else return -1;
