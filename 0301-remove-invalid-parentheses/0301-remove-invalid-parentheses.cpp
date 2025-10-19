22b89bdfe323d9e18477e32d964e7c94ebc7ffa0
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-10-19 19:52:30 +0530

    Time: 636 ms (12.51%), Space: 15.3 MB (55.62%) - LeetHub

---
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+int n;
+unordered_set<string>valid;
+bool isValid(string &s){
+    int cnt=0;
+    for(int i=0;i<s.size();i++){
+        if(isalpha(s[i])) continue;
+        if(s[i]=='(') cnt++;
+        else{
+            if(cnt==0) return false;
+            cnt--;
+        }
+    }
+    return cnt==0;
+}
+void f(int idx,string &s,string &st){
+    if(idx>=n){
+        if(isValid(st)) valid.insert(st);
+        return;
+    }
+    if(!isalpha(s[idx])) f(idx+1,s,st);
+    st+=s[idx];
+    f(idx+1,s,st);
+    st.pop_back();
+    return;
+}
+    vector<string> removeInvalidParentheses(string s) {
+        n=s.size();
+        string st="";
+        f(0,s,st);
+        vector<string>res;
+        int maxi=0;
+        for(auto &str:valid){
+            int sz=str.size();
+            maxi=max(maxi,sz);
+        }
+        for(auto &str:valid){
+            if(str.size()==maxi) res.push_back(str);
+        }
+        return res;
+    }
+};
