fa652c469201385439877042e0efd72f9401ab2f
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-10-20 12:01:12 +0530

    Time: 21 ms (50.84%), Space: 95.7 MB (45.45%) - LeetHub

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/1573-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/1573-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -1,67 +1,40 @@
 class Solution {
 public:
-vector<pair<int,int>>pairs;
-void pairs_cal(vector<int>& arr, int target) {
-    int n=arr.size();
-    int sum = 0, i = 0;
-    pairs.clear();
-
-    for (int j = 0; j < n; j++) {
-        sum += arr[j];
-
-        while (sum > target && i <= j) {
-            sum -= arr[i];
+vector<pair<int,int>>intervals;
+void cal_interval(vector<int>&arr,int target){
+int n=arr.size(),sum=0,i=0;
+intervals.clear(); 
+for(int j=0;j<n;j++){
+    sum+=arr[j];
+    while(sum>target && i<=j){ sum-=arr[i]; i++; }
+   if(sum==target) intervals.push_back({i,j});
+   }
+}
+int cal_minLen(vector<pair<int,int>>&intervals){
+    int n=intervals.size();
+    if (n < 2) return -1;
+    vector<pair<int,int>>start=intervals;
+    sort(start.begin(),start.end());
+    vector<pair<int,int>>end=start;
+    sort(end.begin(),end.end(),[](auto &a,auto &b){
+       return a.second<b.second;
+    });
+    int res=INT_MAX;
+    int min_len=INT_MAX,i=0;
+    for(int j=0;j<n;j++){
+        while(i<n && end[i].second<start[j].first){
+            min_len=min(min_len,end[i].second-end[i].first+1);
             i++;
         }
-
-        if (sum == target) {
-            pairs.push_back({i, j});
+        if(min_len!=INT_MAX){
+            res=min(res,min_len+start[j].second-start[j].first+1);
         }
     }
-}
-
-
-  int minTwoNonOverlap(vector<pair<int,int>>& ranges) {
-    int n = ranges.size();
-    if (n < 2) return -1;
-
-    vector<int> len(n);
-    for (int i = 0; i < n; i++)
-        len[i] = ranges[i].second - ranges[i].first + 1;
-
-    vector<pair<pair<int,int>,int>> byStart; 
-    for (int i = 0; i < n; i++)
-        byStart.push_back({ranges[i], len[i]});
-    sort(byStart.begin(), byStart.end(),
-         [](auto &a, auto &b){ return a.first.first < b.first.first; });
-
-   
-    vector<pair<pair<int,int>,int>> byEnd = byStart;
-    sort(byEnd.begin(), byEnd.end(),
-         [](auto &a, auto &b){ return a.first.second < b.first.second; });
-
-    int j = 0;
-    int minLeftLen = INT_MAX; 
-    int ans = INT_MAX;
-
-    for (int i = 0; i < n; i++) {
-        int start_i = byStart[i].first.first;
-        int len_i   = byStart[i].second;
-
-        while (j < n && byEnd[j].first.second < start_i) {
-            minLeftLen = min(minLeftLen, byEnd[j].second);
-            j++;
-        }
-
-        if (minLeftLen != INT_MAX)
-            ans = min(ans, len_i + minLeftLen);
-    }
-
-    return (ans == INT_MAX ? -1 : ans);
+    return res==INT_MAX?-1:res;
 }
     int minSumOfLengths(vector<int>& arr, int target) {
-        int res=INT_MAX;
-        pairs_cal(arr,target);
-        return minTwoNonOverlap(pairs);
+        int n=arr.size();
+        cal_interval(arr,target);
+        return  cal_minLen(intervals);
     }
 };
