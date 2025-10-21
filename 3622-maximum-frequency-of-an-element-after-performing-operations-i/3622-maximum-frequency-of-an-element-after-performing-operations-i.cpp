b6f80c115776ea6b0aa5260e42715c4e0358710a
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-10-21 11:45:21 +0530

    Time: 239 ms (47.09%), Space: 137.5 MB (41.99%) - LeetHub

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-int f(vector<int>&nums,int target,int k,int op){
-    int n=nums.size(),cnt=0;
-    for(int i=0;i<n;i++){
-        if(nums[i]==target){
-            cnt++;
-            continue;
-        }
-        int left=nums[i]-k,right=nums[i]+k;
-        if(op>0){
-          if(left<=target && right>=target){
-            cnt++;
-            op--;
-          }
-        }
-    }
-    return cnt;
+unordered_map<int,int>mp;
+int f(vector<int>&start,vector<int>&endi,int &target,int op){
+    auto it1=upper_bound(start.begin(),start.end(),target);
+    auto it2=lower_bound(endi.begin(),endi.end(),target);
+    int x=it1-start.begin();
+    int y=it2-endi.begin();
+    int cnt=mp.count(target)?mp[target]:0;
+    int freq=min(op+cnt,(x-y));
+    return freq; 
 }
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
         int n=nums.size(),res=0;
         sort(nums.begin(),nums.end());
+        vector<int>start,endi;
+       for(int i=0;i<n;i++){
+        mp[nums[i]]++;
+        start.push_back(nums[i]-k);
+        endi.push_back(nums[i]+k);
+       }
         int l=nums[0]-k,r=nums[n-1]+k;
         for(int t=l;t<=r;t++){
-            res=max(res,f(nums,t,k,numOperations));
+            res=max(res,f(start,endi,t,numOperations));
         }
         return res;
     }
