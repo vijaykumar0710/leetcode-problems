9a330caedb10c5c2873dc0717591b9b1931727f3
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-10-22 15:12:01 +0530

    Time: 1716 ms (5.31%), Space: 79.6 MB (66.18%) - LeetHub

--- 2510-maximum-deletions-on-a-string/2510-maximum-deletions-on-a-string.cpp
+++ 2510-maximum-deletions-on-a-string/2510-maximum-deletions-on-a-string.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
+int t[4001][4001];
+int f(int l,int r,string &s,int n){
+    if(l==n && r==n) return 0;
+    if(r==n) return -1e9;
+    if(r-l+1>n-r+1) return -1e9;
+    if(t[l][r]!=-1) return t[l][r];
+    int res=0;
+    int len=r-l+1;
+    if(s.compare(l,len,s,r+1,len)==0){
+        res=max(res,1+f(r+1,r+1,s,n));
+    }
+    res=max(res,max(1+f(n,n,s,n),f(l,r+1,s,n)));
+    return t[l][r]=res;
+}
     int deleteString(string s) {
         int n=s.size();
-        vector<vector<int>>lcp(n+1,vector<int>(n+1,0));
-        for(int i=n-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                if(s[i]==s[j]) lcp[i][j]=1+lcp[i+1][j+1];
-            }
-        }
-        vector<int>t(n+1,0);
-        for(int i=n-1;i>=0;i--){
-            t[i]=1;
-            for(int len=1;i+2*len<=n;len++){
-              if(lcp[i][i+len]>=len)  t[i]=max(t[i],1+t[i+len]);
-            }
-        }
-        return t[0];
+        memset(t,-1,sizeof(t));
+        return f(0,0,s,n);
     }
 };
