4cba6d6ea122be87756cb774d7399f13aa0f1f44
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-10-22 15:28:57 +0530

    Time: 566 ms (33.82%), Space: 383.8 MB (31.4%) - LeetHub

--- 2510-maximum-deletions-on-a-string/2510-maximum-deletions-on-a-string.cpp
+++ 2510-maximum-deletions-on-a-string/2510-maximum-deletions-on-a-string.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
-struct RollingHash{
-    vector<long long>prefix,power;
-    long long mod=1e9+7;
-    long long base=131;
-
-    RollingHash(string &s){
-        int n=s.size();
-        prefix.assign(n+1,0);
-        power.assign(n+1,1);
-        for(int i=0;i<n;i++){
-          prefix[i+1]=(prefix[i]*base+s[i])%mod;
-          power[i+1]=(power[i]*base)%mod;
-        }
-    }
-    long long get_hash(int l,int r){
-        return(prefix[r+1]+mod-(prefix[l]*power[r-l+1])%mod)%mod;
-    }
-};
-int t[4001][4001];
-int f(int l,int r,string &s,int n,RollingHash &rh){
-    if(l==n && r==n) return 0;
-    if(r==n) return -1e9;
-    if(r-l+1>n-r+1) return -1e9;
-    if(t[l][r]!=-1) return t[l][r];
-    int res=0;
-    int len = r - l + 1;
-   if(r + len < n && rh.get_hash(l, l + len - 1) == rh.get_hash(r+1, r + len)) {
-    res = max(res, 1 + f(r + 1, r + 1, s, n, rh));
-    }
-    res=max(res,max(1+f(n,n,s,n,rh),f(l,r+1,s,n,rh)));
-    return t[l][r]=res;
-}
     int deleteString(string s) {
-        RollingHash rh(s);
         int n=s.size();
-        memset(t,-1,sizeof(t));
-        return f(0,0,s,n,rh);
+        vector<vector<int>>lcp(n+1,vector<int>(n+1,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(s[i]==s[j]) lcp[i][j]=1+lcp[i+1][j+1];
+            }
+        }
+        vector<int>t(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            t[i]=1;
+            for(int len=1;i+2*len<=n;len++){
+              if(lcp[i][i+len]>=len)  t[i]=max(t[i],1+t[i+len]);
+            }
+        }
+        return t[0];
     }
 };
