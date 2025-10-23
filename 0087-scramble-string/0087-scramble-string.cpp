8e0d5e785744db83d433e1bc3cdf3258908370ca
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-10-23 16:23:35 +0530

    Time: 135 ms (24.4%), Space: 34.3 MB (25.35%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -3,14 +3,14 @@ public:
 unordered_map<string,bool>t;
 bool solve(string s1,string s2){
    if(s1.compare(s2)==0) return true;
-  // if(s1.length()<=1) return false;
+   if(s1.length()<=1) return false;
    string key=s1+"#"+s2;
    if(t.find(key)!=t.end()){
     return t[key];
    }
    int n=s1.length();
    bool flag=false;
-   for(int i=1;i<=n-1;i++){
+   for(int i=1;i<n;i++){
     if(solve(s1.substr(0,i),s2.substr(n-i,i)) && solve(s1.substr(i,n-i),s2.substr(0,n-i))
     || solve(s1.substr(0,i),s2.substr(0,i)) && solve(s1.substr(i,n-i),s2.substr(i,n-i))){
         flag=true;
