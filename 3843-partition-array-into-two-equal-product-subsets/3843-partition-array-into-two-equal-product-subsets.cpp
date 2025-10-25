07d99cb11f05d529c7353333d9bf1d581957410f
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-10-25 21:50:41 +0530

    Time: 831 ms (5.18%), Space: 23.8 MB (59.36%) - LeetHub

---
+++ 3843-partition-array-into-two-equal-product-subsets/3843-partition-array-into-two-equal-product-subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+int n;
+bool f(int i,vector<int>&nums,long double target,long double s1,long double s2){
+if(i>=n){
+    return (s1==target && s2==target);
+   }
+   bool flag=false;
+   for(int idx=i;idx<n;idx++){
+    s1=1ll*s1*nums[idx];
+    flag|=f(idx+1,nums,target,s1,s2);
+    s1=1ll*(s1/nums[idx]);
+    s2=1ll*s2*nums[idx];
+    flag|=f(idx+1,nums,target,s1,s2);
+    s2=1ll*(s2/nums[idx]);
+   }
+   return flag;
+}
+    bool checkEqualPartitions(vector<int>& nums, long long target) {
+        n=nums.size();
+        return f(0,nums,target,1,1);
+    }
+};
