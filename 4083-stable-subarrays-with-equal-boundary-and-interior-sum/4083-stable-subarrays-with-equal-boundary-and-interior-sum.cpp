03a0c9dea8f15a4d007205bb2667daeaed6c2d90
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-10-26 20:15:39 +0530

    Time: 664 ms (9.09%), Space: 423.8 MB (9.09%) - LeetHub

--- 4083-stable-subarrays-with-equal-boundary-and-interior-sum/4083-stable-subarrays-with-equal-boundary-and-interior-sum.cpp
+++ 4083-stable-subarrays-with-equal-boundary-and-interior-sum/4083-stable-subarrays-with-equal-boundary-and-interior-sum.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    long long countStableSubarrays(vector<int>& capacity) {
-        int n = capacity.size();
-        vector<long long> prefix(n + 1, 0);
-        for (int i = 0; i < n; ++i) prefix[i + 1] = prefix[i] + capacity[i];
-        unordered_map<long long, unordered_map<long long, int>> mp;
-        long long ans = 0;
-        for (int r = 0; r < n; ++r) {
-            if (r - 2 >= 0) {
-                int l = r - 2;
-                long long valL = capacity[l];
-                long long key = prefix[l + 1] + valL;
-                mp[valL][key] += 1;
-            }
-            long long valR = capacity[r];
-               if (mp.count(valR)) {
-                auto it = mp[valR].find(prefix[r]);
-                if (it != mp[valR].end()) ans += it->second;
-            }
+    long long countStableSubarrays(vector<int>& v) {
+        int n = v.size();
+        vector<long long> pref(n);
+        pref[0] = v[0];
+        for (int i = 1; i < n; i++) {
+            pref[i] = pref[i - 1] + (long long)v[i];
         }
-        return ans;
+        long long cnt = 0;
+        unordered_map<long long, unordered_map<long long, long long>> mp;
+
+        for (int i = 1; i < n; i++) {
+            long long need = pref[i] - v[i] * 2;
+            cnt += mp[need][v[i]];
+            mp[pref[i-1]][v[i-1]]++;
+        }
+
+        return cnt;
     }
 };
