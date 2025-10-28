7001e173f244d98645ba9c3afbf6615093689ea2
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-10-29 01:37:50 +0530

    Time: 4 ms (67.72%), Space: 12.6 MB (70.08%) - LeetHub

---
+++ 2550-words-within-two-edits-of-dictionary/2550-words-within-two-edits-of-dictionary.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+unordered_set<string>st;
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        int n=queries.size();
+        vector<string>res;
+        for(auto &query:queries){
+            for(auto &word:dictionary){
+                if(query.size()!=word.size())continue;
+                int i=0,cnt=0;
+                while(i<word.size()){
+                    if(query[i]!=word[i]) cnt++;
+                    i++;
+                }
+               if(cnt<=2){
+                 res.push_back(query);
+                 break;
+               }
+            }
+        }
+        return res;
+    }
+};
