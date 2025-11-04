dc3885d55d392b9c87142600299a0458ed1555ef
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-11-04 21:44:30 +0530

    Time: 122 ms (5.2%), Space: 131.5 MB (23.24%) - LeetHub

--- 2085-array-with-elements-not-equal-to-average-of-neighbors/2085-array-with-elements-not-equal-to-average-of-neighbors.cpp
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/2085-array-with-elements-not-equal-to-average-of-neighbors.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int n=nums.size();
-        priority_queue<int,vector<int>,greater<int>>pq(nums.begin(),nums.end());
+        priority_queue<int>pq(nums.begin(),nums.end());
         vector<int>res(n);
         for(int i=0;i<n;i+=2){
             res[i]=pq.top();
