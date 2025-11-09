7f594aaafcac807ef80fd06195281e640f37b7bf
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-11-09 15:51:41 +0530

    Time: 43 ms (38.89%), Space: 44.5 MB (16.67%) - LeetHub

--- 4074-count-subarrays-with-majority-element-i/4074-count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/4074-count-subarrays-with-majority-element-i.cpp
@@ -1,44 +1,62 @@
 class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
-        int n=nums.size();
+        int n = nums.size();
+
+        // convert
         for(int i=0;i<n;i++){
-            nums[i] = (nums[i]==target ? 1 : -1);
+            if(nums[i]==target) nums[i]=1;
+            else nums[i]=-1;
         }
-        
-        vector<long long> pref(n+1);
+
+        vector<int> pref(n+1);
         pref[0]=0;
         for(int i=1;i<=n;i++){
-            pref[i] = pref[i-1] + nums[i-1];
+            pref[i]=pref[i-1]+nums[i-1];
         }
-        
-        // coordinate compress
-        vector<long long> comp = pref;
-        sort(comp.begin(),comp.end());
-        comp.erase(unique(comp.begin(),comp.end()),comp.end());
-        
-        auto getid=[&](long long x){
-            return lower_bound(comp.begin(),comp.end(),x)-comp.begin()+1;
+
+        // coordinate compress prefix
+        vector<int> all = pref;
+        sort(all.begin(), all.end());
+        all.erase(unique(all.begin(), all.end()), all.end());
+
+        auto getID=[&](int x){
+            return (int)(lower_bound(all.begin(),all.end(),x)-all.begin());
         };
-        
-        int m=comp.size();
-        vector<long long> fen(m+1,0);
-        
-        auto update=[&](int i){
-            for(;i<=m;i+=i&-i) fen[i]++;
+
+        int sz = all.size();
+        vector<int> seg(4*sz,0);
+
+        function<void(int,int,int,int)> update = [&](int idx,int l,int r,int pos){
+            if(l==r){
+                seg[idx]++;
+                return;
+            }
+            int mid=(l+r)/2;
+            if(pos<=mid) update(idx*2,l,mid,pos);
+            else update(idx*2+1,mid+1,r,pos);
+            seg[idx]=seg[idx*2]+seg[idx*2+1];
         };
-        auto query=[&](int i){
-            long long s=0;
-            for(;i>0;i-=i&-i) s+=fen[i];
-            return s;
+
+        function<int(int,int,int,int,int)> query=[&](int idx,int l,int r,int ql,int qr){
+            if(ql>r||qr<l) return 0;
+            if(ql<=l && r<=qr) return seg[idx];
+            int mid=(l+r)/2;
+            return query(idx*2,l,mid,ql,qr)+query(idx*2+1,mid+1,r,ql,qr);
         };
-        
-        long long ans=0;
-        for(int i=0;i<=n;i++){
-            int id = getid(pref[i]);
-            ans += query(id-1); // all prefix smaller
-            update(id);
+
+        long long res=0;
+
+        // initial prefix 0 update
+        update(1,0,sz-1,getID(pref[0]));
+
+        for(int i=1;i<=n;i++){
+            int id = getID(pref[i]);
+            // count how many prefix < this
+            if(id>0) res += query(1,0,sz-1,0,id-1);
+            update(1,0,sz-1,id);
         }
-        return ans;
+
+        return (int)res;
     }
 };
