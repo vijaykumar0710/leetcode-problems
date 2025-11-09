107180746c5e537323005bf4efb0bd64339822da
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-11-09 14:48:25 +0530

    Time: 14 ms (88.89%), Space: 43.3 MB (16.67%) - LeetHub

--- 4074-count-subarrays-with-majority-element-i/4074-count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/4074-count-subarrays-with-majority-element-i.cpp
@@ -3,20 +3,42 @@ public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
         int n=nums.size();
         for(int i=0;i<n;i++){
-            if(nums[i]==target) nums[i]=1;
-            else nums[i]=-1;
+            nums[i] = (nums[i]==target ? 1 : -1);
         }
-        vector<int>prefix(n+1);
-        prefix[0]=0;
+        
+        vector<long long> pref(n+1);
+        pref[0]=0;
         for(int i=1;i<=n;i++){
-         prefix[i]=prefix[i-1]+nums[i-1];
+            pref[i] = pref[i-1] + nums[i-1];
         }
-        int res=0;
+        
+        // coordinate compress
+        vector<long long> comp = pref;
+        sort(comp.begin(),comp.end());
+        comp.erase(unique(comp.begin(),comp.end()),comp.end());
+        
+        auto getid=[&](long long x){
+            return lower_bound(comp.begin(),comp.end(),x)-comp.begin()+1;
+        };
+        
+        int m=comp.size();
+        vector<long long> fen(m+1,0);
+        
+        auto update=[&](int i){
+            for(;i<=m;i+=i&-i) fen[i]++;
+        };
+        auto query=[&](int i){
+            long long s=0;
+            for(;i>0;i-=i&-i) s+=fen[i];
+            return s;
+        };
+        
+        long long ans=0;
         for(int i=0;i<=n;i++){
-            for(int j=0;j<i;j++){
-                if(prefix[j]<prefix[i]) res++;
-            }
+            int id = getid(pref[i]);
+            ans += query(id-1); // all prefix smaller
+            update(id);
         }
-        return res;
+        return ans;
     }
 };
