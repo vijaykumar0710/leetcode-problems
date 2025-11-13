6a356e63c82123ed7b1088afd58c18e41468bafb
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-11-14 02:31:46 +0530

    Time: 35 ms (37.86%), Space: 190.2 MB (41.85%) - LeetHub

--- 4098-longest-non-decreasing-subarray-after-replacing-at-most-one-element/4098-longest-non-decreasing-subarray-after-replacing-at-most-one-element.cpp
+++ 4098-longest-non-decreasing-subarray-after-replacing-at-most-one-element/4098-longest-non-decreasing-subarray-after-replacing-at-most-one-element.cpp
@@ -15,16 +15,16 @@ public:
             else R[i]=1;
         }
         int res=0;
-       for(int i=0;i<n;i++){
-           int left=(i>0)?L[i-1]:0;
-           int right=(i<n-1)?R[i+1]:0;
-           if(i>0 && i<n-1 && nums[i-1]<=nums[i+1]){
-               res=max(res,left+1+right);
-           }else{
-               res=max(res,left+1);
-               res=max(res,right+1);
-           }
-       }
-        return min(res,n);
+        for(int i=0;i<n;i++){
+            int left=(i>0)?L[i-1]:0;
+            int right=(i<n-1)?R[i+1]:0;
+            if(i>0 && i<n-1 && nums[i-1]<=nums[i+1]){
+                res=max(res,left+1+right);
+            }else{
+                res=max(res,left+1);
+                res=max(res,right+1);
+            }
+        }
+        return res;
     }
 };
