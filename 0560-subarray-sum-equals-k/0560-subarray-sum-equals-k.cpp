1f1ac5e56c723f6dcbeef719c4f68f016c4e94bc
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-12-05 21:46:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,12 +3,14 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int n=nums.size();
         unordered_map<int,int>mp;
-        int sum=0,cnt=0;
+        int pre=0,cnt=0;
         for(int i=0;i<n;i++){
-          sum+=nums[i];
-          if(sum==k) cnt++;
-          if(mp.count(sum-k)) cnt+=mp[sum-k];
-          mp[sum]++;
+            pre+=nums[i];
+            if(pre==k) cnt++;
+            if(mp.find(pre-k)!=mp.end()){
+                cnt++;
+            }
+            mp[pre]++;
         }
         return cnt;
     }
