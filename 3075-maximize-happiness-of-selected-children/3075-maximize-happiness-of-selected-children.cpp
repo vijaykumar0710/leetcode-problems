5ede356056c973c1103ab26943165f94918cfd8a
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-12-25 10:23:23 +0530

    Time: 221 ms (10.64%), Space: 110.3 MB (18.09%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& ha, int k) {
+        int n=ha.size();
+        long long res=0;
+        sort(ha.rbegin(),ha.rend());
+        for(int i=0;i<k;i++) res=(long long)res+((ha[i]-i>0)?ha[i]-i:0);
+        return res;
+    }
+};
