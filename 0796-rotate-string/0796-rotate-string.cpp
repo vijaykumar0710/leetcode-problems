f5177d7db21ccbd582ffdfd974a1c6f67ee23531
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-12-25 23:30:01 +0530

    Time: 0 ms (100%), Space: 8.2 MB (36.59%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int m=s.length();
+        int n=goal.length();
+        if(m!=n) return false;
+        if((s+s).find(goal)!=string::npos){
+           return true;
+        } 
+        return false;
+    }
+};
