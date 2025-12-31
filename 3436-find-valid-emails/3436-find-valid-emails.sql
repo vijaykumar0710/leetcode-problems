c7edffa9ef86b86ba1c19b9191e6e085e720e58c
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-12-31 23:06:23 +0530

    Time: 375 ms (79.51%), Space: 0B (100%) - LeetHub

--- 3436-find-valid-emails/3436-find-valid-emails.sql
+++ 3436-find-valid-emails/3436-find-valid-emails.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT *
 FROM Users
-WHERE email REGEXP '^[a-zA-z0-9_]+@[a-zA-Z]+[\.]com$'
+WHERE email REGEXP '^[a-zA-z0-9_]+@[a-zA-Z]+[.]com$'
 ORDER BY user_id; 
