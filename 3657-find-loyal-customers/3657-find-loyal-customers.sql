f8e1be0f9c9e541010acd9e21dbab446a93870b6
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-01-01 11:09:23 +0530

    Time: 289 ms (85.71%), Space: 0B (100%) - LeetHub

--- 3657-find-loyal-customers/3657-find-loyal-customers.sql
+++ 3657-find-loyal-customers/3657-find-loyal-customers.sql
@@ -2,7 +2,7 @@
 SELECT customer_id
 FROM customer_transactions 
 GROUP BY customer_id
-HAVING COUNT(customer_id)>=3 &&
+HAVING COUNT(*)>=3 &&
        DATEDIFF(MAX(transaction_date),MIN(transaction_date))>=30 &&
-      (SUM(IF(transaction_type="refund",1,0))*100/COUNT(customer_id))<20
+      (SUM(transaction_type="refund")*100/COUNT(customer_id))<20
 ORDER BY customer_id;
