f9cb9fde3bd335aa61b7c9e5508dae1a033a252d
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-01-03 16:41:23 +0530

    Time: 1142 ms (58.89%), Space: 0B (100%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -5,11 +5,8 @@ WITH
     FROM Orders
     WHERE YEAR(order_date)=2019
     GROUP BY buyer_id
-),
-cte2 AS(SELECT u.user_id AS buyer_id,u.join_date,c.cnt AS orders_in_2019
+)
+SELECT u.user_id AS buyer_id,u.join_date,IFNULL(c.cnt,0) AS orders_in_2019
 FROM Users u
 LEFT JOIN cte c
-ON u.user_id=c.buyer_id)
-
-SELECT buyer_id,join_date,COALESCE(orders_in_2019,0) AS orders_in_2019
-FROM cte2
+ON u.user_id=c.buyer_id
