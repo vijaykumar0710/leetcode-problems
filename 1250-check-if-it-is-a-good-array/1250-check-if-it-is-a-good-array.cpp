78dd217afb6eb7bad0ee7bfa8f75a4184c45f6d0
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-01-18 17:47:36 +0530

    Time: 11 ms (6.58%), Space: 33 MB (53.68%) - LeetHub

--- 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     bool isGoodArray(vector<int>& nums) {
-       int n=nums.size();
-       int hcf=nums[0];
-       for(int i=1;i<n;i++){
-        hcf=gcd(hcf,nums[i]);
-       }
+       int n=nums.size(),hcf=nums[0];
+       for(int i=1;i<n;i++) hcf=gcd(hcf,nums[i]);
        return hcf==1;
     }
 };
