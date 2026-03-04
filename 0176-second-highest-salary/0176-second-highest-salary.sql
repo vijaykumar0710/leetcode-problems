a13aa6fe6db87feb675b653452094d246fba8573
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-03-05 01:19:56 +0530

    Time: 273 ms (47.03%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,13 @@
 # Write your MySQL query statement below
-SELECT MAX(salary) as SecondHighestSalary
-FROM Employee
-WHERE salary<(
-    SELECT MAX(salary)
-    from Employee
-)
+WITH cte1 AS(
+    SELECT DISTINCT salary
+     FROM Employee 
+),
+cte2 AS (SELECT NTH_VALUE(salary,2) OVER(
+                   ORDER BY salary DESC
+                   ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+  ) AS sa
+FROM cte1)
+
+SELECT MAX(sa) as SecondHighestSalary
+FROM cte2;
