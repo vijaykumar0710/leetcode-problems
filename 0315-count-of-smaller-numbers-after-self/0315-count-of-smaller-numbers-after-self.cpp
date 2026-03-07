f046cd34e73554eb22301e8515076e95d3b4773a
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-03-07 12:35:53 +0530

    Time: 26 ms (96.54%), Space: 94.6 MB (80.53%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,52 +1,28 @@
 class Solution {
 public:
-    int query(int qs, int qe, int idx, int l, int r, vector<int>& seg) {
-        if (qe < l || qs > r)
-            return 0;
-        if (qs <= l && r <= qe)
-            return seg[idx];
-        int mid = l + (r - l) / 2;
-        return query(qs, qe, idx * 2 + 1, l, mid, seg) +
-               query(qs, qe, idx * 2 + 2, mid + 1, r, seg);
-    }
-
-    void update(int pos, int idx, int l, int r, vector<int>& seg) {
-        if (l == r) {
-            seg[idx]++;
-            return;
-        }
-        int mid = l + (r - l) / 2;
-        if (pos <= mid)
-            update(pos, idx * 2 + 1, l, mid, seg);
-        else
-            update(pos, idx * 2 + 2, mid + 1, r, seg);
-        seg[idx] = seg[idx * 2 + 1] + seg[idx * 2 + 2];
-    }
-    int getID(int& x, vector<int>& comp) {
-        return lower_bound(comp.begin(), comp.end(), x) - comp.begin();
-    }
+vector<int>res,BIT;
+int m;
+void update(int i){
+for(;i<=m;i+=i&-i) BIT[i]+=1;
+}
+int query(int i){
+int res=0;
+for(;i>0;i-=(i&-i)){
+    res+=BIT[i]; 
+  }
+  return res;
+}
     vector<int> countSmaller(vector<int>& nums) {
-        vector<int> a;
-        a = nums;
-        reverse(a.begin(), a.end());
-        vector<int> res;
-        int n = a.size();
-        vector<int> comp;
-        set<int> st(a.begin(), a.end());
-        for (auto x : st)
-            comp.push_back(x);
-        int sz = comp.size();
-        vector<int> seg(4 * sz, 0);
-        for (int i = 0; i < n; i++) {
-            int id = getID(a[i], comp);
-            int ans = 0;
-            if (id > 0) {
-                ans = query(0, id - 1, 0, 0, sz - 1, seg);
-            }
-            res.push_back(ans);
-            update(id, 0, 0, sz - 1, seg);
+        int n=nums.size();
+        res.resize(n);
+        int maxi=10001;
+        BIT.assign(2*maxi+5,0);
+        m=BIT.size();
+        for(int i=n-1;i>=0;i--){
+            int x=query(nums[i]+maxi);
+            res[i]=x;
+            update(nums[i]+maxi+1);
         }
-        reverse(res.begin(), res.end());
         return res;
     }
 };
