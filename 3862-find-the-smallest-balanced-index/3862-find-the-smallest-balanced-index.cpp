36176150066856dbd3ee3409c276570477b80f57
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-03-08 11:03:46 +0530

    Time: 18 ms (47.02%), Space: 209.5 MB (53.57%) - LeetHub

--- 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.cpp
+++ 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.cpp
@@ -3,14 +3,14 @@ public:
 const int M=1e9+7;
     int smallestBalancedIndex(vector<int>& nums) {
         int n=nums.size();
-        vector<int>p(n,0);
+        vector<double>p(n,0);
         p[0]=nums[0];
-        for(int i=1;i<n;i++) p[i]=(p[i-1]+nums[i])%M;
-        long long pro=1;
+        for(int i=1;i<n;i++) p[i]=(p[i-1]+nums[i]);
+        double pro=1;
         int res=-1;
         for(int i=n-1;i>0;i--){
          if(pro==p[i-1]) res=i;
-         pro=(1LL*pro%M*nums[i]%M)%M;
+         pro=(1LL*pro*nums[i]);
         }
         return res;
     }
