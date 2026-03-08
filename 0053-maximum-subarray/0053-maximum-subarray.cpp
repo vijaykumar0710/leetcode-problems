bd5b59326bea9d04982a4cd9b9f2987c27098cca
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-03-08 17:53:57 +0530

    Time: 26 ms (5.17%), Space: 72.8 MB (15.11%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
+int f(int l,int r,vector<int>&nums){
+    if(l==r) return nums[l];
+    int mid=l+(r-l)/2;
+    int left=f(l,mid,nums);
+    int right=f(mid+1,r,nums);
+    int left_cross_sum=INT_MIN;
+    int curr_sum=0;
+    for(int i=mid;i>=l;i--){
+        curr_sum+=nums[i];
+        left_cross_sum=max(left_cross_sum,curr_sum);
+    }
+    curr_sum=0;
+    int right_cross_sum=INT_MIN;
+    for(int i=mid+1;i<=r;i++){
+        curr_sum+=nums[i];
+        right_cross_sum=max(right_cross_sum,curr_sum);
+    }
+    int cross_sum=left_cross_sum+right_cross_sum;
+    return max({cross_sum,left,right});
+}
     int maxSubArray(vector<int>& nums) {
-        int n=nums.size();
-        int t=nums[0];
-        int res=nums[0];
-        for(int i=1;i<n;i++){
-            t=max(t+nums[i],nums[i]);
-            res=max(res,t);
-        }
-        return res;
+        return f(0,nums.size()-1,nums);
     }
 };
