28ef85f138570355e503f5563b8c83e9370abda6
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-03-08 15:42:15 +0530

    Time: 231 ms (89.97%), Space: 121.3 MB (80.67%) - LeetHub

---
+++ 0327-count-of-range-sum/0327-count-of-range-sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+long long maxn;
+vector<long long>BIT;
+void update(int i,int val){
+for(;i<=maxn;i+=(i&-i)) BIT[i]+=val;
+}
+int query(int i){
+    long long res=0;
+    for(;i>0;i-=(i&-i)) res+=BIT[i];
+    return res;
+}
+    int countRangeSum(vector<int>& nums, int lower, int upper) {
+        int n=nums.size();
+        vector<int>vec(n+1);
+        for(int i=0;i<n;i++) vec[i+1]=nums[i];
+        vector<long long>pre(n+1,0);
+        pre[1]=vec[1];
+        for(int i=2;i<=n;i++) pre[i]=pre[i-1]+vec[i];
+        vector<long long>temp;
+        for(auto p:pre){
+            temp.push_back(p);
+            temp.push_back(p-lower);
+            temp.push_back(p-upper);
+        }
+        sort(temp.begin(),temp.end());
+        temp.erase(unique(temp.begin(),temp.end()),temp.end());
+        maxn=temp.size()+5;
+        BIT.assign(maxn+1,0);
+        int res=0;
+        for(auto p:pre){
+            int up=lower_bound(temp.begin(),temp.end(),p-lower)-temp.begin()+1;
+            int lo=lower_bound(temp.begin(),temp.end(),p-upper)-temp.begin()+1;
+            int rank=lower_bound(temp.begin(),temp.end(),p)-temp.begin()+1;
+            res+=query(up)-query(lo-1);
+            update(rank,1);
+        }
+        return res;
+    }
+};
