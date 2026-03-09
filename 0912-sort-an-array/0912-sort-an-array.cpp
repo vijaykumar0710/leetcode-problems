30cdbdcb1234702fb038fdec68cc0732a4a13db8
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-03-09 16:46:48 +0530

    Time: 642 ms (24.09%), Space: 295.6 MB (17.27%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-vector<int>f(int l,int r,vector<int>&nums){
-    if(l==r) return {nums[l]};
+void f(int l,int r,vector<int>&nums){
+    if(l>=r) return;
     int mid=l+(r-l)/2;
-    vector<int>left=f(l,mid,nums);
-    vector<int>right=f(mid+1,r,nums);
-    int i=0,j=0;
-    int n1=left.size(),n2=right.size();
+    f(l,mid,nums);
+    f(mid+1,r,nums);
+    int i=l,j=mid+1;
     vector<int>res;
-    while(i<n1 && j<n2){
-        if(left[i]<right[j]){
-            res.push_back(left[i]);
-            i++;
+    while(i<=mid && j<=r){
+        if(nums[i]<nums[j]){
+            res.push_back(nums[i++]);
         }else{
-            res.push_back(right[j]);
-            j++;
+            res.push_back(nums[j++]);
         }
     }
-    while(i<n1) res.push_back(left[i]),i++;
-    while(j<n2)  res.push_back(right[j]),j++;
-    return res;
+    while(i<=mid) res.push_back(nums[i++]);
+    while(j<=r)  res.push_back(nums[j++]);
+    for(int k=0;k<res.size();k++) nums[k+l]=res[k];
 }
     vector<int> sortArray(vector<int>& nums) {
-        return f(0,nums.size()-1,nums);
+        f(0,nums.size()-1,nums);
+        return nums;
     }
 };
