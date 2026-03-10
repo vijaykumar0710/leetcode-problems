d4a6a793bc2d2a1e188d336b062ef7003cabb287
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-03-10 20:17:32 +0530

    Time: 0 ms (100%), Space: 11.5 MB (63.72%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n=s.size();
-        unordered_map<char,int>mp;
-        for(int i=0;i<t.size();i++){
-            mp[t[i]]++;
-        }
-        int cnt=mp.size();
+        int m=s.size(),n=t.size();
+        vector<int>freq(128,0);
+        int req=0;
+        for(auto ch:t) freq[ch]++,req++;
+        int idx=-1,min_len=INT_MAX;
         int i=0;
-        int sizeOfWindow=n+1,startIdx=-1;
-        for(int j=0;j<n;j++){
-            if(mp.find(s[j])!=mp.end()){
-                mp[s[j]]--;
-                if(mp[s[j]]==0)
-                   cnt--;
+        for(int j=0;j<m;j++){
+            if(freq[s[j]]>0) req--;
+            freq[s[j]]--;
+            while(req==0){
+                if(j-i+1<min_len){
+                    idx=i;
+                    min_len=j-i+1;
+                }
+                freq[s[i]]++;
+                if(freq[s[i]]>0) req++;
+                i++;
             }
-                while(cnt==0){
-                    if(sizeOfWindow>(j-i+1)){
-                    sizeOfWindow=j-i+1;
-                    startIdx=i;
-                    }
-                    if(mp.find(s[i])!=mp.end()){
-                       mp[s[i]]++;
-                       if(mp[s[i]]>0) cnt++;
-                    }
-                    i++;
-             }
         }
-        if(startIdx==-1) return "";
-        return s.substr(startIdx,sizeOfWindow);
+        return idx==-1?"":s.substr(idx,min_len);
     }
 };
