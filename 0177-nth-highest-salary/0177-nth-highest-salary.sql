8b4210e8b109f41530fb625550da86711c66d7ca
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-03-10 22:26:31 +0530

    Time: 431 ms (55.67%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -3,11 +3,9 @@ BEGIN
 DECLARE offset_val INT;
 SET offset_val=N-1;
   RETURN (
-      select IFNULL((
-        select distinct salary 
-        from Employee 
-        ORDER BY salary DESC
-        LIMIT 1 OFFSET offset_val
-      ),null) as getNthHighestSalary
+SELECT IFNULL((SELECT DISTINCT salary
+FROM Employee
+ORDER BY salary DESc
+LIMIT 1 OFFSET offset_val),NULL) AS 'getNthHighestSalary'
   );
 END
