795e3912581c1fbd42a6257e7ff24f357f0defd9
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-03-14 00:59:43 +0530

    Time: 4 ms (79.38%), Space: 25 MB (24.23%) - LeetHub

--- 2207-maximize-number-of-subsequences-in-a-string/2207-maximize-number-of-subsequences-in-a-string.cpp
+++ 2207-maximize-number-of-subsequences-in-a-string/2207-maximize-number-of-subsequences-in-a-string.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
+    long long f(string& s, string pattern, long long schar) {
+        int m = s.size();
+        long long cnt = 0;
+        long long pcnt = 0;
+        for (int i = 0; i <= m; i++) {
+            if (s[i] == pattern[1])
+                pcnt++;
+            if (s[i] == pattern[0])
+                cnt += (schar - pcnt);
+        }
+        return cnt;
+    }
     long long maximumSubsequenceCount(string text, string pattern) {
-        int m=text.size();
-        long long schar=0;
-        for(auto ch:text) if(ch==pattern[1]) schar++;
-    string s1=pattern[0]+text;
-    long long res=INT_MIN,cnt=0;
-    long long pcnt=0;
-    for(int i=0;i<=m;i++){
-    if(s1[i]==pattern[1]) pcnt++;
-    if(s1[i]==pattern[0]) cnt+=(schar-pcnt);
-      }
-    res=max(res,cnt);
-    s1=text+pattern[1];
-    schar++;
-    pcnt=0;
-    cnt=0;
-    for(int i=0;i<=m;i++){
-    if(s1[i]==pattern[1]) pcnt++;
-    if(s1[i]==pattern[0]) cnt+=(schar-pcnt);
-      }
-      res=max(res,cnt);
-      return res;
+        int m = text.size();
+        long long res = INT_MIN;
+        long long schar = 0;
+        for (auto ch : text)
+            if (ch == pattern[1])
+                schar++;
+        string s1 = pattern[0] + text;
+        res = max(res, f(s1, pattern, schar));
+        s1 = text + pattern[1];
+        schar++;
+        res = max(res, f(s1, pattern, schar));
+        return res;
     }
 };
