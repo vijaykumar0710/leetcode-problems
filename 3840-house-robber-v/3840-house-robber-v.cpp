640b0b99ace98bc6c10778954c8ccad22f92da77
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-03-14 11:29:31 +0530

    Time: 7 ms (65.38%), Space: 244.5 MB (71.48%) - LeetHub

--- 3840-house-robber-v/3840-house-robber-v.cpp
+++ 3840-house-robber-v/3840-house-robber-v.cpp
@@ -3,12 +3,13 @@ public:
     long long rob(vector<int>& nums, vector<int>& colors) {
       int n=nums.size();
       if(n==1) return nums[0];
-      vector<long long>t;
-      for(int i=0;i<n;i++) t.push_back(nums[i]);
-      if(colors[0]!=colors[1]) t[1]+=nums[0];
+      vector<long long>t(n,0);
+      t[0]=nums[0];
+      if(colors[0]!=colors[1]) t[1]=nums[0]+nums[1];
+      else t[1]=nums[1];
       for(int i=2;i<n;i++){
-        if(colors[i]==colors[i-1]) t[i]+=max(t[i-2],(t[i-1]-nums[i-1]));
-        else t[i]+=max(t[i-1],t[i-2]);
+        if(colors[i]==colors[i-1]) t[i]=nums[i]+max(t[i-2],t[i-1]-nums[i-1]);
+        else t[i]=nums[i]+max(t[i-1],t[i-2]);
       }
       return max(t[n-1],t[n-2]);
     }
