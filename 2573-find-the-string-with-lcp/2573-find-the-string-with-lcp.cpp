2b29c774ea1fbc81cc72b352014b596385f37022
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-03-28 17:28:59 +0530

    Time: 14 ms (53.01%), Space: 85.2 MB (22.89%) - LeetHub

---
+++ 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string findTheString(vector<vector<int>>& lcp) {
+    int n = lcp.size();
+    if (n == 0) return "";    
+    string s(n, ' '); 
+    for (int i = 0; i < n; i++) {
+        if (s[i] != ' ') continue; 
+        char c = 'a';
+        while (c <= 'z') {
+            bool valid = true;
+            for (int j = 0; j < i; j++) {
+                if (lcp[i][j] == 0 && s[j] == c) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) break;
+            c++;
+        }
+        if (c > 'z') return ""; 
+        s[i] = c;
+        for (int j = i + 1; j < n; j++) {
+            if (lcp[i][j] > 0) {
+                if (s[j] != ' ' && s[j] != c) return ""; 
+                s[j] = c;
+            }
+        }
+    }
+    vector<vector<int>> actual_lcp(n + 1, vector<int>(n + 1, 0));    
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = n - 1; j >= 0; j--) {
+            if (s[i] == s[j]) actual_lcp[i][j] = actual_lcp[i + 1][j + 1] + 1;
+            else  actual_lcp[i][j] = 0;
+            if (actual_lcp[i][j] != lcp[i][j]) return ""; 
+        }
+    }
+    return s;
+    }
+};
