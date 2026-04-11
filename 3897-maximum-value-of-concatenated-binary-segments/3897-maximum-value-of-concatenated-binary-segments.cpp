02c53a31dc0d57637ed293fb476d75d7ac3222bf
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-04-12 00:53:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3897-maximum-value-of-concatenated-binary-segments/3897-maximum-value-of-concatenated-binary-segments.cpp
+++ 3897-maximum-value-of-concatenated-binary-segments/3897-maximum-value-of-concatenated-binary-segments.cpp
@@ -1,46 +1,70 @@
 class Solution {
 public:
-    const int mod = 1e9 + 7;
-    long long find(long long base, long long exp) {
+    const int M = 1e9 + 7;
+        long long power(long long a, long long b) {
         long long res = 1;
-        while (exp > 0) {
-            if (exp % 2 == 1)
-                res = res * base % mod;
-            base = base * base % mod;
-            exp /= 2;
+        a %= M;
+        while (b > 0) {
+            if (b % 2 == 1) res = (res * a) % M;
+            a = (a * a) % M;
+            b /= 2;
         }
-        return res % mod;
+        return res;
     }
-    int maxValue(vector<int>& nums1, vector<int>& nums0) {
-        vector<pair<int, int>> a;
-        long long n = nums1.size(), ans = 0;
-        int last = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums0[i] == 0) {
-                last += nums1[i];
-            } else {
-                a.push_back({nums1[i], nums0[i]});
+    static vector<long long> normalize(long long a, long long b, long long c) {
+        long long arr[3] = {a, b, c};
+        sort(arr, arr + 3); 
+        
+        if (arr[0] == arr[1] && arr[1] == arr[2]) {
+            return {arr[0] + 1, arr[0]};
+        } else if (arr[0] == arr[1]) {
+            long long x = arr[0] + 1;
+            long long y = arr[2];
+            if (x == y) return {x + 1};
+            else return {max(x, y), min(x, y)};
+        } else if (arr[1] == arr[2]) {
+            long long x = arr[0];
+            long long y = arr[1] + 1;
+            return {max(x, y), min(x, y)};
+        } else {
+            return {arr[2], arr[1], arr[0]}; 
+        }
+    }
+   static bool compareSegs(const pair<int, int>& A, const pair<int, int>& B) {
+        long long LA = A.first + A.second;
+        long long LB = B.first + B.second;
+        long long zA = A.second;
+        long long zB = B.second;
+        vector<long long> LHS = normalize(LA + zB, LB, zA);
+        vector<long long> RHS = normalize(LB + zA, LA, zB);       
+        for (int i = 0; i < min(LHS.size(), RHS.size()); i++) {
+            if (LHS[i] != RHS[i]) {
+                return LHS[i] > RHS[i];
             }
         }
-        sort(a.begin(), a.end(), [](auto& a1, auto& a2) {
-            if (a1.first == a2.first)
-                return a1.second < a2.second;
-            return a1.first > a2.first;
-        });
-        long long exp = 0;
-        for (int i = a.size() - 1; i >= 0; i--) {
-            auto [one, zero] = a[i];
-            exp += zero;
-            long long first = find(2, exp);
-            long long rn = find(2, one);
-            long long val = (first * (rn - 1 + mod) % mod) % mod;
-            ans = (ans + val) % mod;
-            exp += one;
+        return LHS.size() > RHS.size();
+    }
+    int maxValue(vector<int>& nums1, vector<int>& nums0) {
+        int n = nums1.size();
+        vector<pair<int, int>> nums;
+            for (int i = 0; i < n; i++) {
+            nums.push_back({nums1[i], nums0[i]});
         }
-        long long first = find(2, exp);
-        long long rn = find(2, last);
-        long long val = (first * (rn - 1 + mod) % mod) % mod;
-        ans = (ans + val) % mod;
-        return ans;
+        sort(nums.begin(), nums.end(), compareSegs);
+        long long res = 0;
+        for (int i = 0; i < n; i++) {
+            int ones = nums[i].first;
+            int zeros = nums[i].second;
+                if (ones > 0) {
+                long long p_ones = power(2, ones);
+                res = (res * p_ones) % M;
+                res = (res + p_ones - 1 + M) % M; 
+            }
+            if (zeros > 0) {
+               long long p_zeros = power(2, zeros);
+               res = (res * p_zeros) % M;
+            }
+        }
+        return res;
     }
 };
