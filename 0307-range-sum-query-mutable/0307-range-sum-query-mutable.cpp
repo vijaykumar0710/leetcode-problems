8d0a5a4a35a2f4beb8bb9349df2d5ff0e8ec1645
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-04-11 11:36:03 +0530

    Time: 137 ms (10.69%), Space: 179.4 MB (95.71%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -1,32 +1,43 @@
 class NumArray {
 public:
-vector<int>BIT,vec;
-int n;
-void updateBIT(int i,int val){
-    for(;i<=n;i+=(i&-i)) BIT[i]+=val;
-}
-int query(int i){
-    int sum=0;
-    for(;i>0;i-=(i&-i)){
-        sum+=BIT[i];
-    }
-    return sum;
-}
+vector<int>vec,block;
+int n,blocksize;
     NumArray(vector<int>& nums) {
         n=nums.size();
-        BIT.assign(n+1,0);
-        vec.resize(n+1,0);
-        for(int i=1;i<=n;i++) vec[i]=nums[i-1],updateBIT(i,nums[i-1]);
+        vec=nums;
+        blocksize=ceil(sqrt(n));
+        block.resize(blocksize);
+        for(int i=0;i<n;i++){
+            block[i/blocksize]+=nums[i];
+        }
     }
     
     void update(int index, int val) {
-        index++;
-        updateBIT(index,val-vec[index]);
+        int block_idx=index/blocksize;
+        block[block_idx]=(block[block_idx]-vec[index]+val);
         vec[index]=val;
     }
     
-    int sumRange(int left, int right) {
-        left++,right++;
-        return query(right)-query(left-1);
+    int sumRange(int l, int r) {
+        int sum=0;
+        int startblock=l/blocksize;
+        int endblock=r/blocksize;
+        if(startblock==endblock){
+            for(int i=l;i<=r;i++) sum+=vec[i];
+        }else{
+            int endidx=((startblock+1)*blocksize)-1;
+            for(int i=l;i<=endidx;i++) sum+=vec[i];
+            for(int i=startblock+1;i<=endblock-1;i++) sum+=block[i];
+            int startidx=endblock*blocksize;
+            for(int i=startidx;i<=r;i++) sum+=vec[i];
+        }
+        return sum;
     }
 };
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
