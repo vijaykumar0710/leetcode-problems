50b3a196f6c4054b6ebc30f0962905ba7c8a11b4
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-04-20 19:59:13 +0530

    Time: 0 ms (100%), Space: 52.3 MB (56.36%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         int maxi=0;
         for(int i=0;i<n-1;i++){
-           if(nums[i]==0 && i==maxi) return false;
+           if(nums[i]==0 && maxi<=i) return false;
            maxi=max(maxi,nums[i]+i);
         }
         return true;
