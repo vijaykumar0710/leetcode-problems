ae89117b9fdd4784514751947e7774920ed41c18
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-05-02 09:56:52 +0530

    Time: 0 ms (100%), Space: 7.8 MB (83.68%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool valid(int num) {
-        bool flag1=false,flag2=false;
+        bool flag2=false;
         while (num) {
             int x = num % 10;
             if(x==3 || x==4 || x==7) return false;
-            if(x==0 || x==1 || x==8) flag1=true;
             if (x==2 || x==5 || x==6 || x==9) flag2=true;
             num/=10;
         }
