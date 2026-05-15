59b5bb6b6ab551e480eb0c52e828b5e02ca239d8
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-05-15 22:17:40 +0530

    Time: 1 ms (13.54%), Space: 9.3 MB (53.13%) - LeetHub

--- 1864-minimum-number-of-swaps-to-make-the-binary-string-alternating/1864-minimum-number-of-swaps-to-make-the-binary-string-alternating.cpp
+++ 1864-minimum-number-of-swaps-to-make-the-binary-string-alternating/1864-minimum-number-of-swaps-to-make-the-binary-string-alternating.cpp
@@ -1,5 +1,48 @@
 class Solution {
 public:
+    int odd(string& s,int ones,int zeros) {
+        int n=s.size();
+        char ch;
+        if (ones > zeros)
+            ch = '1';
+        else
+            ch = '0';
+        int op1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] != ch)
+                op1++;
+            if (ch == '1')
+                ch = '0';
+            else
+                ch = '1';
+        }
+        return op1;
+    }
+    int even(string& s) {
+        int n=s.size();
+        int op1 = 0;
+        char ch = '1';
+        int cnt_zeor = 0, cnt_ones = 1;
+        for (int i = 0; i < n; i++) {
+            if (s[i] != ch)
+                op1++;
+            if (ch == '1')
+                ch = '0';
+            else
+                ch = '1';
+        }
+        int op2 = 0;
+        ch = '0';
+        for (int i = 0; i < n; i++) {
+            if (s[i] != ch)
+                op2++;
+            if (ch == '1')
+                ch = '0';
+            else
+                ch = '1';
+        }
+        return min(op1, op2);
+    }
     int minSwaps(string s) {
         int n = s.size();
         int ones = count(s.begin(), s.end(), '1');
@@ -11,48 +54,11 @@ public:
             if (abs(ones - zeros) > 1)
                 return -1;
         }
-        char ch;
         int res = INT_MAX;
-        if (n % 2 == 1) {
-            if (ones > zeros)
-                ch = '1';
-            else
-                ch = '0';
-            int op1 = 0;
-            for (int i = 0; i < n; i++) {
-                if (s[i] != ch)
-                    op1++;
-                if (ch == '1')
-                    ch = '0';
-                else
-                    ch = '1';
-            }
-            res = min(res, op1);
-        } else {
-            int op1 = 0;
-            char ch = '1';
-            int cnt_zeor = 0, cnt_ones = 1;
-            for (int i = 0; i < n; i++) {
-                if (s[i] != ch)
-                    op1++;
-                if (ch == '1')
-                    ch = '0';
-                else
-                    ch = '1';
-            }
-            int op2 = 0;
-            ch = '0';
-            for (int i = 0; i < n; i++) {
-                if (s[i] != ch)
-                    op2++;
-                if (ch == '1')
-                    ch = '0';
-                else
-                    ch = '1';
-            }
-            res = min({res, op1, op2});
-        }
-
+        if (n % 2 == 1)
+            res = min(res, odd(s,ones,zeros));
+        else
+            res = min(res, even(s));
         return res / 2;
     }
 };
