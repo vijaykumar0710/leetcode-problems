7f0f1177251d30df9bf131cba17cb9f4e1d8e65f
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-05-25 12:16:04 +0530

    Time: 0 ms (100%), Space: 33.2 MB (80%) - LeetHub

--- 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.cpp
+++ 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<int> limitOccurrences(vector<int>& nums, int k) {
-        int n = nums.size();
-        int cnt = 1;
-        int i = 0;
-        while (i < n-1) {
-            if (cnt >= k) {
-                while (i+1<n && nums[i] == nums[i + 1]) {
-                    nums[i] = INT_MAX;
+        int n=nums.size();
+        int i=0;
+        int freq=1;
+        while(i<n-1){
+            if(freq>=k){
+                while(i+1<n && nums[i]==nums[i+1]){
+                    nums[i]=INT_MAX;
                     i++;
                 }
             }
-            if (i+1<n && nums[i] == nums[i + 1])
-                cnt++;
-            else
-                cnt = 1;
-         i++;
+            if(i+1<n && nums[i]==nums[i+1]) freq++;
+            else freq=1;
+            i++;
         }
-        sort(nums.begin(),nums.end());
-        int cnt1=count(nums.begin(),nums.end(),INT_MAX);
-        int sz=n-cnt1;
+        sort(nums.begin(),nums.end());;
+        int cnt=count(nums.begin(),nums.end(),INT_MAX);
+        int sz=n-cnt;
         nums.resize(sz);
         return nums;
     }
