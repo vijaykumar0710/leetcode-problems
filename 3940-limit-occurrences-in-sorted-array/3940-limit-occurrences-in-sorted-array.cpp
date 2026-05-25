4bd188f51901444c383867d9a19cca27888720b1
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-05-25 12:49:55 +0530

    Time: 0 ms (100%), Space: 33.3 MB (60%) - LeetHub

--- 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.cpp
+++ 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     vector<int> limitOccurrences(vector<int>& nums, int k) {
         int n=nums.size();
-        int i=0;
-        int freq=1;
+        int i=0,freq=1;
         while(i<n-1){
             if(freq>=k){
                 while(i+1<n && nums[i]==nums[i+1]){
@@ -15,10 +14,10 @@ public:
             else freq=1;
             i++;
         }
-        sort(nums.begin(),nums.end());;
+        sort(nums.begin(),nums.end());
         int cnt=count(nums.begin(),nums.end(),INT_MAX);
         int sz=n-cnt;
-        nums.resize(sz);
-        return nums;
+         nums.resize(sz);
+         return nums;
     }
 };
