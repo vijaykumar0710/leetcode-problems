d0b3a2816958cdf0c9fa3034fa82fd6105f85fe1
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-05-28 08:32:39 +0530

    Time: 868 ms (38.28%), Space: 316.5 MB (84.88%) - LeetHub

--- 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.cpp
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.cpp
@@ -1,57 +1,39 @@
 class Solution {
 public:
+const long long b = 31;
+    unordered_map<unsigned long long, int> m;
+    void update(unsigned long long hash, int idx, vector<string>& wordsContainer) {
+        if (m.find(hash) == m.end()) {
+            m[hash] = idx;
+        } else { 
+            int curr = m[hash];
+            if (wordsContainer[idx].length() < wordsContainer[curr].length()) {
+                m[hash] = idx;
+            }
+        }
+    }
     vector<int> stringIndices(vector<string>& wordsContainer, vector<string>& wordsQuery) {
-        unordered_map<uint64_t, int> m;
-        long long mod1 = 1000000007, mod2 = 1000000009;
-        long long b1 = 31, b2 = 37;
-        auto update = [&](uint64_t h, int idx) {
-            if (m.find(h) == m.end()) {
-                m[h] = idx;
-            } else {
-                int curr = m[h];
-                if (wordsContainer[idx].length() < wordsContainer[curr].length()) {
-                    m[h] = idx;
-                }
-            }
-        };
-        for (int i = 0; i < wordsContainer.size(); ++i) {
-            long long h1 = 0, h2 = 0;
-            long long p1 = 1, p2 = 1;
-            update(0, i);
-            for (int j = wordsContainer[i].length() - 1; j >= 0; --j) {
-                long long val = wordsContainer[i][j] - 'a' + 1;
-                h1 = (h1 + val * p1) % mod1;
-                h2 = (h2 + val * p2) % mod2;
-                p1 = (p1 * b1) % mod1;
-                p2 = (p2 * b2) % mod2;
-                update((h1 << 32) | h2, i);
+        for (int k = 0; k < wordsContainer.size(); k++) {
+            unsigned long long  hash=0;
+            update(0, k, wordsContainer); 
+            for (int i = wordsContainer[k].size() - 1; i >= 0; i--) {
+                 hash=(hash*b+wordsContainer[k][i]);
+                update(hash, k, wordsContainer);
             }
         }
-
-        vector<int> ans;
-        ans.reserve(wordsQuery.size());
-        for (const string& q : wordsQuery) {
-            vector<uint64_t> hlist;
-            hlist.reserve(q.length() + 1);
-            hlist.push_back(0);
-            long long h1 = 0, h2 = 0;
-            long long p1 = 1, p2 = 1;
-            for (int j = q.length() - 1; j >= 0; --j) {
-                long long val = q[j] - 'a' + 1;
-                h1 = (h1 + val * p1) % mod1;
-                h2 = (h2 + val * p2) % mod2;
-                p1 = (p1 * b1) % mod1;
-                p2 = (p2 * b2) % mod2;
-                hlist.push_back((h1 << 32) | h2);
-            }
-            
-            for (int j = hlist.size() - 1; j >= 0; --j) {
-                if (m.find(hlist[j]) != m.end()) {
-                    ans.push_back(m[hlist[j]]);
-                    break;
+        vector<int> res(wordsQuery.size(), 0);
+            for (int i = 0; i < wordsQuery.size(); i++) {
+            res[i] = m[0]; 
+            unsigned long long hash=0;
+            for (int j = wordsQuery[i].size() - 1; j >= 0; j--) {
+                 hash=(hash*b+wordsQuery[i][j]);
+                if (m.count(hash)) {
+                    res[i] = m[hash]; 
+                } else {
+                    break; 
                 }
             }
         }
-        return ans;
+        return res;
     }
 };
