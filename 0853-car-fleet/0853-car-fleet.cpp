02f58435d7d7e599c667ddb969ee3b788b92d64a
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-01 06:41:39 +0530

    Time: 32 ms (91.06%), Space: 104.1 MB (79.68%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -6,9 +6,8 @@ public:
         for(int i=0;i<n;i++) pair.push_back({position[i],speed[i]});
         sort(pair.begin(),pair.end());
         int cnt=0;
-        double mini=double(target-pair[n-1].first)/pair[n-1].second;
-        if(mini!=0.0) cnt++;
-        for(int i=n-2;i>=0;i--){
+        double mini=0.0;
+        for(int i=n-1;i>=0;i--){
             if((double(target-pair[i].first))/pair[i].second>mini){
                 cnt++;
             mini=double(target-pair[i].first)/pair[i].second;
