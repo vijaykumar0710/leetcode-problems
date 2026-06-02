ffe12d19b2d517accb298c2130e51cb4a0cc272e
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-02 19:22:21 +0530

    Time: 105 ms (26.5%), Space: 114.7 MB (27.35%) - LeetHub

--- 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.cpp
+++ 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-long long t[100005][5];
-long long f(int i,int sign,vector<int>& nums,int n){
-if(i>=n) return 0;
-if(t[i][sign+2]!=-1) return t[i][sign+2];
-return t[i][sign+2]=nums[i]*sign+max(f(i+1,1,nums,n),f(i+1,-1*sign,nums,n));
-}
     long long maximumTotalCost(vector<int>& nums) {
         int n=nums.size();
-        memset(t,-1,sizeof(t));
-        return f(0,1,nums,n);
+        vector<vector<long long>> dp(n + 1, vector<long long>(2, 0));
+        for(int i=n-1;i>=0;i--){
+            dp[i][1]=nums[i]+max(dp[i+1][1],dp[i+1][0]);
+            dp[i][0]=-nums[i]+dp[i+1][1];
+        }
+        return dp[0][1];
     }
 };
