5a91d6b098ad15d64eb122893812b9c52dcf3022
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-02 09:03:57 +0530

    Time: 5 ms (58.65%), Space: 91.7 MB (85.65%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int earliestFinishTime(vector<int>& ls, vector<int>& ld, vector<int>& ws, vector<int>& wd) {
         int m=ls.size(),n=ws.size();
-       int mini=INT_MAX;
-       for(int i=0;i<m;i++){
-        int d1=ls[i]+ld[i];
-        for(int j=0;j<n;j++){
-            if(ws[j]<=d1) mini=min(mini,d1+wd[j]);
-            else mini=min(mini,d1+wd[j]+ws[j]-d1);
+        int res=INT_MAX;
+        for(int i=0;i<m;i++){
+            int t=ls[i]+ld[i];
+            for(int j=0;j<n;j++){
+                if(ws[j]<=t) res=min(res,t+wd[j]);
+                else res=min(res,ws[j]+wd[j]);
+            }
         }
-       }
-       for(int i=0;i<n;i++){
-        int d1=ws[i]+wd[i];
-        for(int j=0;j<m;j++){
-            if(ls[j]<=d1) mini=min(mini,d1+ld[j]);
-            else mini=min(mini,d1+ld[j]+ls[j]-d1);
+        for(int i=0;i<n;i++){
+            int t=ws[i]+wd[i];
+            for(int j=0;j<m;j++){
+                if(ls[j]<=t) res=min(res,t+ld[j]);
+                else res=min(res,ls[j]+ld[j]);
+            }
         }
-       }
-       return mini;
-    } 
+        return res;
+    }
 };
