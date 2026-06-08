81f9680ba0f6d2055e2d88a0ebaa686058569d79
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-08 11:59:39 +0530

    Time: 401 ms (13.33%), Space: 202 MB (6.67%) - LeetHub

--- 3956-maximum-sum-of-m-non-overlapping-subarrays-i/3956-maximum-sum-of-m-non-overlapping-subarrays-i.cpp
+++ 3956-maximum-sum-of-m-non-overlapping-subarrays-i/3956-maximum-sum-of-m-non-overlapping-subarrays-i.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
-    const long long NEG = -1e18;
+long long NEG=-1e14;
     long long maximumSum(vector<int>& nums, int m, int l, int r) {
-        int n = nums.size();
-        vector<long long> prefix(n + 1, 0);
-        for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + nums[i];
-        }
-        vector<vector<long long>> dp(m + 1, vector<long long>(n + 1, NEG));
-        for (int i = 0; i <= n; i++) {
-            dp[0][i] = 0;
-        }
-        long long answer = NEG;
-        for (int k = 1; k <= m; k++) {
-            deque<int> dq;
-            for (int i = 1; i <= n; i++) {
-                dp[k][i] = dp[k][i - 1];
-                int addIndex = i - l;
-                if (addIndex >= 0){
-                    while (!dq.empty() && (dp[k - 1][dq.back()] - prefix[dq.back()]) <= (dp[k - 1][addIndex] - prefix[addIndex])) {
-                        dq.pop_back();
-                    }
-                    dq.push_back(addIndex);
+        int n=nums.size();
+        vector<long long>prefix(n+1,0);
+        for(int i=0;i<n;i++) prefix[i+1]=prefix[i]+nums[i];
+        vector<vector<long long>>dp(m+1,vector<long long>(n+1,NEG));
+        for(int i=0;i<=n;i++) dp[0][i]=0;
+        long long res=NEG;
+        for(int k=1;k<=m;k++){
+            deque<int>dq;
+            for(int i=1;i<=n;i++){
+                dp[k][i]=dp[k][i-1];
+                int l_idx=i-r,r_idx=i-l;
+                if(r_idx>=0){
+                    long long val=dp[k-1][r_idx]+prefix[i]-prefix[r_idx];
+                    while(!dq.empty() && (dp[k-1][dq.back()]+prefix[i]-prefix[dq.back()])<=val) 
+                         dq.pop_back();
+                    dq.push_back(r_idx);
                 }
-                int leftLimit = i - r;
-                while (!dq.empty() && dq.front() < leftLimit) {
-                    dq.pop_front();
+                while(!dq.empty() && dq.front()<l_idx) dq.pop_front();
+                if(!dq.empty()){
+                    int idx=dq.front();
+                    dp[k][i]=max(dp[k][i],dp[k-1][idx]+prefix[i]-prefix[idx]);
                 }
-                if (!dq.empty()) {
-                    int bestJ = dq.front();
-                    dp[k][i] = max(
-                        dp[k][i],
-                        prefix[i] + dp[k - 1][bestJ] - prefix[bestJ]
-                    );
-                }
+                res=max(res,dp[k][i]);
             }
-            answer = max(answer, dp[k][n]);
         }
-        return answer;
+        return res;
     }
 };
