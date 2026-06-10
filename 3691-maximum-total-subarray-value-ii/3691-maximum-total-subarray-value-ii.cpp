487d615bd8828878ab73df5aa8808a0220bbb1dc
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-10 08:37:47 +0530

    Time: 1395 ms (8.06%), Space: 337.2 MB (17.74%) - LeetHub

---
+++ 3691-maximum-total-subarray-value-ii/3691-maximum-total-subarray-value-ii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+vector<pair<int,int>>segTree;
+void buildSeg(vector<int>&nums,int i,int l,int r){
+if(l==r){
+    segTree[i]={nums[l],nums[l]};
+    return;
+}
+int mid=l+(r-l)/2;
+buildSeg(nums,2*i+1,l,mid);
+buildSeg(nums,2*i+2,mid+1,r);
+segTree[i] = {max(segTree[2 * i + 1].first, segTree[2 * i + 2].first), 
+              min(segTree[2 * i + 1].second, segTree[2 * i + 2].second)};
+}
+pair<int,int> query(int st,int end,int i,int l,int r){
+    if(l>end || r<st) return {INT_MIN, INT_MAX};
+    if(l>=st && r<=end) return segTree[i];
+    int mid = l + (r - l) / 2;
+    pair<int, int> left = query(st, end, 2 * i + 1, l, mid);
+    pair<int, int> right = query(st, end, 2 * i + 2, mid + 1, r);
+    return {max(left.first, right.first),min(left.second, right.second)};
+}
+long long f(vector<int>&nums,int i,int j){
+    int n=nums.size();
+   pair<int,int> p=query(i,j,0,0,n-1);
+   return (long long)p.first-p.second;
+}
+    long long maxTotalValue(vector<int>& nums, int k) {
+        int n=nums.size();
+        segTree.assign(4*n,{0,0});
+        buildSeg(nums,0,0,n-1);
+        priority_queue<vector<long long>>pq;
+        set<pair<int,int>>st;
+        long long val=f(nums,0,n-1);
+        pq.push({val,0,n-1});
+        st.insert({0,n-1});
+        long long res=0;
+        while(k--){
+            auto vec=pq.top();
+            pq.pop();
+            res+=vec[0];
+            if(vec[1]<vec[2]){
+            if(!st.count({vec[1]+1,vec[2]})){
+                val=f(nums,vec[1]+1,vec[2]);
+                pq.push({val,vec[1]+1,vec[2]});
+                st.insert({vec[1]+1,vec[2]});
+            }
+            if(!st.count({vec[1],vec[2]-1})){
+                val=f(nums,vec[1],vec[2]-1);
+                pq.push({val,vec[1],vec[2]-1});
+                st.insert({vec[1],vec[2]-1});
+            }
+        }
+        }
+        return res;
+    }
+};
