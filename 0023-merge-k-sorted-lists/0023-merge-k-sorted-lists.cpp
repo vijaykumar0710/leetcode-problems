8fd990a1ce866373bee285c139237265e64bd3c0
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-13 14:14:42 +0530

    Time: 5 ms (31.05%), Space: 18.7 MB (36.29%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-ListNode* f(ListNode* a,ListNode* b){
- ListNode* dummy=new ListNode(-1);
- ListNode* temp=dummy;
- while(a && b){
-    if(a->val<b->val){
-        temp->next=a;
-        a=a->next;
-    }else{
-        temp->next=b;
-        b=b->next;
-    }
-    temp=temp->next;
- }
- if(a) temp->next=a;
- if(b) temp->next=b;
- return dummy->next;
-}
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if(lists.size() == 0) return NULL;
-        ListNode* res=lists[0];
-        for(auto i=1;i<lists.size();i++){
-            res=f(lists[i],res);
-        }
-        return res;
+        priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>>pq;
+        for(auto x:lists) if(x) pq.push({x->val,x});
+        ListNode* dummy=new ListNode(-1);
+        ListNode* it=dummy;
+        while(!pq.empty()){
+          it->next=pq.top().second;
+          if(pq.top().second->next) pq.push({pq.top().second->next->val,pq.top().second->next});
+          pq.pop();
+          it=it->next;
+        } 
+        return dummy->next;
     }
 };
