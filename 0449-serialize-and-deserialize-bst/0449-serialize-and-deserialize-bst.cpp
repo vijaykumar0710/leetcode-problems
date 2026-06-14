1e72bcb73bffca13bafe75c8a330cb347eba1c87
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-14 16:50:03 +0530

    Time: 27 ms (33.04%), Space: 33.5 MB (23.1%) - LeetHub

---
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.cpp
@@ -0,0 +1,40 @@
+class Codec {
+public:
+    string s;
+    void preorder(TreeNode* root){
+        if(root==NULL){
+            s+="#,";
+            return;
+        }
+        s+=to_string(root->val)+",";
+        preorder(root->left);
+        preorder(root->right);
+    }
+    string serialize(TreeNode* root) {
+        preorder(root);
+        return s;
+    }
+TreeNode* build(vector<string>&s,int &idx){
+    if(s[idx]=="#"){
+        idx++;
+        return NULL;
+    }
+    TreeNode* root=new TreeNode(stoi(s[idx]));
+    idx++;
+    root->left=build(s,idx);
+    root->right=build(s,idx);
+    return root;
+}
+    TreeNode* deserialize(string data) {
+        vector<string>s;
+        string temp="";
+        for(auto ch:data){
+            if(ch==','){
+                s.push_back(temp);
+                temp="";
+            }else temp+=ch;
+        }
+        int idx=0;
+        return build(s,idx);
+    }
+};
