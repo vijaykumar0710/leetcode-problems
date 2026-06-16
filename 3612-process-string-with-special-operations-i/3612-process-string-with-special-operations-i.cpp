129b0fa2b9b2ba35fc0109411a0b2180b23e33a5
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-16 07:58:47 +0530

    Time: 7 ms (35.61%), Space: 39.7 MB (69.7%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     string processStr(string s) {
         int n=s.size();
-        string st="";
+        string res="";
         for(int i=0;i<n;i++){
             if(s[i]=='*'){
-               if(!st.empty()) st.pop_back();
+                if(!res.empty()) res.pop_back();
             }
-            else if(s[i]=='#') st+=st;
-            else if(s[i]=='%') reverse(st.begin(),st.end());
-            else st+=s[i];
+            else if(s[i]=='#') res+=res;
+            else if(s[i]=='%') reverse(res.begin(),res.end());
+            else res+=s[i];
         }
-        return st;
+        return res;
     }
 };
