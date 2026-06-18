f8d9e624ac1b868895e11557b1d62a43b5b762d4
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-18 10:23:40 +0530

    Time: 47 ms (14.89%), Space: 54.2 MB (82.85%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let n=nums.length;
+    for(let i=0;i<n;i++){
+        for(let j=i+1;j<n;j++){
+            if(nums[i]+nums[j]==target) return [i,j];
+        }
+    }
+    return {};
+};
