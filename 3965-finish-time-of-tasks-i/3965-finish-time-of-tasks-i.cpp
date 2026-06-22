59f49093fa5bfc89e20edd7f5811ab80b5cf0b04
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-22 08:43:37 +0530

    Time: 219 ms (28.57%), Space: 369.6 MB (14.29%) - LeetHub

--- 3965-finish-time-of-tasks-i/3965-finish-time-of-tasks-i.cpp
+++ 3965-finish-time-of-tasks-i/3965-finish-time-of-tasks-i.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     unordered_map<int,vector<int>>adj;
-    void f(int u,int n,vector<int>&baseTime,vector<long long>&res){
+    long long f(int u,int n,vector<int>&baseTime){
         if(adj[u].empty()){
-            res[u]=baseTime[u];
-            return;
+            return baseTime[u];
         }
         long long maxi=LLONG_MIN,mini=LLONG_MAX;
         for(auto &v:adj[u]){
-            f(v,n,baseTime,res);
-            maxi=max(maxi,res[v]);
-            mini=min(mini,res[v]);
+           long long val=f(v,n,baseTime);
+            maxi=max(maxi,val);
+            mini=min(mini,val);
         }
         long long ownDuration=(long long)(maxi-mini)+baseTime[u];
-        res[u]=(long long)maxi+ownDuration;
+        return (long long)maxi+ownDuration;
     }
     long long finishTime(int n, vector<vector<int>>& edges, vector<int>& baseTime) {
         for(auto &e:edges){
             int u=e[0],v=e[1];
             adj[u].push_back(v);
         }
-        vector<long long>res(n,0);
-        f(0,n,baseTime,res);
-        return (long long)res[0];
+        return f(0,n,baseTime);
+        
     }
 };
