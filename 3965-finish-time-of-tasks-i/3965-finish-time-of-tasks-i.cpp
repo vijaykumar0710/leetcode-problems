f8649241031d6027a07f407130ddbfc12c394fc2
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-22 09:20:56 +0530

    Time: 337 ms (14.29%), Space: 393.9 MB (14.29%) - LeetHub

--- 3965-finish-time-of-tasks-i/3965-finish-time-of-tasks-i.cpp
+++ 3965-finish-time-of-tasks-i/3965-finish-time-of-tasks-i.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    unordered_map<int,vector<int>>adj;
-    long long f(int u,int n,vector<int>&baseTime){
-        if(adj[u].empty()){
-            return baseTime[u];
-        }
-        long long maxi=LLONG_MIN,mini=LLONG_MAX;
-        for(auto &v:adj[u]){
-           long long val=f(v,n,baseTime);
-            maxi=max(maxi,val);
-            mini=min(mini,val);
-        }
-        long long ownDuration=(long long)(maxi-mini)+baseTime[u];
-        return (long long)maxi+ownDuration;
+unordered_map<int,vector<int>>adj;
+long long f(int u,vector<int>& baseTime){
+    if(adj[u].empty()) return baseTime[u];
+    long long maxi=LONG_MIN,mini=LLONG_MAX;
+    for(auto v:adj[u]){
+        long long x=f(v,baseTime);
+        maxi=max(maxi,x);
+        mini=min(mini,x);
     }
+    long long ownDuration=(long long)maxi-mini+baseTime[u];
+    return (long long)maxi+ownDuration;
+}
     long long finishTime(int n, vector<vector<int>>& edges, vector<int>& baseTime) {
-        for(auto &e:edges){
+        for(auto e:edges){
             int u=e[0],v=e[1];
             adj[u].push_back(v);
         }
-        return f(0,n,baseTime);
-        
+        return f(0,baseTime);
     }
 };
