565e1630633b43cd56fd72c903da734b37b76e2a
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-23 16:56:01 +0530

    Time: 2864 ms (5.56%), Space: 55.9 MB (40.74%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
@@ -1,34 +1,29 @@
-int MOD = 1e9 + 7;
-int t[2005][2005][2];
+int t[2005][2005][3];
+const int M=1e9+7;
 class Solution {
-    int N, L, R;
-    int dfs(int idx, int val, int dir) {
-        if (idx == N) return 1;
-        if (t[idx][val][dir] != -1) return t[idx][val][dir];
-        long long ways = 0;
-        if (dir == 1) { 
-            if (val < R) {
-                ways = (dfs(idx + 1, val + 1, 0) + dfs(idx, val + 1, 1)) % MOD;
-            }
-        } 
-        else { 
-            if (val > L) {
-                ways = (dfs(idx + 1, val - 1, 1) + dfs(idx, val - 1, 0)) % MOD;
-            }
-        }
-        return t[idx][val][dir] = ways;
-    }
 public:
+int N,L,R;
+int f(int len,int num,int flag){
+ if(len==N) return 1;
+ if(t[len][num][flag]!=-1) return t[len][num][flag];
+ int ans=0;
+ if(flag){
+    if(num<R)
+    ans=(ans+f(len+1,num+1,0)+f(len,num+1,1))%M;
+ }else{
+    if(num>L)
+    ans=(ans+f(len+1,num-1,1)+f(len,num-1,0))%M;
+ }
+ return t[len][num][flag]=ans;
+}
     int zigZagArrays(int n, int l, int r) {
-        N = n;
-        L = l, R = r;
-        if (n == 1) return r - l + 1;
-        memset(t, -1, sizeof(t));
-        long long total_ways = 0;
-        for (int first_val = l; first_val <= r; first_val++) {
-            total_ways = (total_ways + dfs(1, first_val, 1)) % MOD;
-            total_ways = (total_ways + dfs(1, first_val, 0)) % MOD;
+        N=n,L=l,R=r;
+        int res=0;
+        memset(t,-1,sizeof(t));
+        for(int i=l;i<=r;i++){
+            res=(res+f(1,i,1))%M;
+            res=(res+f(1,i,0))%M;
         }
-        return total_ways;
+        return res;
     }
 };
