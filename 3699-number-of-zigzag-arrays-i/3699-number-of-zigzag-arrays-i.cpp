a6ba45b42a990232ec4917aeaf82858c1bb2a2be
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-23 10:44:29 +0530

    Time: 1826 ms (17.59%), Space: 331.3 MB (31.48%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
@@ -1,61 +1,38 @@
 class Solution {
-public:
-    static const int MOD = 1e9 + 7;
-    static const int MAXN = 2005;
-    static const int MAXK = 2005;
-    int N, K;
-    int up[MAXN][MAXK];
-    int down[MAXN][MAXK];
-    int vis[MAXN];
-    void solve(int len) {
-        if (vis[len]) return;
-        if (len == 2) {
-            for (int x = 0; x < K; x++) {
-                up[len][x] = x;
-                down[len][x] = K - 1 - x;
+    int MOD = 1e9 + 7;
+    int K;
+    int N;
+    vector<int> t;
+    int f(int idx, int val, int dir) {
+        if (idx == N) return 1;
+        int memo_idx = (idx * (K + 1) + val) * 2 + dir;
+        if (t[memo_idx] != -1) {
+            return t[memo_idx];
+        }
+        long long ways = 0;
+            if (dir == 1) { 
+            if (val < K) {
+                ways = (f(idx + 1, val + 1, 0) + f(idx, val + 1, 1)) % MOD;
             }
-            vis[len] = 1;
-            return;
-        }
-        solve(len - 1);
-        long long prefUp[MAXK], prefDown[MAXK];
-        prefUp[0] = up[len - 1][0];
-        prefDown[0] = down[len - 1][0];
-        for (int i = 1; i < K; i++) {
-            prefUp[i] = (prefUp[i - 1] + up[len - 1][i]) % MOD;
-            prefDown[i] = (prefDown[i - 1] + down[len - 1][i]) % MOD;
-        }
-        long long totalUp = prefUp[K - 1];
-        for (int x = 0; x < K; x++) {
-            up[len][x] = 0;
-            down[len][x] = 0;
-            if (x > 0) {
-                up[len][x] = prefDown[x - 1];
+        } 
+        else { 
+            if (val > 1) {
+                ways = (f(idx + 1, val - 1, 1) + f(idx, val - 1, 0)) % MOD;
             }
-            down[len][x] = (totalUp - prefUp[x] + MOD) % MOD;
         }
-        vis[len] = 1;
+        return t[memo_idx] = ways;
     }
+public:
     int zigZagArrays(int n, int l, int r) {
-        N = n;
         K = r - l + 1;
+        N = n;
         if (n == 1) return K;
-        for (int i = 0; i <= N; i++) {
-            vis[i] = 0;
-            for (int j = 0; j < K; j++) {
-                up[i][j] = 0;
-                down[i][j] = 0;
-            }
+        t.assign((N + 1) * (K + 1) * 2, -1);
+        long long total_ways = 0;
+        for (int first_val = 1; first_val <= K; first_val++) {
+            total_ways = (total_ways + f(1, first_val, 1)) % MOD;
+            total_ways = (total_ways + f(1, first_val, 0)) % MOD;
         }
-        solve(N);
-        long long ans = 0;
-        for (int x = 0; x < K; x++) {
-            ans += up[N][x];
-            ans %= MOD;
-
-            ans += down[N][x];
-            ans %= MOD;
-        }
-        return ans;
+        return total_ways;
     }
 };
