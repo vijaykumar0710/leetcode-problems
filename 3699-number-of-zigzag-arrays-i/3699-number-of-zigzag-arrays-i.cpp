acb6be3080722093e34fcb151c0d232eda537ec5
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-23 14:11:55 +0530

    Time: 2115 ms (13.89%), Space: 339.8 MB (31.48%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
@@ -1,37 +1,43 @@
 class Solution {
     int MOD = 1e9 + 7;
-    int K;
-    int N;
-    vector<int> t;
-    int f(int idx, int val, int dir) {
+    int N,L,R;
+    
+    // Sirf ek 1D Vector allocate karenge. Super fast!
+    vector<int> memo;
+
+    int dfs(int idx, int val, int dir) {
         if (idx == N) return 1;
-        int memo_idx = (idx * (K + 1) + val) * 2 + dir;
-        if (t[memo_idx] != -1) {
-            return t[memo_idx];
-        }
+
+        // 3D coordinates ko 1D index mein convert karne ka formula
+        int memo_idx = (idx * (R + 1) + val) * 2 + dir;
+
+        if (memo[memo_idx] != -1) return memo[memo_idx];
+
         long long ways = 0;
-            if (dir == 1) { 
-            if (val < K) {
-                ways = (f(idx + 1, val + 1, 0) + f(idx, val + 1, 1)) % MOD;
+        
+        if (dir == 1) { 
+            if (val < R) {
+                ways = (dfs(idx + 1, val + 1, 0) + dfs(idx, val + 1, 1)) % MOD;
             }
         } 
         else { 
-            if (val > 1) {
-                ways = (f(idx + 1, val - 1, 1) + f(idx, val - 1, 0)) % MOD;
+            if (val > L) {
+                ways = (dfs(idx + 1, val - 1, 1) + dfs(idx, val - 1, 0)) % MOD;
             }
         }
-        return t[memo_idx] = ways;
+
+        return memo[memo_idx] = ways;
     }
 public:
     int zigZagArrays(int n, int l, int r) {
-        K = r - l + 1;
         N = n;
-        if (n == 1) return K;
-        t.assign((N + 1) * (K + 1) * 2, -1);
+        L=l,R=r;
+        if (n == 1) return  r - l + 1;
+        memo.assign((N + 1) * (r + 1) * 2, -1);
         long long total_ways = 0;
-        for (int first_val = 1; first_val <= K; first_val++) {
-            total_ways = (total_ways + f(1, first_val, 1)) % MOD;
-            total_ways = (total_ways + f(1, first_val, 0)) % MOD;
+        for (int first_val = l; first_val <= r; first_val++) {
+            total_ways = (total_ways + dfs(1, first_val, 1)) % MOD;
+            total_ways = (total_ways + dfs(1, first_val, 0)) % MOD;
         }
         return total_ways;
     }
