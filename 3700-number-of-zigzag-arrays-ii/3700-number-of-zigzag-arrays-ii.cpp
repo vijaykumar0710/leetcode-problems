e57f1f26dfe70374eeeeb035cb082185a514b8a6
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-24 15:46:13 +0530

    Time: 1353 ms (23.26%), Space: 49.8 MB (30.23%) - LeetHub

--- 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.cpp
+++ 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.cpp
@@ -4,10 +4,9 @@ public:
     vector<vector<long long>> multiply(const vector<vector<long long>>& A, const vector<vector<long long>>& B) {
         int n = A.size();
         vector<vector<long long>> C(n, vector<long long>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int k = 0; k < n; k++) {
-                if (A[i][k] == 0) continue;
-                for (int j = 0; j < n; j++) {
+       for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
                     C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % MOD;
                 }
             }
