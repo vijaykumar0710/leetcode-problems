0a3cd94df74d2520a818f575f66aeb6eb3544597
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-26 12:01:59 +0530

    Time: 488 ms (29.14%), Space: 175.4 MB (34.43%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
-    int query(int qs, int qe, int idx, int l, int r,
-              unordered_map<int, int>& seg) {
-        if (qe < l || qs > r)
-            return 0;
-        if (qs <= l && r <= qe)
-            return seg[idx];
+unordered_map<int, int> seg;
+    int query(int qs, int qe, int idx, int l, int r) {
+        if (qe < l || qs > r) return 0;
+        if (qs <= l && r <= qe) return seg[idx];
         int mid = l + (r - l) / 2;
-        return query(qs, qe, idx * 2 + 1, l, mid, seg) +
-               query(qs, qe, idx * 2 + 2, mid + 1, r, seg);
+        return query(qs, qe, idx * 2 + 1, l, mid) +
+               query(qs, qe, idx * 2 + 2, mid + 1, r);
     }
-    void update(int pos, int idx, int l, int r, unordered_map<int, int>& seg) {
+    void update(int pos, int idx, int l, int r) {
         if (l == r) {
             seg[idx]++;
             return;
         }
         int mid = l + (r - l) / 2;
         if (pos <= mid)
-            update(pos, idx * 2 + 1, l, mid, seg);
+            update(pos, idx * 2 + 1, l, mid);
         else
-            update(pos, idx * 2 + 2, mid + 1, r, seg);
+            update(pos, idx * 2 + 2, mid + 1, r);
         seg[idx] = seg[idx * 2 + 1] + seg[idx * 2 + 2];
     }
     long long countSmaller(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int> seg;
         long long res = 0;
         int mini=*min_element(nums.begin(),nums.end());
         int maxi=*max_element(nums.begin(),nums.end());
         for (int i = 0; i < n; i++) {
             long long ans = 0;
-            ans = query(mini, nums[i] - 1, 0, mini, maxi, seg);
+            ans = query(mini, nums[i] - 1, 0, mini, maxi);
             res += ans;
-            update(nums[i], 0, mini, maxi, seg);
+            update(nums[i], 0, mini, maxi);
         }
         return res;
     }
