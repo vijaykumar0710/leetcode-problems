ce86b0a6ee2e95b6bcd9155bf547d915da01d5ff
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-26 10:07:03 +0530

    Time: 596 ms (43.44%), Space: 151.3 MB (61.26%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
-vector<int>res;
-map<int,int>BIT;
-int m;
-void update(int i){
-for(;i<=m;i+=i&-i) BIT[i]+=1;
-}
-int query(int i){
-int res=0;
-for(;i>0;i-=(i&-i)){
-    res+=BIT[i]; 
-  }
-  return res;
-}
+    int query(int qs, int qe, int idx, int l, int r, unordered_map<int,int>& seg) {
+        if (qe < l || qs > r)
+            return 0;
+        if (qs <= l && r <= qe)
+            return seg[idx];
+        int mid = l + (r - l) / 2;
+        return query(qs, qe, idx * 2 + 1, l, mid, seg) +
+               query(qs, qe, idx * 2 + 2, mid + 1, r, seg);
+    }
+
+    void update(int pos, int idx, int l, int r, unordered_map<int,int>& seg) {
+        if (l == r) {
+            seg[idx]++;
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        if (pos <= mid)
+            update(pos, idx * 2 + 1, l, mid, seg);
+        else
+            update(pos, idx * 2 + 2, mid + 1, r, seg);
+        seg[idx] = seg[idx * 2 + 1] + seg[idx * 2 + 2];
+    }
     vector<int> countSmaller(vector<int>& nums) {
         int n=nums.size();
-        res.resize(n);
-        int maxi=10001;
-        m=2*maxi+1;
-        for(int i=n-1;i>=0;i--){
-            int x=query(nums[i]+maxi);
-            res[i]=x;
-            update(nums[i]+maxi+1);
+        unordered_map<int,int>seg;
+        vector<int>res;
+        for (int i = n-1; i>=0; i--) {
+            int ans = 0;
+            ans = query(0, nums[i] - 1+10000, 0, 0, 20000, seg);
+            res.push_back(ans);
+            update(nums[i]+10000, 0, 0, 20000, seg);
         }
+        reverse(res.begin(), res.end());
         return res;
     }
 };
