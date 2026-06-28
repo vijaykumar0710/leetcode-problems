9bdfe717e115b20e12fafd8ea16f447b5d08223f
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-06-28 16:36:00 +0530

    Time: 104 ms (36.63%), Space: 192.5 MB (49.59%) - LeetHub

--- 3975-filter-occupied-intervals/3975-filter-occupied-intervals.cpp
+++ 3975-filter-occupied-intervals/3975-filter-occupied-intervals.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
-    vector<vector<int>> filterOccupiedIntervals(vector<vector<int>>& OI, int freeStart, int freeEnd) {
+    vector<vector<int>> filterOccupiedIntervals(vector<vector<int>>& OI, int FS, int FE) {
         int n=OI.size();
-        vector<vector<int>>res;
+        // merge
         sort(OI.begin(),OI.end());
-        int prev_st=OI[0][0];
-        int prev_end=OI[0][1];
-        for(int i=1;i<n;i++){
-           if(OI[i][0]<=prev_end+1){
-            prev_end=max(prev_end,OI[i][1]);
-           }else{
-            res.push_back({prev_st,prev_end});
-            prev_st=OI[i][0];
-            prev_end=OI[i][1];
-           }
-        }
-        res.push_back({prev_st,prev_end});
-        int m=res.size();
-        for(int i=0;i<m;i++){
-           if(res[i][0]<freeStart && res[i][1]>freeEnd){
-              res.push_back({freeEnd+1,res[i][1]});
-              res[i][1]=freeStart-1;
-           }
-           if(res[i][0]>=freeStart && res[i][1]<=freeEnd){
-            res[i][0]=0;
-            res[i][1]=0;
-           }
-           if(res[i][0]<freeStart && res[i][1]>=freeStart) res[i][1]=freeStart-1;
-           if(res[i][0]<=freeEnd && res[i][1]>freeEnd) res[i][0]=freeEnd+1;
-        }
-        sort(res.begin(),res.end());
-        vector<vector<int>>ans;
-        for(int i=0;i<res.size();i++){
-            if(res[i][0]==0 && res[i][1]==0) continue;
-            ans.push_back(res[i]);
-        }
-        return ans;
+        vector<vector<int>>merge;
+         int prev_st=OI[0][0];
+         int prev_end=OI[0][1];
+         for(int i=1;i<n;i++){
+            if(OI[i][0]<=prev_end+1) prev_end=max(prev_end,OI[i][1]);
+            else{
+                merge.push_back({prev_st,prev_end});
+                prev_st=OI[i][0];
+                prev_end=OI[i][1];
+            }
+         }
+        merge.push_back({prev_st,prev_end});
+       // FS and FE ka Tandav
+       int m=merge.size();
+       for(int i=0;i<m;i++){
+          if(merge[i][0]<FS && merge[i][1]>FE){
+            merge.push_back({FE+1,merge[i][1]});
+            merge[i][1]=FS-1;
+          }
+          if(merge[i][0]>=FS && merge[i][1]<=FE){
+            merge[i][0]=0;
+            merge[i][1]=0;
+          }
+          if(merge[i][0]<FS && merge[i][1]>=FS) merge[i][1]=FS-1;
+          if(merge[i][0]<=FE && merge[i][1]>FE) merge[i][0]=FE+1;
+       }
+       //Remove interval--> {0,0}
+       sort(merge.begin(),merge.end());
+       vector<vector<int>>res;
+       for(int i=0;i<merge.size();i++){
+        if(merge[i][0]==0 && merge[i][1]==0) continue;
+        res.push_back(merge[i]); 
+       }
+       return res;
     }
 };
